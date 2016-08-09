6d050fabae1086be79b2df0507ff598eab23bebc
RGero215 <rgero215@gmail.com>
2016-08-09 12:28:55 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function takeANumber (katzDeliLine, newName) {
+  // body...
+  var katzDeli = [];
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    katzDeli.push(katzDeliLine[i]);
+    return katzDeliLine
+  };
+}
