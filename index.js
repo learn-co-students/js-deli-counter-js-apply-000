27ab7e4264a622bc9264f8867ff4e36a59eef5b5
Marissa Okada <marissaokada@gmail.com>
2016-08-10 05:06:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1)
+  + " in line.";
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    var current = "The line is currently: "
+
+    for (var i = 0; i < katzDeliLine.length; i++){
+      current += ((i + 1) + ". " + katzDeliLine[i] + ", ");
+    }
+  }
+
+  return current.slice(0, current.length - 2);
+}
