46e4629a0a36ee733a256cad13c0b5f06ca97d92
SUMANT NIDAGUNDI <sumantnidagundi525@gmail.com>
2016-08-11 19:37:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(line,name)
+{
+//  var katzDeli=[]
+  line.push(name)
+  var i=0
+  if(katzDeliLine.length==1)
+
+  return "Welcome" +name.+ "You are number 1 in line."
+  else {
+    return "Welcome" +name.+"You are" +katzDeliLine.length+ "in line."
+  }
+}
