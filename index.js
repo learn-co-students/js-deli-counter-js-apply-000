0c7300e29867cc017f7d68459bb43e4ef0291886
Niky Morgan <niky.morgan@gmail.com>
2016-08-15 02:07:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeli = [];
+
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${line.shift()}.`
+  }
+}
+
+function currentLine(line) {
+  var indexArray = []
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+   for (let i = 0; i < line.length; i++) {
+    indexArray.push(`${i+1}. ${line[i]}`)
+   }
+    return `The line is currently: ${indexArray.join(', ')}`
+  }
+}
