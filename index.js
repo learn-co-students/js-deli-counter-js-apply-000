e0fb4ec302a1ff51455f39d08af588e907c53b25
Gurwinder Singh <gurwindersinghnyc@gmail.com>
2016-08-22 04:19:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var katzDeli = [];
 
-takeANumber = function(katzDeliLine, name){
+var takeANumber = function(katzDeliLine, name){
 katzDeliLine.push(name)
 return "Welcome, " + name + "." + " You are number " + katzDeliLine.length + " in line."
 }
-function nowServing(katzDeliLine, name){
-katzDeliLine.pop(name)
-  return katzDeliLine.length[katzDeliLine.length-1]
-  if(katzDeliLine===0){
-    return "There is nobody waiting to be served!"
+var nowServing = function(katzDeliLine){
+    if(katzDeliLine.length===0){
+      return "There is nobody waiting to be served!"
     }
-}
+    var names = ["Steven", "Blake", "Avi"]
+    katzDeliLine.shift(names)
+      return "Currently serving " + names[0] + "."
+  }
+var currentLine = function(katzDeliLine){
+  if(katzDeliLine.length===0){
+    return "The line is currently empty."
+  }
+  var names = ["1. Bill", " 2. Jane", " 3. Ann"]
+    return "The line is currently: " + names
+  }
