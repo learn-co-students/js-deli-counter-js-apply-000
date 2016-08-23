862748d52045e376caa5399cd634beddc07b94b4
Gurwinder Singh <gurwindersinghnyc@gmail.com>
2016-08-23 13:35:20 +0000

    Done.

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ var currentLine = function(katzDeliLine){
   if(katzDeliLine.length===0){
     return "The line is currently empty."
   }
-  var names = ["1. Bill", " 2. Jane", " 3. Ann"]
-    return "The line is currently: " + names
+var array = []
+for(var i = 0; i<katzDeliLine.length; i++){
+  array.push(i+1 + ". " + katzDeliLine[i])
+}
+return "The line is currently: " + array.join(', ')
   }
