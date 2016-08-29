5a099e2a666db6351e000692e54aeae19bf72bab
Marine Boudeau <m14868@gmail.com>
2016-08-29 11:30:11 +0000

    Done.

--- index.js
+++ index.js
@@ -8,13 +8,30 @@ function takeANumber(deli, name) {
 function nowServing(deli) {
   if (deli.length > 0) {
     return "Currently serving " + deli[0]+ ".";  
-    deli.splice(0, 1);
+    deli = deli.splice(0, 1);
+    return deli;
   } else {
     return "There is nobody waiting to be served!";
   }
 }
 
-function currentLine(line) {
+function currentLine(line) { 
+  var text = "The line is currently:";
   
-  
+  if (line.length > 0) {
+    for (var i=0 ; i < line.length ; i++) {
+      text += " "+ (line.indexOf(line[i])+1) + ". " + line[i] +",";  
+      if (i === (line.length - 1)) {
+        text = text.slice(0,-1)
+      }      
+    }
+    return text;  
+        
+  } else {
+    return "The line is currently empty."
+  }
 }
+
+
+
+
