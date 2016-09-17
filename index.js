1dc989fc2b2c6d2540cce5b74333062bb5b327e9
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2016-09-17 19:46:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-var katzDeliLine = [];
-
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return(name + ", you are number " + katzDeliLine.length " in line.");
+function takeANumber(line, name) {
+  line.push(name)
+  return("Welcome, " + name + ". You are number " + line.length + " in line.")
 };
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return("Currently serving " + katzDeliLine.shift());
+function nowServing(line) {
+  if (line.length > 0) {
+    return("Currently serving " + line.shift() + ".")
   } else {
-    return("There is nobody waiting to be served!");
-  };
+    return("There is nobody waiting to be served!")
+  }
 };
 
-function currentLine(katzDeliLine) {
-  var lineText = '';
-  if (katzDeliLine.length > 0) {
-    for (var i = 0; i < katzDeliLine.length; i ++) {
-      lineText += " " + i + 1 + ". " + katzDeliLine[i]);
+function currentLine(line) {
+  var lineText = [];
+  if (line.length > 0) {
+    for (var i = 0; i < line.length; i ++) {
+      lineText.push(i + 1 + ". " + line[i])
     };
-    return ("The line is currently:" + lineText);
+    return("The line is currently: " + lineText.join(', '))
   } else {
-    return("The line is currently empty.");
+    return("The line is currently empty.")
   };
 };
