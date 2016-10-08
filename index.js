5806abc11eced5c3985d3c266744a9a321583404
Joan Lamrack <joanlamrack@gmail.com>
2016-10-08 04:45:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 var katzDeli=[];
-var katzDeliLine=0;
+var katzDeliLine;
 var otherDeli = ["Steven", "Blake", "Avi"];
 
 
 function takeANumber(array, name){
-  array.unshift(name);
+  array.push(name);
   var katzDeliLine= array.indexOf(name);
   return "Welcome, "+name+". You are number "+ (katzDeliLine+1)+ " in line.";
+}
+
+function nowServing(array){
+
+  if(array[0]==null){
+    return "There is nobody waiting to be served!";
+  }
+  else{
+    var announcement="Currently serving "+ array[0]+".";
+    array.shift();
+    return announcement;
+  }
+}
+
+function currentLine(array){
+  if(array[0]==null){
+    return "The line is currently empty.";
+  }
+  else{
+    var i=0;
+    var announcement= "The line is currently: ";
+    while(array[i]!=null){
+      announcement+=(i+1)+ ". " + array[i]+ " ";
+    }
+  }
+  return announcement;
 }
