a72ef0cb12b25e9d7740b76433646054ed765235
Ajmal jalal <ajmaljalal58@gmail.com>
2016-10-09 21:36:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function takeANumber(katzDeliLine, name){
+  var katzDeli = [];
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    if (katzDeliLine[i] === name){
+      katzDeli.Push(name);
+
+  }
+
+  }
+return "Welcome, " katzDeli[i] + ". You are number" + katzDeli.indexOf[i] + "in line.";
+
+}
