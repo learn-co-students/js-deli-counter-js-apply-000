4dce9808b6f4cee62ee9d6530e612ec50dc2f197
Jenny Lai <jlai902@gmail.com>
2016-11-10 04:11:16 +0000

    Done.

--- index.js
+++ index.js
@@ -3,24 +3,35 @@ function takeANumber(katzDeli, name) {
 	return("Welcome, " + name + ". You are number " + (katzDeli.length) + " in line.");
 }
 
+//Build a function nowServing. This function should return the first person in line
+//and then remove that individual from the line. If there is nobody in line, it should
+//return "There is nobody waiting to be served!"
+
+var deliLine = ["Steven", "Blake", "Avi"];
 
 function nowServing(deliLine) {
   if (deliLine.length === 0) {
     return('There is nobody waiting to be served!')
   } else {
-    deliLine.shift();
-    return('Currently serving ' + deliLine[0] + '.');
+    console.log('Currently serving ' + deliLine[0] + '.');
+		deliLine.shift();
   }
+	var servingNow = nowServing()();
 }
 
+//const deliLine = ["Steven", "Blake", "Avi"]
+
+var deli = [];
+
 function currentLine(line) {
+	var i = 0
 	if (line.length === 0) {
 		return('The line is currently empty.');
 	} else {
-		for (var i = 0; i < line.length; i++) {
-			var deli = (i + 1) + ". " + line[i];
-				line.push(deli);
+		while (i < line.length) {
+			deli.push(" " + (i + 1) + ". " + line[i])
+			i++
 		}
-		return("The line is currently: " + line);
+		return("The line is currently:" + deli);
 	}
 }
