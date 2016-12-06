350ee2bedb77d58c5af5e64b6e4f2ce5afd0c39a
Kevin Dam <kevinddam@gmail.com>
2016-12-06 20:37:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(line, name){
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line){
+  if(line[0] === undefined){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    return `Currently serving ${line.shift()}.`
+  }
+}
+
+function currentLine(line){
+  if(line[0] === undefined){
+    return "The line is currently empty."
+  }
+  else{
+    var lineMessage = "The line is currently: "
+    for(var i = 0; i<line.length; i++){
+      lineMessage += (i+1) + ". " + line[i] + ", "
+    }
+    lineMessage = lineMessage.substring(0,lineMessage.length-2)
+  }
+
+  return lineMessage
+}
