155a4e0f8cdad5ff90e77630b84e08fdbc491ef2
Tim Freeman <d.timothy.freeman@gmail.com>
2016-12-19 18:45:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  for (let i = 0; i < name.length; i++) {
     katzDeliLine.push(name);
-    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
   }
-}
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   } else {
-      return `Currently serving ${katzDeliLine.shift()}.`
+      return `Currently serving ${katzDeliLine.shift()}.`;
     }
 }
 
 function currentLine(katzDeliLine) {
-  var currentLine = []
-  var n = 1
+  var currentLine = [];
+  var n = 1;
   if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   } else {
     for (let i = 0; i < katzDeliLine.length; i++) {
-      currentLine.push(` ${n}. ${katzDeliLine[i]}`)
-      n+=1
+      currentLine.push(` ${n}. ${katzDeliLine[i]}`);
+      n+=1;
     }
-    return "The line is currently:" + currentLine
+    return "The line is currently:" + currentLine;
   }
 }
