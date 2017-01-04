78ef4eea2550053b93846b81805cfdb594619925
Shineka Barnett <barnettshin1@gmail.com>
2017-01-04 21:09:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (!katzDeliLine.length) {
+    return "There is nobody waiting to be served!";
+  }
+
+  return `Currently serving ${katzDeliLine.shift()}.`;
+}
+
+function currentLine(katzDeliLine) {
+  if (!katzDeliLine.length) {
+    return "The line is currently empty.";
+  }
+
+  const numbersAndNames = [];
+
+  for (var i = 0, j = katzDeliLine.length; i < j; i++) {
+    numbersAndNames.push(`${i + 1}. ${katzDeliLine[i]}`);
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`;
+}
