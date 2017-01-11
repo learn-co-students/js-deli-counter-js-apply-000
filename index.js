a0e54200d352e38c6f495b4c2a0d5d289d6c25a8
Antony West <antonywest.design@gmail.com>
2017-01-11 16:46:27 +0000

    Done.

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ function takeANumber(line, name) {
   line.push(name)
     return `Welcome, ${name}. You are number ${line.length} in line.`
 }
-
 function nowServing(line) {
-  if(!line.length) {
-    return "There is nobody waiting to be served!"
+  if (!line.length){
+    return 'There is nobody waiting to be served!'
   }
   return `Currently serving ${line.shift()}.`
 }
-
 function currentLine(line) {
-  if (!line.length) {
-    return "The line is currently empty."
+  if (!line.length){
+    return 'The line is currently empty.'
   }
   const peopleInLine = []
     for (let i = 0, p = line.length; i < p; i++) {
