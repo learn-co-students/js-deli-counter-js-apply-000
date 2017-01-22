6b8d39ee047741d96179e77425bce3cc19132eb0
Abraham  Kwak <kwak.abe@gmail.com>
2017-01-22 01:05:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-var katzDeliLine = [];
+var line = [];
 
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+function nowServing(line) {
+  if (line.length === 0) {
     return 'There is nobody waiting to be served!';
   }
-
-  return 'Currently serving ' + katzDeliLine.shift(name) + '.';
+  return 'Currently serving ' + line.shift(name) + '.';
 }
 
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+function currentLine(line) {
+  if (line.length === 0) {
     return 'The line is currently empty.';
   }
-
   var customerNumber = [];
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    customerNumber.push(`${[i + 1]}. ${katzDeliLine[i]}`);
+  for(var i = 0; i < line.length; i++) {
+    customerNumber.push(`${[i + 1]}. ${line[i]}`);
   }
   return `The line is currently: ${customerNumber.join(', ')}`;
 }
