e2114ba61b19245894c65492c8794dc795bd11a2
Abraham  Kwak <kwak.abe@gmail.com>
2017-01-22 00:52:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!';
+  }
+
+  return 'Currently serving ' + katzDeliLine.shift(name) + '.';
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.';
+  }
+
+  var customerNumber = [];
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    customerNumber.push(`${[i + 1]}. ${katzDeliLine[i]}`);
+  }
+  return `The line is currently: ${customerNumber.join(', ')}`;
+}
