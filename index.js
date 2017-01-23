52197b7198bc5ad36baa5fb4b739c91835ab12a6
Abraham  Kwak <kwak.abe@gmail.com>
2017-01-23 17:40:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-var line = [];
+var personInLine = [];
 
-function takeANumber(line, name) {
-  line.push(name);
-  return `Welcome, ${name}. You are number ${line.length} in line.`;
+var takeANumber = (personInLine, name) => {
+  personInLine.push(name);
+  return `Welcome, ${name}. You are number ${personInLine.length} in line.`;
 }
 
-function nowServing(line) {
-  if (line.length === 0) {
-    return 'There is nobody waiting to be served!';
+// function takeANumber(personInLine, name) {
+//   personInLine.push(name);
+//   return `Welcome, ${name}. You are number ${personInLine.length} in line.`;
+// }
+
+var nowServing = personInLine => {
+  if (personInLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return "Currently serving " + personInLine.shift(name) + ".";
   }
-  return 'Currently serving ' + line.shift(name) + '.';
 }
 
-function currentLine(line) {
-  if (line.length === 0) {
-    return 'The line is currently empty.';
+// function nowServing(personInLine) {
+//   if (personInLine.length === 0) {
+//     return 'There is nobody waiting to be served!';
+//   }
+//   return 'Currently serving ' + personInLine.shift(name) + '.';
+// }
+
+var currentLine = personInLine => {
+  if (personInLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    var customerNumber = [];
+    for (var i = 0; i < personInLine.length; i++) {
+      customerNumber.push(`${[i + 1]}. ${personInLine[i]}`);
+    }
+    return `The line is currently: ${customerNumber.join(', ')}`;
   }
-  var customerNumber = [];
-  for(var i = 0; i < line.length; i++) {
-    customerNumber.push(`${[i + 1]}. ${line[i]}`);
-  }
-  return `The line is currently: ${customerNumber.join(', ')}`;
 }
+
+// function currentLine(personInLine) {
+//   if (personInLine.length === 0) {
+//     return 'The line is currently empty.';
+//   }
+//   var customerNumber = [];
+//   for(var i = 0; i < personInLine.length; i++) {
+//     customerNumber.push(`${[i + 1]}. ${personInLine[i]}`);
+//   }
+//   return `The line is currently: ${customerNumber.join(', ')}`;
+// }
