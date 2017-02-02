4aec2647c04258fa68c03cf70cb807cdfeba1b5d
Justin Bland <jrbland@yahoo.com>
2017-02-02 04:50:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + (katzDeliLine.length ) + " in line."
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }else {
+    var temp = katzDeliLine[0]
+    katzDeliLine.shift()
+    return "Currently serving " + temp + "."
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }else {
+    var temp = "The line is currently: "
+    for(var i = 0; i < katzDeliLine.length; i++) {
+      temp += (i+1) + ". " + katzDeliLine[i]
+      if(i !== katzDeliLine.length - 1){
+        temp += ", "
+      }
+    }
+    return temp
+  }
+}
