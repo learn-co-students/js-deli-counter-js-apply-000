d7740d56a39759b178e6a3891f9e18028f15e793
Christopher Buggelli <cjab86@gmail.com>
2017-02-03 17:07:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeli = [];
+
+function takeANumber(deliLine, name) {
+  deliLine.push(name);
+  return "Welcome, " + name + ". You are number " + deliLine.length + " in line.";
+}
+
+
+function nowServing(deliLine) {
+  if (deliLine.length > 0) {
+    // so how do we get a name and how do we store it?
+    var name = deliLine[0]
+    // how do we remove the first element of an array?
+    deliLine = deliLine.shift();
+    return "Currently serving " + name + "."
+  }
+
+  if (deliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(deliLine) {
+  var lineList = []
+  if (deliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    for(var i = 0; i < deliLine.length; i++){
+      lineList.push(`${i+1}. ${deliLine[i]}`);
+    }
+    return `The line is currently: ${lineList.join(', ')}`;
+  }
+}
