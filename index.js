d44831a0b669344f7a9d7df672d41e7e6508fc82
ayafushimi <aya.fushimi@gmail.com>
2017-02-07 11:12:51 -0500

    Done

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+"use strict";
 
-function takeANumber(line, name) {
-  line.push(name);
-  return "Welcome, " + name +". You are number " + line.length + " in line.";
+function takeANumber(line, person) {
+  line.push(person);
+  return "Welcome, " + person + ". You are number "+ line.length + " in line.";
 }
 
 function nowServing(line) {
-  if(line.length === 0) {
+  if (!line.length) {
     return "There is nobody waiting to be served!";
   } else {
-    var name = line.shift()
+    var name = line.shift();
     return "Currently serving " + name + ".";
   }
 }
 
 function currentLine(line) {
-  if(line.length === 0) {
-    return "The line is currently empty."
+  if (!line.length) {
+    return "The line is currently empty.";
   } else {
-    var arr =[];
-    for(var i = 1; i <= line.length; i++) {
-      arr.push(i + ". " + line[i-1]);
-    }
-    return  "The line is currently: " + arr.join(", ");
+    var order = [];
+    line.forEach(
+      function(p, i) {
+        order.push(i + 1 + ". " + p);
+      }
+    );
+    return "The line is currently: " + order.join(", ");
   }
 }
