660c786b9e2961c5ac13957f21f80a7b8e81ecca
John Petsagourakis <jpets91@gmail.com>
2017-03-27 05:48:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+function currentLine(line) {
+  if (!line.length) {
+    //checks if line length = 0
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+    //adds people in line to array numbersAndNames
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+  //spits out the names and line positions of everyone in line
+};
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+  //uses line.length to return the value of the position of the person in line
+};
+
+
+
+/*My original solution which approached it somewhat incorrectly
+
 var katzDeli = [];
 
 function takeANumber(line, name) {
@@ -25,3 +61,4 @@ function currentLine(linename) {
     return (intro + "1. " + linename[0] + ", 2. " + linename[1] + ", 3. " + linename[2])
   }
 }
+*/
