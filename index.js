4bf511fd16cf8dab5c506e6a84a941daef5a6d77
Eskandari Nathalia <eskandari.nat@gmail.com>
2017-04-12 19:23:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+    for (var i = 0; i < katzDeliLine; i++) {
+  }
+  return "Welcome, " + name + "." + "You are number " + i + " in line"
+  }
+
+function nowServing(name) {
+    for(var i = 0; i < name; i++) {
+      if(name = 0) {
+    console.log ("There is nobody waiting to be served!")
+  }
+  return i
+  }}
+
+function currentLine (name){
+    console.log ("")
+  }
+
