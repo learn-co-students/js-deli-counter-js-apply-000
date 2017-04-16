6b5a25bf48adac89ba273a31978623a54f0bbf1b
Jason Ioannides <jason.ioannides@gmail.com>
2017-04-16 22:24:51 +0000

    Done.

--- index.js
+++ index.js
@@ -3,30 +3,36 @@ let katzDeli = [];
 let takeANumber = (katzDeliLine, name) => {
   katzDeliLine.push(name);
 
-
-  return `Welcome, ${name}, you are currently number ${(katzDeliLine.indexOf(name) + 1)} in line.`;
+  return `Welcome, ${name}. You are number ${(katzDeliLine.indexOf(name) + 1)} in line.`;
 }
 
 let nowServing = katzDeliLine => {
-  if (katzDeliLine.length < 0) {
+  if (katzDeliLine.length <= 0) {
+
     return "There is nobody waiting to be served!";
   } else {
-    return katzDeliLine.shift();
+
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
 
 let currentLine = katzDeliLine => {
   let sentence = "The line is currently: "
-  if (katzDeliLine.length < 0) {
+  if (katzDeliLine.length <= 0) {
+    
     return "The line is currently empty."
   } else {
-    
     for (let i = 0; i < katzDeliLine.length; i++) {
-      sentence += `${i + 1}. ${katzDeliLine[i]} `;
+
+      if (i === ((katzDeliLine.length) - 1)) {
+        sentence += `${i + 1}. ${katzDeliLine[i]}`;
+      } else {
+        sentence += `${i + 1}. ${katzDeliLine[i]}, `;
+      }
     }
   }
 
   return sentence;
-  
+
 }
