65d7c75abd87fd812bd31e4d8fa764abef38616f
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-05-15 04:43:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function takeANumber (katzDeliLine, personName){
+katzDeliLine.push(personName);
+var finalArray=[]
+  var personNumber=0;
+  var string =`Welcome, Ada. You are number 1 in line`                   
+  finalArray.push(string);
+return (`Welcome ${personName}. You are ${number} in line.`)
+ }
