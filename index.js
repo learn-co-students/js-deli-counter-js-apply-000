c18fa1568867e544b6579504b88a62ba0434ced1
David Wen Riccardi-Zhu <dwrz@dwrz.net>
2017-06-14 17:26:17 +0000

    Fix whitespace.

--- index.js
+++ index.js
@@ -14,20 +14,13 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-
   if (katzDeliLine.length === 0){ return "The line is currently empty."; }
   else {
-    var line = "The line is currently:";
-
+    var line = "The line is currently:";   
     for(var i=0; i<katzDeliLine.length; i++){
-
       line += ` ${i+1}. ${katzDeliLine[i]}`
-
       if (i < katzDeliLine.length - 1){ line += ',';}
-
     }
     return line;
-
   }
-
 }
