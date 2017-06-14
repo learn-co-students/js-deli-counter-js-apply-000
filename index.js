cb815998818e1c1f57a8b76c0e076f10a7577b39
David Wen Riccardi-Zhu <dwrz@dwrz.net>
2017-06-14 17:41:04 +0000

    Indentation, added semicolon on line 21.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+    katzDeliLine.push(name);
+    return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length > 0){
-    var report = `Currently serving ${katzDeliLine[0]}.`;
-    katzDeliLine.shift();
-    return report;
-  } else { return "There is nobody waiting to be served!"; }
+    if (katzDeliLine.length > 0){
+	var report = `Currently serving ${katzDeliLine[0]}.`;
+	katzDeliLine.shift();
+	return report;
+    } else { return "There is nobody waiting to be served!"; }
 }
 
 function currentLine(katzDeliLine){
-  if (katzDeliLine.length === 0){ return "The line is currently empty."; }
-  else {
-    var line = "The line is currently:";   
-   katzDeliLine.forEach(function(val,index){
-    line += ` ${index+1}. ${katzDeliLine[index]}`
-    if (index < katzDeliLine.length - 1){ line += ',';}
-   });
-    return line;
-  }
+    if (katzDeliLine.length === 0){ return "The line is currently empty."; }
+    else {
+	var line = "The line is currently:";   
+	katzDeliLine.forEach(function(val,index){
+	    line += ` ${index+1}. ${katzDeliLine[index]}`;
+	    if (index < katzDeliLine.length - 1){ line += ',';}
+	});
+	return line;
+    }
 }
