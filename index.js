48e3d7bff7abfe6177b50eb54d0741b95bc3525d
Marlon Merjos <marlonmerjos@gmail.com>
2017-06-25 20:06:33 +0000

    Done.

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ function takeANumber(katzDeliLine, name) { // function should return the persons
 
 
 function nowServing(katzDeliLine) { // should return the first person in line and then remove that individual from the line. If there is nobody in line, it should return "There is nobdoy waiting to be served!"
-  if (katzDeliLine.length > 0) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
     var serving = katzDeliLine.shift();
     return `Currently serving ${serving}.`
-  } else {
-    return "There is nobody waiting to be served!"
   }
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine.length == 0) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
     for (var i = 0, text = "The line is currently: "; i < katzDeliLine.length; i++) {
