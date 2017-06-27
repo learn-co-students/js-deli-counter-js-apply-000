4d8314159cfc4183fa0d57c35877160f76c1573e
William Derrenberger <wcderrenberger@gmail.com>
2017-06-27 18:19:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-function takeANumber (katzDeli, name) {
- katzDeli.push(`${name}`);
- return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
-}
-
-function nowServing(katzDeli) {
- let i = 0;
- while (i < katzDeli.length) {
- i++;
-}
-  if (katzDeli.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-    else
-      return (`Currently serving ${katzDeli.shift()}.`);
-
-      function currentLine(line) {
+function currentLine(line) {
   if (!line.length) {
     return "The line is currently empty."
   }
@@ -40,4 +24,4 @@ function takeANumber(line, name) {
   line.push(name)
 
   return `Welcome, ${name}. You are number ${line.length} in line.`
-}
+};
