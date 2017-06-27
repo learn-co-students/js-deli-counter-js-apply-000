25e7380205fdc96795d1adfa5c17e94aa827b7bd
William Derrenberger <wcderrenberger@gmail.com>
2017-06-27 18:17:12 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ function nowServing(katzDeli) {
   }
     else
       return (`Currently serving ${katzDeli.shift()}.`);
+
+      function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
