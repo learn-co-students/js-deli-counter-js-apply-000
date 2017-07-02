e5961a83e7d233a3ef3834ccab3b7dc1fe38b192
Lisa Hufnagel <lmhufnagel@gmail.com>
2017-07-02 23:06:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var katzDeliLine = [] //variable takes an empty array, as the below function iterates elements will be added to it
+var katzDeliLine = []
 
-function takeANumber(katzDeliLine, name) { //function takes 2 parameters - the place in the katzDeliLine array and the person's name
-  katzDeliLine.push(name); //pushes a new name onto the end of the  katzDeliLine array and assigns the name an index
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.` //outputs a statement returning a name and their position in line
-};                                                                           // based on the length of the list, not the index
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+};
 
-function nowServing(katzDeliLine, name) {
-  if(!katzDeliLine.length){ // if the line length is 0 (!) the function returns the statement below
+
+function nowServing(katzDeliLine) {
+  if(!katzDeliLine.length){
     return "There is nobody waiting to be served!"
   }
   else{
-  return `Currently serving ${katzDeliLine.shift(0)}.`; // otherwise the name in the 0 index place is destructively called with this statement
+  return `Currently serving ${katzDeliLine.shift(0)}.`;
   }
-}
+};
 
-function currentLine(katzDeliLine) { // this function takes the one paramater, but requires multiple steps
-  if (!katzDeliLine.length){ // if there is no one on line (array has 0(!) people in it) the statement below returns
+
+function currentLine(katzDeliLine) {
+  if (!katzDeliLine.length){
   return "The line is currently empty."
 };
-var listFormat = [] // in order to get the array list to output in the desired format a new variable is created and used in a for loop
-for (var i = 0, l = katzDeliLine.length; i < l; i++) {
-  listFormat.push(` ${i + 1}. ${katzDeliLine[i]}`) // the new variable represents a push of a physical number next to the name in the array
-}                                                       //so I used i + 1 on each loop, then  period and a space and then the person's name in line who is
-return `The line is currently:${listFormat}`       //represented by that loop's position in the array. so if  i = 0, the number 1 will come up,
-}                                                       //but the person in 0 place (Bill) will be printed next to it.
+  var listFormat = [];
+  for (var i = 0, l = katzDeliLine.length; i < l; i++) {
+  listFormat.push(` ${i + 1}. ${katzDeliLine[i]}`)
+}
+  return `The line is currently:${listFormat}`
+}
