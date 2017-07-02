2bf7fdef3555e73b699349914cb95b255cf3ac26
Silvia Chung <schung97@gmail.com>
2017-07-02 04:36:16 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var katzDeliLine = [];
 
 function takeANumber(line, name) {
   line.push(name);
-  for (var i = 0; i < katzLine.length; i++) {
-    if (katzLine[i] === name) {
+  for (var i = 0; i < line.length; i++) {
+    if (line[i] === name) {
       return 'Welcome, ' + name + '.' + ' You are number ' + (i + 1) + ' in line.';
     }
   }
@@ -11,8 +11,8 @@ function takeANumber(line, name) {
 
 
 function nowServing(line) {
-  if (line.length > - 1) {
-    return 'Currently serving ' + line[0] + '.' + line.shift();
+  if (line.length !== 0 ){
+    return 'Currently serving ' + line.shift() + '.';
   } else {
     return 'There is nobody waiting to be served!';
   }
@@ -20,33 +20,32 @@ function nowServing(line) {
 
 
 function currentLine(line) {
-
-  if (line.length === -1) {
+  if (line.length === 0 ) {
     return 'The line is currently empty.';
   } else {
     var lineTracker = [];
     for (var i = 0; i < line.length; i++) {
       lineTracker.push((i + 1) + '. ' + line[i]);
     }
-    return 'The line is currently:' + lineTracker.join(', ');
+    return 'The line is currently: ' + lineTracker.join(', ');
   }
 }
 
 
-takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
-takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
-takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
- //-----------------------------------------------------------------------
-currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
-
-nowServing(katzDeliLine); // "Currently serving Ada."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
-
-takeANumber(katzDeliLine, "Matz"); // "3"
-
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
-
-nowServing(katzDeliLine); // "Currently serving Grace."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
+// //takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+// takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+// takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
+//  //-----------------------------------------------------------------------
+// currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+//
+// nowServing(katzDeliLine); // "Currently serving Ada."
+//
+// currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
+//
+// takeANumber(katzDeliLine, "Matz"); // "3"
+//
+// currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
+//
+// nowServing(katzDeliLine); // "Currently serving Grace."
+//
+// currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
