9bafd5235bfb11732b5a03b5c0d68ac42efa20c5
Alex Busch <abusch1013@gmail.com>
2017-07-18 17:42:45 +0000

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(line) {
 
 function currentLine(line) {
   if (!line.length) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
     var lineNamesAndNumbers = [];
 
