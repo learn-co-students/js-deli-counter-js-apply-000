ca835236a105e596be08a39a0c378b1af13c44d5
Diego Flores <dflores@fandm.edu>
2017-08-04 21:58:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 var katzDeliLine = [];
 function takeANumber (katzDeliLine, customer) {
-  var line = katzDeliLine.length;
+    var line = katzDeliLine.length;
   katzDeliLine.push(customer);
-  return 'Welcome, ${customer}. You are number ${line} in line';
+  return `Welcome, ${customer}. You are number ${line+1} in line.`;
+}
+
+function nowServing (katzDeliLine) {
+    var line = katzDeliLine.length;
+  if (line > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+  else {
+    return `There is nobody waiting to be served!`;
+  }
+}
+
+function currentLine (katzDeliLine) {
+ var KL = katzDeliLine.length;
+ var i = 0;
+ while (i < KL){
+   katzDeliLine[i] = [`${i+1}. ${katzDeliLine[i]}, `];
+   i++}
+ if (i>0) {
+   return `The line is currently: ${katzDeliLine} `;
+ }
+ else {
+   return "The line is currently empty."
 }
 }
