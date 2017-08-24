a5f2ed329c6c7a9f8f3840593278a337635401c0
JoshD1 <joshdeiner75@gmail.com>
2017-08-24 00:48:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
+
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return ("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.");
+  return("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.");
 }
 
-function currentLine(variable) {
-  var line = [];
-  
-  if(variable.length === 0) {
-    return ("The line is currently empty.");
-  } else {
-    for (var i = 0; i < variable.length; i += 1) {
-      line += ((i +1) + ". " + variable[i] + ", ");
-    }
-    line = line.slice(0, line.length-2);
-    return ("The line is currently: " + line);
-  }
-}
-
-function nowServing(variable) {
-  if(variable.length === 0) {
+function nowServing(person) {
+  var result = [];
+  if (!person.length) {
     return ("There is nobody waiting to be served!");
-  } else {
-    var fullName = variable[0];
-    variable.splice(0,1);
-    return ("Currently serving " + fullName + ".");
+  } else{
+    var name = person[0];
+    person.splice(0,1);
+    return ("Currently serving " + name + ".");
   }
+}
+
+function currentLine(line) {
+  var result = [];
+  if(!line.length) {
+    return("The line is currently empty.");
+  } else {
+  for(var i = 0; i < line.length; i += 1) {
+    result += ((i + 1) + ". "  +  line[i] + ", ");
+  }
+  result = result.slice(0,result.length-2);
+  }
+  return ("The line is currently: " + result);
 }
