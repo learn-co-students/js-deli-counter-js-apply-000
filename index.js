8d20d075f6bdd115aa2283015648dab0fb6944cd
Mor Kenane <morkenan@hotmail.com>
2017-09-05 19:16:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var katzDeli = [];
+
+
+function takeANumber(katzDeliLine, name) {
+katzDeliLine.push(name)
+return "Welcome, " + name + "." + " You are number " + katzDeliLine.length + " in line."
+}
+
+
+function nowServing(katzDeliLine) {
+if (katzDeliLine.length > 0) {return "Currently serving " + katzDeliLine.shift() +"."}
+else {return "There is nobody waiting to be served!"
+}}
+
+function currentLine(katzDeliLine) {
+var inline = [];
+if (katzDeliLine.length < 1) {return "The line is currently empty."}
+else {
+for (var i = 0; i < katzDeliLine.length; i++) {
+inline.push(" " + (i+1) + ". " + katzDeliLine[i])}
+return "The line is currently:" + inline
+}}
