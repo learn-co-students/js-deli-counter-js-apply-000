2781aede561edf8c321eed1cd7d8c276480c1575
Ryan Mascolo <rjmascolo@gmail.com>
2017-09-09 02:10:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber (currentLine, newPerson){
   currentLine.push(newPerson);
-  return "Welcome, " + newPerson + ". You are number " + (currentLine.length) + " in line.";
+  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`;
 }
 
 function nowServing (currentLine){
