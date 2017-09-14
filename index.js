a27cc3f6009144ba0124f6a766e785e946aab302
Adiel Wilson <adielwilson@gmail.com>
2017-09-14 05:08:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function takeANumber(var line, var person) {
+function takeANumber ( var line, var person) {
   line.push(person);
   return line.length;
-}
+};
 
-function nowServing (var line){
+function nowServing ( var line){
   if(line == null) return 'There is nobody waiting to be served!';
   else return line.shift();
-}
+};
 
-function currentLine (var line){
+function currentLine ( var line){
   var s = 'The line is currently';
   if(line == null) return s + ' empty.';
   s += ':';
   for(var i = 0; i < line.length; i++) s += ' ${line[i]}';
   s+= '.';
-}
+};
