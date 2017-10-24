90e913ce81d4bd6b4a811edfe17d31cb721e97c1
David Welch <dmw9400@gmail.com>
2017-10-24 03:25:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// A pretty important deli needs somebody to program the "Take a Number" feature for their counter.
-//
-// At the beginning of the day, the deli is empty and is represented by an empty array, e.g.,
-//
-// `var katzDeli = [];`
-//
-// 1. Build a function that a new customer will use when entering the deli. The function, `takeANumber`, should accept the current line of
-//  people, `katzDeliLine`, along with the new person's name as parameters. The function should return their position in line. And don't go
-//   being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their
-//   hopes up by telling them they are number 6 in line.
-//
-// 2. Build a function `nowServing`. This function should return the first person in line and then remove that individual from the line.
-// If there is nobody in line, it should return "There is nobody waiting to be served!"
-//
-// 3. Build a function `currentLine` that returns the current line. For example, if `katzDeliLine` is currently `["Ada", "Grace"]`,
-// `currentLine(katzDeliLine)` would return `"The line is currently: 1. Ada, 2. Grace"`.  If there is nobody in line, it should return
-//  `"The line is currently empty."`
-
 
 function takeANumber(line,name){
   var lineNumber = line.length + 1
@@ -23,8 +5,10 @@ function takeANumber(line,name){
   line = ("Welcome, " + name + ". You are number " + lineNumber + " in line.")
   return line
 }
+
+
 function nowServing(line,name){
-  name = line[0]
+
   if(line.length === 0){
     var line = "There is nobody waiting to be served!"
   }else{
@@ -32,13 +16,13 @@ function nowServing(line,name){
   }
   return line
 }
+
+
 function currentLine(line){
 
-
-
   if(line.length ===0){
-    var finalString = "The line is currently empty."
-    return finalString
+
+    return "The line is currently empty."
 
   }else{
     var middleItems = ""
