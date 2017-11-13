e9f3aa72daf0fc1bd90f7710ba1578b620916501
Kevin Smith <kevincaulfield630@gmail.com>
2017-11-13 00:58:01 +0000

    Done.

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ function nowServing(otherDeli) {
 function currentLine(katzDeli) {
   let i = 0
   while (i < katzDeli.length) {
-    katzDeli.push(` `+[i+1]+`. `  + katzDeli[i])
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
     i++
   }
   if (katzDeli.length === 0) {
     return "The line is currently empty."
   }
   else
-    return (`The line is currently:` + katzDeli)
+    return (`The line is currently:` + line)
 }
