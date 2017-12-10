c819cb3495d8649d22a06cc96cf06e8584259b86
harenarium <carolinel2610@gmail.com>
2017-12-10 02:13:35 +0000

    Done.

--- index.js
+++ index.js
@@ -15,14 +15,18 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  let string = "The line is currently: "
+
   if (katzDeliLine.length === 0){
      return "The line is currently empty."
   } else{
+    let string = "The line is currently: "
     for(let i=0; i<katzDeliLine.length; i++){
       const add = (i+1) === katzDeliLine.length ? "" : ", "
       string = string + `${i+1}. ${katzDeliLine[i]}` + add
+
     }
-  }
+
   return string
+  }
+
 }
