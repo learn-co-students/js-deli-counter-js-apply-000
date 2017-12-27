3e73c985ea8857ee782177aa4059995b67b87dbf
Albert Chen <albert.chen@nyu.edu>
2017-12-27 19:24:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+// var katzDeliLine = [];
+
+function takeANumber(arr, str) {
+  arr.push(str);
+  let position = arr.length;
+  let result = `Welcome, ${str}. You are number ${position} in line.`;
+  return result;
+}
+
+function nowServing(arr) {
+  if (arr.length >= 1) {
+    let firstPerson = arr.shift();
+    return `Currently serving ${firstPerson}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(arr) {
+  let result = 'The line is currently: ';
+  if (arr.length >= 1) {
+    for (let i = 0; i < arr.length; i++) {
+      if (i !== arr.length - 1) {
+        result += `${i + 1}. ${arr[i]}, `;
+      } else {
+        result += `${i + 1}. ${arr[i]}`;
+      }
+    }
+  } else {
+    result = "The line is currently empty.";
+  }
+  return result; 
+}
+
+
+
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (str === arr[i])
+// }
