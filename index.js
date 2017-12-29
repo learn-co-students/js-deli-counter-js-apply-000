a1fd4f62d190f4f2e62fc2595ba5c874774ec094
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-29 15:17:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-function takeANumber(katzDeli, name){
-katzDeli.push(name)
-return "Welcome, " + name + ". You are number" + katzDeli[i] + " in line."
+function takeANumber(katzDeliLine, name){
+var placeInLine = katzDeliLine.length + 1;
+katzDeliLine.push(name)
+return "Welcome, " + name + ". You are number " + placeInLine + " in line."
+};
+
+function nowServing(katzDeliLine){
+	if(katzDeliLine.length === 0){
+		return "There is nobody waiting to be served!"
+	} else{
+		var nextInLine = katzDeliLine[0];
+		var next = katzDeliLine.slice(0, 1)
+		katzDeliLine.shift()
+		return "Currently serving " + next + "."
+	}
+}
+
+function currentLine(katzDeliLine){
+	var list2 = [];
+	var katzDeliLine2 = [];
+		if(katzDeliLine.length === 0){
+			return "The line is currently empty."
+		} else{
+				for(var i = 0; i < katzDeliLine.length; i++){
+					if(i = 0){katzDeliLine2[i] = [i + 1] + ". " + katzDeliLine[i]
+						list2.push(katzDeliLine2[i])
+					} else{
+						katzDeliLine2[i] = " " + [i + 1] + ". " + katzDeliLine[i]
+						list2.push(katzDeliLine2[i])
+					}
+		}
+			return "The line is currently: " + katzDeliLine2
+		}		
+					
+}
