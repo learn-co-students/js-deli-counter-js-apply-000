153b2a375e57bff10583a760f624b6a923992a40
Noah Kalkstein <nkalkstein@gmail.com>
2018-01-03 02:37:08 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function takeANumber(line, name) {
 
   line.push(name);
 
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 
 }
 
@@ -10,18 +10,18 @@ function nowServing(line) {
   if (line.length > 0) {
     return "Currently serving " + line.shift() + ".";
 } else {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
 }
 }
 
-function currentLine(line) {
-  if (line.length === 1) {
-    return  `The line is currently: 1. ${line[0]}`
-} else if (line.length === 2) {
-    return `The line is currently: 1. ${line[0]}, 2. ${line[1]}`
-} else if (line.length === 3) {
-    return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`
-}  else {
-    return "The line is currently empty."
-}
+function currentLine(line){
+  if(line.length < 1) {
+    return "The line is currently empty.";
+  }
+  var lineAssignment = [];
+
+  for(var i=0; i<line.length; i++) {
+    lineAssignment.push(i+1 + ". "+ line[i]);
+  }
+  return "The line is currently: " + lineAssignment.join(', ');
 }
