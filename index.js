0bca51e2bbe91c079cfff4c68e7eb5b7f10ddd92
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-25 02:13:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-var katzDeli = [];
-var katzDeliLine = []
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name)
-  var index = katzDeliLine.indexOf(name)
+var katzDeli = []
+
+function takeANumber(deliArray, name){
+  deliArray.push(name)
+  var index = deliArray.indexOf(name)
   var pos = parseInt(index) + 1
   return `Welcome, ${name}. You are number ${pos} in line.`
 }
-function nowServing(katzDeliLine){
+
+function nowServing(deliArray){
   
-  if(katzDeliLine.length === 0){
-    return `There is nobody waiting to be served!`
-    } else{
-    var shiftName = katzDeliLine[0];
-    katzDeliLine.shift();
-    return `Currently serving ${shiftName}.`;
+  if(deliArray.length > 0){
+    const thisDude = deliArray[0]
+    deliArray.shift()
+    return `Currently serving ${thisDude}.`
+    }
+    if(deliArray.length === 0){
+    
+      return `There is nobody waiting to be served!`
     
   }
 }
-function currentLine(katzDeliLine){
-  var line = katzDeliLine
-  if (line.length === 0){
+
+function currentLine(deliArray){
+  if (deliArray.length === 0){
     return `The line is currently empty.`
-  } else{
-    var i = 0
+  } 
+  else{
     var str = "The line is currently: " 
-    while(i+1<line.length){
-      str = str + `${i+1}. ${katzDeliLine[i]}, `
-      i++
-    }
-    while(i+1===line.length){
-      str = str + `${i+1}. ${katzDeliLine[i]}`
+    for(let i = 0; i < deliArray.length; i++){
+      if(i < deliArray.length - 1){
+      str = str + `${i + 1}. ${deliArray[i]}, `
+      } 
+      
+      if(i === deliArray.length - 1){
+        str = str + `${i + 1}. ${deliArray[i]}`
+      }
+      
     }
       return str
   }
