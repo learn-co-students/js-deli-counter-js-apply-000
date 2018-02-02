d9686a0e33abb9205500d79836bc50097ff63b3e
Betsy Sallee <ensallee@gmail.com>
2018-02-02 18:50:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine=[];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(){
+  if (katzDeliLine.length>0){
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(){
+  let array=[];
+  if (katzDeliLine.length>0){
+    for (let i=0; i<katzDeliLine.length; i++){
+      array.push(`${i+1}. katzDeliLine(i)`);
+    }
+   return `The line is currently: ${array}.`;
+  }
+  else {
+    return "The line is currently empty.";
+  }
+}
