d7e8f25856f4130ca1131c5ecb350598f3720e81
Betsy Sallee <ensallee@gmail.com>
2018-02-05 20:11:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 var katzDeliLine=[];
+var katzDeliLine=[];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length!==0){
-    return `Currently serving ${katzDeliLine.shift()}.`;
-  }
-  else {
+  if (katzDeliLine.length===0){
     return "There is nobody waiting to be served!";
+  }else{
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
 function currentLine(katzDeliLine){
-  let array=[];
-  if (katzDeliLine.length>0){
-    for (let i=0; i<katzDeliLine.length; i++){
-      array.push(` ${i+1}. ${katzDeliLine[i]}`);
-    }
-   return `The line is currently:${array}`;
-  }
-  else {
+var array=[];
+  if (katzDeliLine.length===0){
     return "The line is currently empty.";
+  }else{
+   for (let i=0; i<katzDeliLine.length; i++){
+     array.push(` ${i+1}. ${katzDeliLine[i]}`);
+   }    
+   return `The line is currently:${array}`;
   }
 }
