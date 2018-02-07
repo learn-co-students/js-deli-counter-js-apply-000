5fdc1e6fc129f96595706d6eb32987d9c26a12ad
Kurt <kurt.bauer55@gmail.com>
2018-02-07 02:31:54 +0000

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   var line = [];
 
-  if (katzDeliLine.length == 0) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
     for (var i = 0; i < katzDeliLine.length; i++) {
