9ca2d2b06112c18b9c2261755ff07bda1e0c5af7
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-18 19:29:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 function takeANumber(deliLine, name){
-  deliLine.push(name);
-  return `Welcome, ${name}. You are number ${deliLine.length} in line.`;
+  deliLine.push(name)
+  return `Welcome, ${name}. You are number ${deliLine.length} in line.`
 }
 
 function nowServing(deliLine){
   if (deliLine.length === 0) {
-    return 'There is nobody waiting to be served!';
+    return 'There is nobody waiting to be served!'
   }
-  return `Currently serving ${deliLine.shift()}.`;
+  return `Currently serving ${deliLine.shift()}.`
 
 }
 function currentLine(deliLine){
-  if (deliLine.length === 0) return 'The line is currently empty.'
-  var result = 'The line is currently: ';
+  if (deliLine.length === 0){
+    return 'The line is currently empty.'
+  }
+  var result = 'The line is currently: '
   for (var i = 0; i < deliLine.length; i++) {
-    result += `${i+1}. ${deliLine[i]}`;
+    result += `${i+1}. ${deliLine[i]}`
     if (i+1 !== deliLine.length) {
-      result += ', ';
+      result += ', '
     }
   }
-  return result;
+  return result
 }
