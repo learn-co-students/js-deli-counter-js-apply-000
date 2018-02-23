cd720cb71af14ff3e2c49075e61c857e9bf732e7
Daniel Shimon <danielzshimon@gmail.com>
2018-02-23 05:57:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeli  = [];
+
+function takeANumber(katzDeliLine, name){
+  if (katzDeliLine => 0);
+  katzDeliLine.push(name);
+    return ("Welcome, "  + name +  ". You are number " + (katzDeliLine.length)+  " in line.");
+}
+
+function  nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  } else {
+    return ("Currently serving " +katzDeliLine.splice(0, 1) + ".");
+    }
+}
+
+function  currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0){
+    return ("The line is currently empty.");
+  } else {
+      var numName = [];
+      for(var i = 0; i < katzDeliLine.length; i++ ){
+        numName.push( (i + 1) + ". " + katzDeliLine[i]);
+      }
+    return ("The line is currently: " + numName.join(", "));
+      }
+}
+
+
+
+
