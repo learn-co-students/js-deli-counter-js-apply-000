e257bf0e5aebbe677d11289db597fcbd3e4fa553
Adam Cohn <adamcohn88@gmail.com>
2018-02-24 21:33:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var katzDeliLine = [];
+
+function nowServing (line) {
+	if (line.length === 0) {
+		return "There is nobody waiting to be served!";
+	} else 
+	{
+		return "Currently serving " + line.shift() +".";
+	}
+}
+
+function currentLine(line) {
+	if (line.length === 0) {
+		return "The line is currently empty.";
+	} else {
+		var numberName = [];
+		for (var i = 0; i < line.length; i++) {
+			numberName.push(i+1 + ". "+ line[i]);
+		}
+		return "The line is currently: " + numberName.join(", ");
+	} 
+}
+
+function takeANumber (line, name) {
+	line.push(name);
+	return "Welcome, " + name + ". You are number " + line.length + " in line.";
+}
