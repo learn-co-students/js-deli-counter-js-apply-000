88913d4c847caf136a792104846eff9516fafb75
Sandy Yeung <sandynism@gmail.com>
2018-02-24 20:13:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
+//empty deli therefore empty array
 var katzDeli = []
 
-function takeANumber(katzDeliLine, person) {
-  katzDeliLine.push(person)
-  return (`Welcome, ${person}. You are number ${katzDeliLine.length} in line.`)
+function takeANumber(katzDeliLine, newCustomer) {
+  //add therefore PUSH new customer at the end
+  katzDeliLine.push(newCustomer)
+  //RETURN the new customer line and notify them of their order
+  return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`
 }
 
+//removes first person in current line aka SHIFT (GO OVER)
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
-  } else {
-    return (`Currently serving ${katzDeliLine.shift()}.`)
+  if (katzDeliLine.length > 1) {
+    // or if (katzDeliLine.length === 0) or (!katzDeliLine.length)
+  return `Currently serving ${katzDeliLine.shift()}.`
+} else {
+  return "There is nobody waiting to be served!"
   }
 }
 
-function currentLine(line) {
+//return current line in number order
+function currentLine(katzDeliLine) {
   let peopleArray = []
-  for (var i = 0; i < line.length; i++) {
-    peopleArray.push(`${i + 1}. ${line[i]}`)
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    //PUSH new customer to the end of current line as long as there is one or more
+    peopleArray.push(`${i + 1}. ${katzDeliLine[i]}`)
   }
 
-  if (line.length === 0) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
+    //alert customers of current line position
     return (`The line is currently: ${peopleArray.join(", ")}`)
   }
-
 }
