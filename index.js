a292481b77abb0d5d24a9916e2948d1d1a818d6b
maxsmouha@gmail.com <maxsmouha@gmail.com>
2018-03-12 21:09:26 +0000

    Done.

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 
 
 function nowServing (katzDeliLine) {
-  
   if (katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!";
 } else {
@@ -21,10 +19,10 @@ function currentLine (katzDeliLine) {
   if (katzDeliLine.length > 0) {
       var array = [];
       for (let i = 0; i < katzDeliLine.length; i++) {
-            array.push(` ${i+1}. ${katzDeliLine[i]}`)
+            array.push(` ${i+1}. ${katzDeliLine[i]}`);
             }
-            return `The line is currently:${array.toString()}`
+            return `The line is currently:${array.toString()}`;
   } else {
-      return 'The line is currently empty.'
+      return 'The line is currently empty.';
   }
 }
