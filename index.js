0c3768fe78b5bd89dcbe84414bd2b73f337b6982
Hanif Warren <hwarrendev@gmail.com>
2018-03-26 12:14:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
 var katzDeliLine = [];
-katzDeliLine = ["Ada", "Grace", "Kent"];
+var newName = [];
+//katzDeliLine = ["Ada", "Grace", "Kent"];
 function takeANumber(katzDeliLine, newName){
-  if (katzDeliLine.length > 0)
-  {
-    var newNumber = katzDeliLine.length;
-    //if newName has more than 1 person
-    if (newName.length > 1)
-    {
-      var i = 0;
-      do  {
-      katzDeliLine.push(newName[i]);
-        i++;
-      }
-      while (i < newName.length) ;
-      
-      
-    }
-  katzDeliLine.push[newName];
-  var newPerson = "katzDeliLine[newNumber - 1]";
-  var message = "Welcome, " + newName + ". You are number " + (katzDeliLine.length + 1) + " in line";
-    return message;
-  }
-  message = "The line is currently empty";
- // return message;
-}
+  katzDeliLine.push(newName);
+  var linePosition = katzDeliLine.indexOf(newName) + 1;
+  
+  var message = "Welcome, " + newName + ". You are number " + linePosition + 
+  " in line.";
+  return message;
+ }
 
-function nowServing()
+function nowServing(katzDeliLine)
 {
   if (katzDeliLine.length > 0)
   {
- var removePerson = katzDeliLine[0];
+    //shift method returns the removed item
+    
+   var personNow = katzDeliLine.shift();
+    var message = "Currently serving " + personNow + ".";
+    return message;
+// var removePerson = katzDeliLine[0];
  
- var index = katzDeliLine.indexOf(removePerson);
- if (index > -1){
- katzDeliLine.splice(index, 1);
+ 
+ //var index = katzDeliLine.indexOf(removePerson);
+ //if (index > -1){
+ //katzDeliLine.splice(index, 1);
  
 
-  return "Currently serving " + (removePerson );
+  //return "Currently serving " + (removePerson );
  } 
+ else {
+     return "There is nobody waiting to be served!";
+ }
   }
-  return "There is nobody waiting to be served!";
-}
 
-//currentLine(){
-//
-  //
-  //message =
-  //return "The line is currently: " + 
+
+function currentLine(katzDeliLine)
+{
+
+if (katzDeliLine.length === 0){
+  var message = "The line is currently empty.";
+  return message;
+  }
   
-//}
+  if (katzDeliLine.length > 0){
+    var i = 0;
+    var pNumber = 1;
+    var messageString = [];
+    do {
+      //.shift() returns the person removed
+      var pPerson =katzDeliLine[i];
+      var messageSnip = " " + pNumber + ". " + pPerson;
+      
+    messageString.push(messageSnip);
+    i++;
+    pNumber++;
+       }
+       while (i < katzDeliLine.length);
+       messageString = "The line is currently:" + messageString;
+       return messageString;
+}
+}
