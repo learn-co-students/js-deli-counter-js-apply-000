aca9d9125d0c3fa884bc27076716eb2f41e855be
Jarret Bryan <jarret.bryan@gmail.com>
2018-03-29 14:33:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+//post 2018.3.29 technical interview, takeANumber works with the katzDeli array as a list of unique numerical identifiers
+//not integers, that will increment every time one is taken. name and strings for names are not used as parameters
+
 //setting this variable here isn't necessary - serves as a reminder
 //on how to use the array as a parameter
 //let katzDeli = [];
 
 //two parameters here: the array and a string that acts as the 'name'
-function takeANumber(katzDeli, name){
+/*function takeANumber(katzDeli, name){
   katzDeli.push(name);
   return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+*/
+let counter = 1;
+function takeANumber(katzDeli){
+  counter++;
+  katzDeli.push(counter);
+  return (`Now serving number ${counter}.`);
 }
 
 //why does the below actually shift that first element in addition to just return the string?
 //conditional statement, then defining statement
+//below is an example of 'defensive programming'
 function nowServing(katzDeli){
   if (katzDeli.length === 0){
     return (`There is nobody waiting to be served!`);
