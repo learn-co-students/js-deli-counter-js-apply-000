d0b4330898aa22959f162d5802f202c506fd198c
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-18 02:28:43 +0000

    Done.

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ var katzDeliLine = [];
 //Return: patron's position in line in plain english
 
 function takeANumber (katzDeliLine, patronName) {
-  
   //.push returns the new length of the array
-  //katzDeliLine.length is the same as the last patron's line position
-  
-  return `Welcome, ${patronName}. You are number ${katzDeliLine.push(patronName)} in line.`;
+  let katzDeliLineLength = katzDeliLine.push(patronName);
+  return `Welcome, ${patronName}. You are number ${katzDeliLineLength} in line.`;
 }
 
 
@@ -18,19 +16,19 @@ function takeANumber (katzDeliLine, patronName) {
 
 //nowServing function states the next patron to be served (first in line) and removes the individual from the line, or returns a string if the line is empty.
 //Parameters: katzDeliLine
-//Return: Person that is now being served (first in line) --- OR --- string stating the line is empty
+//Return: Person that is now being served (first in line) --- OR --- string stating the line is empty
 
 function nowServing (katzDeliLine) {
-  //check if there is a line. If there is no line, return string from lesson.
-  //lineEmpty will return true if line is empty
-  if ( lineEmpty(katzDeliLine) ) {
+  //check if there is a line. If there is no line, return string provided by the lesson instructions.
+  if ( katzDeliLine.length === 0 ) {
     return "There is nobody waiting to be served!";
   }
   
   //If there is a line, return string stating next patron and remove that individual from the line (katzDeliLine array)
   else {
-    //.shift removes first element from array AND returns the removed element from array.
-    return `Currently serving ${katzDeliLine.shift()}.`;
+    //.shift returns the element removed from the array.
+    let nextUp = katzDeliLine.shift();
+    return `Currently serving ${nextUp}.`;
   }
 }
 
@@ -38,56 +36,43 @@ function nowServing (katzDeliLine) {
 
 
 
-//currentLine function returns the current line of patrons.
-//Parameters: katzDeliLine
-//Return: String with list of patrons (including their position) --- OR --- string stating that the line is empty.
 function currentLine (katzDeliLine) {
-  
-  
-  
-  //Check if the the line has patrons
-  //lineEmpty will return true if line is empty
-  
-  if ( lineEmpty(katzDeliLine) ) {
+  //check if there is a line. If there is no line, return string provided by the lesson instructions.
+  if ( katzDeliLine.length === 0 ) {
     return "The line is currently empty.";
   }
-
-
-
-  //If line has patrons, form string with patron's and their location in line.
+  
+  
   else {
-
-    //create beginning of string to start sentence. The string will be added to by iterating through the katzDeliLine array.
-    var theLine = "The line is currently:";
-
-    //Adds the line position and patron name for each patron in the line.
-    //this is accomplished through concatanation using += assignment operator for each iteration.
-    for (var c = 0; c < katzDeliLine.length; c++) {
-      theLine += ` ${c + 1}. ${katzDeliLine[c]},`;
-    }
     
-    //removes the final comma in the string because list is over.
-    //.substring removes the last character in this instance
-    theLine = theLine.substring(0, theLine.length - 1);
+    //Use .reduce function to condense the katzDeliLine array into a string using an accumulator to hold the initial string ("The line is current: ") and the additional concatenated strings as .reduce iterates over the elements
+    //The output of the .reduce function will be saved to the currentLineString variable defined below
+    //the .reduce function requires an accumulator, elements (to iterate over) and index to match a patron with a line position 
+    
+    let currentLineString = katzDeliLine.reduce (function ( accumulator, item, index ) {
+  
+      //Assign this variable to hold the accumulator. Cannot return string to .reduce here becuase we need to add a ", " to all elements, except for the last patron/element in the line/array.
+      //index+1 is required because computers start from zeroth index and humans start from 1.
+      let runningLineString = accumulator + (index + 1) + ". " + item;
+      
+      //This if statement checks if this is the final element in the array. If it is not, then a ", " is added to fulfill mocha JS test in learn.co
+      if (index < katzDeliLine.length - 1 ) {
+        //", " is added to end of string using addition assignment
+        runningLineString += ", "; 
+      }  
+      
+      //return the current string so that .reduce can continue iterating  
+      return runningLineString;  
+      
+    }, "The line is currently: "); //String here is the initial value of the accumulator for .reduce
+    
+    //Return the entire string created by the .reduce function. This return will be presented when calling the currentLine function
+    return currentLineString;
+    
+    
+  } //ends else statement
+} //ends currentLine function
 
-    //return the string that contains the line with patrons and their positions.
-    return theLine;
-  }
-
-}
 
 
-
-
-
-
-
-//This function checks to see if there is a line (if katzDeliLine array is empty or not)
-//Returns true if the line is empty (array === 0)
-function lineEmpty (katzDeliLine) {
-
-  if (katzDeliLine.length === 0) {
-    return true;
-  }
-}
 
