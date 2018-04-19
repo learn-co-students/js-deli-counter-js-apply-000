599130965781fb8285712f8f67a9fe0b25cb6834
Ameer <ameerkabir2@gmail.com>
2018-04-19 00:52:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-function takeANumber(katzDeliLIne, name) {
-  console.log('welocme Ada ur Number 1')
+function takeANumber(katzDeli, name) {
+    
+    if (katzDeli.length > 0);
+      katzDeli.push(name);
+      return ('Welcome, ' + name + '. You are number ' + katzDeli.length + ' in line.')
+  }
+   takeANumber(katzDeli, 'Ada');
+   
+function nowServing(katzDeli) {
+  
+  
+  if(katzDeli.length=== 0) {
+    return 'There is nobody waiting to be served!'
+  } 
+  var result = katzDeli.shift()
+  
+  return  'Currently serving ' + result + ".";
+}
+
+nowServing()
+
+function currentLine(katzDeli) {
+  if(katzDeli.length ===0) {
+    return 'The line is currently empty.'
+  }else {
+    
+    for(let i = 1; i<=katzDeli.length; i ++){
+    return 'The line is  currently: ' +  [i].indexOf() + '.' + ' '  + katzDeli
+    }
+    
+  }
+  
   
 }
-takeANumber(Ada)
