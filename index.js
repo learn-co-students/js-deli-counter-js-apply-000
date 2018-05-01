b1ccdbc4abd7479fd0827b9d0c8ca51d02521386
Michael Ballard <ballard.michael9@gmail.com>
2018-05-01 17:11:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var katzDeli = [ ];
 
+// Destructive functions
+
 function takeANumber(line, person){
   line.push(person)
   return `Welcome, ${person}. You are number ${line.length} in line.`
@@ -26,3 +28,42 @@ function currentLine(line){
     return cleanStr
   }
 }
+
+
+/* non Destructive methods
+
+function takeANumber(line, person){
+  var newLine = [...line,person]
+  return `Welcome, ${person}. You are number ${newLine.length} in line.`
+}
+
+
+function nowServing(lineCurrent) {
+  if (lineCurrent.length === 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    var message = `Currently serving ${lineCurrent[0]}.`;
+    let newLine = lineCurrent.slice(1);
+    return message  
+    
+  }
+}
+
+function currentLine(line){
+  if(line.length === 0){
+    return "The line is currently empty."
+  } else {
+    var str = "The line is currently: "
+    var element = 0;
+    while( element < line.length){
+      str += `${element+1}.` + ` ${line[element]}, `
+    element++
+    }
+    var cleanStr = str.slice(0,-2)
+    return cleanStr
+  }
+}
+
+
+*/
+
