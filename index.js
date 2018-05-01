d95235de1f2c6a8bb573d56bb341d7c5e10cce45
Michael Ballard <ballard.michael9@gmail.com>
2018-05-01 12:55:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-function takeANumber(lineCurrent, newTicket)
-{
-  lineCurrent.push(newTicket)
-  var message = `Welcome, ${newTicket}. You are number ${lineCurrent.length} in line.`
-  return message
+var katzDeli = [ ];
+
+function takeANumber(line, person){
+  line.push(person)
+  return `Welcome, ${person}. You are number ${line.length} in line.`
+  
 }
 
-
-function nowServing(lineCurrent) {
-  
-  if (lineCurrent.length === 0){
+function nowServing(line){
+  if (line.length === 0 ){
     return "There is nobody waiting to be served!"
-  } else {
-    var message = `Currently serving ${lineCurrent[0]}.`
-    lineCurrent.shift()
-    return message
+  }else{
+    return `Currently serving ${line.shift()}.`
   }
 }
 
-function currentLine(line) {
-  if (line.length === 0){
+function currentLine(line){
+  if(line.length === 0){
     return "The line is currently empty."
   } else {
-    var str = "The line is currently: ";
-    
-    for (var i = 0 ; i < line.length; i++ ){
-      str += `${i+1}.` + ` ${line[i]}, `
+    var str = "The line is currently: "
+    for(var element = 0; element < line.length; element++){
+      str += `${element+1}.` + ` ${line[element]}, `
     }
-    
-   var cleanStr = str.slice(0,-2)
+    var cleanStr = str.slice(0,-2)
     return cleanStr
   }
 }
