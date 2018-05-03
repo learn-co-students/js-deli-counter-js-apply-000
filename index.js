2131cd10ad7352eee277dfc57784cffa7aa04cbc
Allegra <aukseallegra@gmail.com>
2018-05-03 17:38:15 +0000

    Done.

--- index.js
+++ index.js
@@ -23,17 +23,26 @@ function nowServing(katzDeli){
 function currentLine(line){
 
   var line = [];
-  i = 0;
+  var i = 0;
 
   while (i < katzDeli.length){
-  	line.push(i+1 + "." + katzDeli[i]);
+  	line.push(i+1 + ". " + katzDeli[i]);
   	i++;
   }
 
   if (katzDeli.length > 0) {
-  	return `The line is currently: ${line}`
+  	//return `The line is currently: ${line}`
+  	return "The line is currently: " + line + " ";
   } else{
    return "The line is currently empty."
   }
 }
+
+//nowServing(katzDeli);
+takeANumber(katzDeli, "Bill");
+takeANumber(katzDeli, "Jane");
+takeANumber(katzDeli, "Ann");
+//nowServing(katzDeli);
+// nowServing(katzDeli);
+// currentLine(katzDeli);
 
