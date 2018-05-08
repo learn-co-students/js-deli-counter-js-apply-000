428f03f51a1328b70dd03c068d789a494fe65ae7
Derek Silva <dereksilvamusic@gmail.com>
2018-05-08 01:39:27 +0000

    Done.

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ var takeANumber = function(katzDeli, newCustomer) {
   return ('Welcome, ' + newCustomer + '. You are number ' +  katzDeli.length + ' in line.')
 }
 
-function nowServing (katzDeliLine) {
+function nowServing (katzDeli) {
   
-  if(katzDeliLine.length===0) { 
+  if(katzDeli.length===0) { 
   return 'There is nobody waiting to be served!'; 
 }
  
-  var customer = katzDeliLine.shift(); 
+  var customer = katzDeli.shift(); 
   return "Currently serving " + customer + "."; 
 }
 
-var currentLine = function (katzDeliLine) {
+var currentLine = function (katzDeli) {
    
-  if (katzDeliLine.length===0) {
+  if (katzDeli.length===0) {
   return "The line is currently empty.";
 }
  
   var customerAndNumber = [];
-  for (var i=0; i<katzDeliLine.length; i++) {
-  customerAndNumber.push(i + 1 + ". " + katzDeliLine[i])
+  for (var i=0; i<katzDeli.length; i++) {
+  customerAndNumber.push(i + 1 + ". " + katzDeli[i])
     
 }
   return "The line is currently: " + customerAndNumber.join(", ");
