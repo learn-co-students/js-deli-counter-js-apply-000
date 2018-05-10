1b3e4e950dd6782fe78d23b6e83ed56187bdd31a
Duane Felton <dfelton2010@gmail.com>
2018-05-10 01:06:10 +0000

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return ('Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.');
+  return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-  return ('There is nobody waiting to be served!');
+  return 'There is nobody waiting to be served!';
   } else { 
     return 'Currently serving ' + katzDeliLine.shift(name) + '.';
 }
