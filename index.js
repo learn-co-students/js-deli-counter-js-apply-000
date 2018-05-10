8f60d7ff22d49ff62ee2642e3e5357f9abef7662
Shumei Huang <rose1264@gmail.com>
2018-05-10 16:11:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 function currentLine(line) {
+  const numbersAndNames = [];
+
   if (!line.length) {
     return "The line is currently empty.";
   }
-
-  const numbersAndNames = [];
-  let l = line.length;
-  
-  for (let i = 0; i < l; i++) {
+  for (let i = 0; i < line.length; i++) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`);
   }
-
   return `The line is currently: ${numbersAndNames.join(', ')}`;
 };
 
@@ -17,12 +14,10 @@ function nowServing(line) {
   if (!line.length) {
     return "There is nobody waiting to be served!";
   }
-
   return `Currently serving ${line.shift()}.`;
 };
 
 function takeANumber(line, name) {
   line.push(name);
-
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 };
