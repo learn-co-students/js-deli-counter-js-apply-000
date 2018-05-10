9dd2c0c7184bc0a68d10c8adfb55fec445ba3cc5
Shumei Huang <rose1264@gmail.com>
2018-05-10 16:43:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!";
+  }
+  return `Currently serving ${line.shift()}.`;
+}
+
 function currentLine(line) {
   const numbersAndNames = [];
 
@@ -8,16 +20,4 @@ function currentLine(line) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`);
   }
   return `The line is currently: ${numbersAndNames.join(', ')}`;
-};
-
-function nowServing(line) {
-  if (!line.length) {
-    return "There is nobody waiting to be served!";
-  }
-  return `Currently serving ${line.shift()}.`;
-};
-
-function takeANumber(line, name) {
-  line.push(name);
-  return `Welcome, ${name}. You are number ${line.length} in line.`;
-};
+}
