50aab6f9b8e56d5161f12d5d94ed924ec633f8b0
Duane Felton <dfelton2010@gmail.com>
2018-05-12 23:06:55 +0000

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function nowServing(katzDeliLine) {
 }
 }
 
-
 function currentLine(line){
 if(line.length === 0) {
   return 'The line is currently empty.';
@@ -24,6 +23,6 @@ var nameAndNumber = [];
 for (var i = 0; i<line.length; i++) {
   nameAndNumber.push(i+1 + '. ' + line[i]);
 }
-console.log('The line is currently: ' + nameAndNumber);
+
 return 'The line is currently: ' + nameAndNumber.join(', ');
 }
