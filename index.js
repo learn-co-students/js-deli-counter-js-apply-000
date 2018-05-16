a04560819bea4fcd9b9685efef888c74932d7481
jaredianmills <jaredianmills@gmail.com>
2018-05-16 18:41:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli = ['joe', 'sue', 'bob'];
+
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var serving = line.shift();
+    return `Currently serving ${serving}.`;
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var katzDeliLine = []
+    var i = 1
+    line.forEach(name => {
+      katzDeliLine.push(` ${i}. ${name}`)
+      i++
+    })
+    return `The line is currently:${katzDeliLine}`
+  }
+}
