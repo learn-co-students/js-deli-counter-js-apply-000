bc3374720e9bfd4baacc9ccf808e34ff8262e926
Duane Felton <dfelton2010@gmail.com>
2018-05-16 19:28:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var katzDeli = [];
 
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
+function takeANumber(line, name) {
+  line.push(name);
+  return 'Welcome, ' + name + '. You are number ' + line.length + ' in line.';
 }
 
-function nowServing(katzDeliLine) {
-  if (!katzDeliLine.length) {
+function nowServing(line) {
+  if (line.length === 0) {
   return 'There is nobody waiting to be served!';
   } else { 
-    return 'Currently serving ' + katzDeliLine.shift(name) + '.';
+    return 'Currently serving ' + line.shift(name) + '.';
 }
 }
 
 function currentLine(line){
-if (!line.length) {
+if (line.length === 0) {
   return 'The line is currently empty.';
 }
 
