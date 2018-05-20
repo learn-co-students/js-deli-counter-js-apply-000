f53f2abc0c840b0fdf891f5fe675d95639bd3769
Cindy Vo <yendai.vo@hotmail.com>
2018-05-20 19:27:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, newPerson) {
+  katzDeliLine.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var firstPerson = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${firstPerson}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var i;
+    for (i=0; i < katzDeliLine.length; i++) {
+      var lineString = "The line is currently: ";
+      
+      lineString = `${lineString} ${(i+1)}. ${katzDeliLine[i]},`;
+    }
+    lineString = lineString.substring (0, lineString.length - 1);
+    return lineString;
+  }
+}
