8aac9097e415fe51fb26e170bfd15ce481a2979b
bobjin42 <37715520+bobjin42@users.noreply.github.com>
2018-05-21 10:06:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-var katzDeliLine = [];
-
-
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  } else {
-   var index = katzDeliLine.indexOf(name) + 1;
-}
+function takeANumber(arr, name){
+  arr.push(name);
+  var index = arr.indexOf(name) + 1;
   return "Welcome, " + name + ". You are number " + index + " in line."
 }
 
-function nowServing(katzDeliLine){
-  if(katzDeliLine.length === 0){
+
+function nowServing(arr){
+  if(arr.length === 0){
     return "There is nobody waiting to be served!";
   } else {
-    var servedName = katzDeliLine.shift();
+    var servedName = arr.shift();
   }
   return "Currently serving " + servedName + "." 
 }
 
-function currentLine(katzDeliLine){
-  if(katzDeliLine.length === 0){
+function currentLine(arr){
+  if(arr.length === 0){
     return "The line is currently empty."
   }else{
     var newArr = [];
-    for(var i = 0; i < katzDeliLine.length; i++){
-      var ele = katzDeliLine[i];
+    for(var i = 0; i < arr.length; i++){
+      var ele = arr[i];
       var pushEle = (i + 1) + ". " + ele;
       newArr.push(pushEle);
     }
