7cecd6b6d4958acf0fdc1e67f288a9625102a9a7
wangyan6783 <37424160+wangyan6783@users.noreply.github.com>
2018-05-21 15:24:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var katzDeli = [];
 var otherDeli = ["Steven", "Blake", "Avi"];
 
-function takeANumber(array, string) {
-  array.push(string);
-  var index = array.indexOf(string) + 1;
-  return "Welcome, " + string + ". You are number " + index + " in line.";
+function takeANumber(array, name) {
+  array.push(name);
+  var number = array.length;
+  return "Welcome, " + name + ". You are number " + number + " in line.";
 }
 
 function nowServing(array) {
@@ -21,11 +21,11 @@ function currentLine(array) {
   if (array.length === 0) {
     return "The line is currently empty.";
   }
-  var line = "The line is currently: ";
+  var currentLine = "The line is currently: ";
   for (var i = 0; i < array.length; i++) {
     var number = i + 1;
     var name = array[i];
-    line += number + ". " + name + ", ";
+    currentLine += number + ". " + name + ", ";
   }
-  return line.slice(0, -2);
+  return currentLine.slice(0, -2);
 }
