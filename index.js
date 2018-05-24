0b2488ccf599ce8c5d974acf7547d25ec3a079be
jaredianmills <jaredianmills@gmail.com>
2018-05-24 13:50:29 +0000

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var katzDeli = [];
 
 function takeANumber(line, name) {
   line.push(name);
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
 function nowServing(line) {
   if (line.length === 0) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   } else {
     let serving = line.shift();
     return `Currently serving ${serving}.`;
@@ -16,14 +16,14 @@ function nowServing(line) {
 
 function currentLine(line) {
   if (line.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   } else {
-    let katzDeliLine = []
-    let i = 1
+    let katzDeliLine = [];
+    let i = 1;
     line.forEach(name => {
-      katzDeliLine.push(` ${i}. ${name}`)
+      katzDeliLine.push(` ${i}. ${name}`);
       i++
     })
-    return `The line is currently:${katzDeliLine}`
+    return `The line is currently:${katzDeliLine}`;
   }
 }
