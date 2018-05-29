5cfdd7bf901c979c31de1d719c31299912d4160f
Ronald nunez <ronaldcuello11@gmail.com>
2018-05-29 23:57:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function takeANumber(line, name){
-  line.push(name);
-  return `Welcome, ${name}. You are number ${line.length} in line.`
-}
-function nowServing(line) {
-  if (line.length > 0) {
-    return `Currently serving ${line.shift()}.`
-  } else {
-    return "There is nobody waiting to be served!";
-  }
+var katzDeli = [];
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
- 
+function nowServing (katzDeliLine){
+  if (katzDeliLine.length === 0){
+  return `There is nobody waiting to be served!`;
+  }    else {
+  return `Currently serving ${katzDeliLine.shift()}.`; 
+}
+}
 
-function currentLine(katzDeli) {
-  var line = [];
-  for (let i = 0; i < katzDeli.length; i++) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
+function currentLine(katzDeli){
+  var array = [];
+  for (let i=0; i < katzDeli.length; i++){
+array.push(` ${i+1}. ${katzDeli[i]}`)
   }
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
-} 
+  } else return `The line is currently:${array}`;
+  
+}
