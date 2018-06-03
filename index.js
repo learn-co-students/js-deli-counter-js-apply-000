49ce295e6eea72f36aedfd912b0e29fbff6ebcbe
Christopher Calle <christophercalle@gmail.com>
2018-06-03 03:20:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 var katzDeliLine = [];
 
-function currentLine(x) {
-    var line = []
-    if (x.length === 0) {
-      return "The line is currently empty."
-    } else {
-      for(var i = 0; i < x.length; i++) {
-        line += (i + 1) + ". " + x[i] + ", "
-      }
-      line = line.slice(0, line.length-2)
-      return "The line is currently: " + line
-    }
-}
+ /****************** takeANumber **************************/
+
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
-function nowServing(x) {
+
+
+
+ /******************* nowServing *************************/
+ 
+ function nowServing(x) {
   if (x.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
@@ -25,3 +20,20 @@ function nowServing(x) {
     return "Currently serving " + name + ".";
   }
 }
+ 
+ 
+  /****************** currentLine ************************/
+  
+  function currentLine(x) {
+    var line = []
+    if (x.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for(var i = 0; i < x.length; i++) {
+        line += (i + 1) + ". " + x[i] + ", "
+      }
+      
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
+    }
+  }
