6e60ddf8c02c265a50b500fac8800e88b657ff99
Cheryl Kelson <cherylkelson@icloud.com>
2018-06-05 13:45:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+  var katzDeli = [];
+  function currentLine (katzDeli){
+     if (katzDeli.length ===0);
+     return "The line is currently empty.";
+  } 
+  function takeANumber(line, name) {
+    takeANumber(katzDeliLine, "Ada");
+    takeANumber(katzDeliLine, "Grace");
+    takeANumber(katzDeliLine, "Kent");
+    takeANumber(katzDeliLine, "Matz");
+    
+    line.push(name);
+    console.log("")
+  }
+  
+  var currentLineName [];
+  var lineNumber = [];
+  var lineNamesandNumbers = [];
+   for(var i= 0; i <katzDeliLine.length; i++);{
+     lineNamesandNumbers
+     
+  
+  function nowServing(line) {
+    if(!line.lengrth) {
+      console.log("There is nobody waiting to be served!")
+      return "there is nobody waiting to be served!"
+    } else {
+      return "TheLine is currently serving" + line;
+    }
+    
+  
+   
+
+  
