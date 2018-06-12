a9d14c95e560df4811fe98239e50050e52332e54
bwadlow <bwadlow@gmail.com>
2018-06-12 10:00:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-let katzDeli=[]
 function takeANumber (line, name) {
   line.push(name)
-  return 'Welcome, ' + name +'.' + ' You are number ' + line.length + ' in line.'
+  return `Welcome, $(name}. You are number ${line.length} in line.`
 }
 
 function nowServing(line) {
-  if(!line.length) return 'There is nobody waiting to be served!'
-  return 'Currently serving ' + line.shift() + '.'
+ return !line.length ? 'There is nobody waiting to be served!' : 'Currently serving ' + line.shift() + '.'
 }
 
  function currentLine(line) {
