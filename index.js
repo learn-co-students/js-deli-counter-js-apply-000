f764de1c4084bff285c90c8ad5425600fb803ee5
Terry Foley <terry.foley555@gmail.com>
2018-06-15 06:10:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 
+
 function takeANumber(line, name){
   line.push(name);
   return "Welcome, " + name + ". You are number " + line.length +
   " in line."
 }
-takeANumber(katzDeli, "Ada")
+
+function nowServing(line){
+  if (line.length < 1){
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    return "Currently serving " + line.shift() + ".";
+  }
+}
+
+function currentLine(line){
+  var finalLine = [];
+  if (line.length < 1){
+    return "The line is currently empty.";
+  }
+  else {
+    for (var i=0; i < line.length; i++){
+      finalLine += (i+1) + ". " + line[i] + ", ";
+    }
+    finalLine = finalLine.slice(0, finalLine.length-2)
+  }
+  return "The line is currently: " + finalLine;
+}
