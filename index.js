2e23e8546e10947edcdea3d991eaa2593970ecf2
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-06-19 18:01:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 var katzDeli = [];
 function takeANumber(katzDeli,name){
  katzDeli.push(name);
- 
-  return "Welcome, "+name+". You are number "+ katzDeli.length +" in line.";
+ var number=katzDeli.indexOf(name)+1;
+ return `Welcome, ${name}. You are number ${number} in line.`;
 }
 
 function nowServing(katzDeli){
   if (katzDeli.length>0){
-    
-    var cus =katzDeli[0];
+    var customer =katzDeli[0];
     katzDeli.shift();
-    return "Currently serving "+cus+".";
-  } else {
-    return "There is nobody waiting to be served!"
+    return `Currently serving ${customer}.`;
+   } else {
+    return "There is nobody waiting to be served!";
   }
 }
 
 function currentLine(katzDeli){
-  var n = katzDeli.length
-  var cusandnum=[]
-  var i=0
-  if( katzDeli.length===0){
-    return "The line is currently empty."
-  } 
-
- 
-  for (i=0;i<n;i++){
-  cusandnum.push( i+1+". "+katzDeli[i])}
-  return "The line is currently: "+ cusandnum.join(", ") ;
-    
+  var empty=[];
+    if( katzDeli.length>0){
+       for (var i=0;i<katzDeli.length;i++){
+       empty.push((i+1)+". " +katzDeli[i]);
+       }
+       return `The line is currently: ${empty.join(", ")}`;
+     }else {
+      return "The line is currently empty.";
+    }
   }
