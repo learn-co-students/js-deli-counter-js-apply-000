2e8ea2aab9d902f2e8e7d1e1aa4a8892c2828a5e
Jessica Long <jessicalong627@gmail.com>
2018-06-26 14:11:12 +0000

    Done.

--- index.js
+++ index.js
@@ -4,18 +4,31 @@
 // whats my output? returning a position in line
 
 //create skeleton function
-//
+//add two parameters
+//add user input (name) to the line
+//do length instead of index 
+// return a statement
 
 //2. Build a function `nowServing`. This function should return the first person in line and then remove that individual from the lineIf there is nobody in line, it should return "There is nobody waiting to be served!"
 // whats my input? the line
 // whats my output? the first person or nobody
 
+//create skeleton function
+//add 1 parameters
+//handle if there is no one in line
+//handle else statement that returns person and removes.(shift())?
 
 //3. Build a function `currentLine` that returns the current line. For example, if `katzDeliLine` is currently `["Ada", "Grace"]`, `currentLine(katzDeliLine)` would return `"The line is currently: 1. Ada, 2. Grace"`.  If there is nobody in line, it should return `"The line is currently empty."`
 // whats my input? the line
 // whats my output?  either empty or the current line
+//create skeleton and add one parameter
+//first handle (if) no one is in line
+//next handle looping through the line to get the Names
+//figure out how to get number and name 
+//return the new array with new message 
 
-katzDeliLine=[];
+
+let katzDeliLine=[];
 
 function takeANumber(katzDeliLine, name) {
    katzDeliLine.push(name)
@@ -36,10 +49,13 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty.";
   }
   else {
-     var currentLineTwo = [];
+    let currentLineTwo= [];
      for(let i=0; i<katzDeliLine.length; i++) {
      currentLineTwo.push(`${i+1}. ${katzDeliLine[i]}`) 
+     
   }
+    console.log(currentLineTwo)
    return `The line is currently: ${currentLineTwo.join(", ")}`
   }
+
 }
