22d860b3e61e91d898a633808818e098ccd0f6c9
Jessica Long <jessicalong627@gmail.com>
2018-06-26 03:11:46 +0000

    Done.

--- README.md
+++ README.md
@@ -44,6 +44,4 @@ currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz
 nowServing(katzDeliLine); // "Currently serving Grace."
 
 currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
-```
 
-<p data-visibility='hidden'>View <a href='https://learn.co/lessons/js-deli-counter' title='Deli Counter Lab'>Deli Counter Lab</a> on Learn.co and start learning to code for free.</p>
--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 //1. Build a function that a new customer will use when entering the deli. The function, `takeANumber`, should accept the current line of people, `katzDeliLine`, along with the new person's name as parameters. The function should return their position in line. And don't go being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
 
-//2. Build a function `nowServing`. This function should return the first person in line and then remove that individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!"
+// whats my input? A current line of people and a name to add
+// whats my output? returning a position in line
+
+//create skeleton function
+//
+
+//2. Build a function `nowServing`. This function should return the first person in line and then remove that individual from the lineIf there is nobody in line, it should return "There is nobody waiting to be served!"
+// whats my input? the line
+// whats my output? the first person or nobody
+
 
 //3. Build a function `currentLine` that returns the current line. For example, if `katzDeliLine` is currently `["Ada", "Grace"]`, `currentLine(katzDeliLine)` would return `"The line is currently: 1. Ada, 2. Grace"`.  If there is nobody in line, it should return `"The line is currently empty."`
+// whats my input? the line
+// whats my output?  either empty or the current line
 
-var katzDeli = [];
+katzDeliLine=[];
 
 function takeANumber(katzDeliLine, name) {
    katzDeliLine.push(name)
-   return "Welcome, " + name +". You are number " + katzDeliLine.length + " in line."
+   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
-  
-  if(katzDeliLine.length === 0) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
   else {
-    return "Currently serving " + katzDeliLine.shift() +"."
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
-}
+ }
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
   else {
-    var currentLineTwo = [];
-     for(var i=0; i<katzDeliLine.length; i++) {
-    currentLineTwo .push(i+1 + ". "+ katzDeliLine[i]) ;
+     var currentLineTwo = [];
+     for(let i=0; i<katzDeliLine.length; i++) {
+     currentLineTwo.push(`${i+1}. ${katzDeliLine[i]}`) 
   }
-  return "The line is currently: " + currentLineTwo.join(", ")
+   return `The line is currently: ${currentLineTwo.join(", ")}`
   }
 }
