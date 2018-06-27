397d9b6acaa67143f33a6dbee5d01ac791ba7af7
Trevor Jameson <tjameson15@gmail.com>
2018-06-27 14:26:55 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-  return `Currently serving ${line.shift()}.`;
+    return `Currently serving ${line.shift()}.`;
   }
 }
 
