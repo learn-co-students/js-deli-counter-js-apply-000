711573c1eb981e395af92e473f4963637bb59bd4
Roberto Busó-García <robertobuso@gmail.com>
2018-07-02 19:30:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
-    } else {
-        return `Currently serving ${katzDeliLine.shift()}.`;
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
 function currentLine (katzDeliLine) {
+  var resultArray = [];
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-      var i = 0;
-      while (i<katzDeliLine.length){
-      katzDeliLine[i] = ` ${i+1}. ${katzDeliLine[i]}`;
+    var i = 0;
+    while (i < katzDeliLine.length){
+      resultArray.push(` ${i+1}. ${katzDeliLine[i]}`);
       i++;
-}
-        return `The line is currently:${katzDeliLine}`; 
+    }
+    return `The line is currently:${resultArray}`; 
   }
 }
+
