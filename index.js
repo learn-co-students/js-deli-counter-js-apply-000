6b3c7d1e78a4991419eb066ad7dcff8bf9c4fcbb
Amanda-LearnCo <shottsan@dukes.jmu.edu>
2018-07-03 12:04:53 -0700

    Done.

--- index.js
+++ index.js
@@ -28,7 +28,35 @@ if (linepos.length === 0) {
 function currentLine (linepos) {
   for (var i = 0; i < linepos.length; i++) {
   var list = 1;
-    return `The line is currently: ${list++}. ${linepos[i]},`}
+    return `The line is currently: ${list++}. ${linepos[i]},`
+    }   
+    return `The line is currently empty.` 
 }
-    else {
-        return `The line is currently empty.` }
+
+
+
+
+
+
+function currentLine(line) {
+if (!line.length) {
+    return "The line is currently empty."
+}
+const numbersAndNames = []
+for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+}
+return `The line is currently: ${numbersAndNames.join(', ')}`;
+}
+
+function nowServing(line) {
+if (!line.length) {
+    return "There is nobody waiting to be served!";
+}
+return `Currently serving ${line.shift()}.`;
+}
+
+function takeANumber(line, name) {
+line.push(name);
+return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
