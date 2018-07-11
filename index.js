23336bd380989a026ddb65a7aa0e1cc35fd653e1
Obi Marcus Onuora <obimarcus11@gmail.com>
2018-07-11 12:03:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var katzdeli = []
-
+var numbersAndNames = []
 function currentLine(line) 
   {
   if (!line.length) {
     return "The line is currently empty."
   }
 
-  const numbersAndNames = []
-
+  
   for (let i = 0, l = line.length; i < l; i++) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
   }
