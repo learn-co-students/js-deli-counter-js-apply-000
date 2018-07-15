548fd99ae35fe54c475ef74bebeeb8ca3b8163f6
Amanda <40045156+amandashotts@users.noreply.github.com>
2018-07-15 14:47:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var katzDeliLine = [''];
 // Build a function that a new customer will use when entering the deli. The function, takeANumber, should accept the current line of people, katzDeliLine, along with the new person's name as parameters. The function should return their position in line. And don't go being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
 function takeANumber(line, name) {
     line.push(name);
-    return `Welcome, ${name}. You are number ${line.length} in line. \n`;
+    return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
 //Build a function nowServing. This function should return the first person in line and then remove that individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!"
@@ -22,22 +22,20 @@ function nowServing (line) {
 
 //Build a function currentLine that returns the current line. For example, if katzDeliLine is currently ["Ada", "Grace"], currentLine(katzDeliLine) would return "The line is currently: 1. Ada, 2. Grace". If there is nobody in line, it should return "The line is currently empty."
 function currentLine (line) {
-    let numberedLines = [];
-  for (i = 0; i !== line.length; i++) {
+let numberedLines = [];
+  for (var i = 0; i < line.length; i++) {
     numberedLines.push(`${i+1}. ${line[i]}`);
-    } 
-return `The line is currently: ${numberedLines.join(', ')}.`
-    if (line.length === 0) {
-        return `The line is currently empty.` 
-    }
-} 
-
-
+  } 
+  if (line.length === 0) {
+    return `The line is currently empty.` 
+  }
+ else {
+    return `The line is currently: ${numberedLines.join(', ')}`
+  }
+}
 
 /* 
-
 An alternative way to build currentLine:
-
 function currentLine(line) {
 if (!line.length) {
     return "The line is currently empty."
@@ -48,15 +46,10 @@ for (let i = 0, l = line.length; i < l; i++) {
 }
 return `The line is currently: ${numbersAndNames.join(', ')}`;
 } 
-
-
-
 Testie Testie
-
 console.log(takeANumber(katzDeliLine, 'Declan'))
 console.log(nowServing(katzDeliLine))
 console.log(currentLine(katzDeliLine))
 console.log(nowServing(katzDeliLine))
 console.log(currentLine(katzDeliLine))
-
 */
