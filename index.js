d754ab6d9284c00eebf6a29b0de2e075cde3aef4
Benjamin Lee <nycxbl@hotmail.com>
2018-07-16 02:08:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
-}
-
-function nowServing(katzDeliLine){
-  if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-  else {
-    return "Currently serving " + katzDeliLine.shift() + ".";
-  }
-}
-
-function currentLine(katzDeliLine){
-  var print = "The line is currently: ";
-  if (katzDeliLine.length === 0)
-  {
+function currentLine(line) {
+  if(!line.length) {
     return "The line is currently empty.";
   }
-  else{
-    for(let i=0; i<katzDeliLine.length; i++)
-    {
-      print = print + (i+1) + ". " + katzDeliLine[i];
-      if (i<katzDeliLine.length-1)
-      {
-      print = print + ", ";
-      }
-    }
-  return print;
+  
+  var numbersAndNames = [];
+  for (let i = 0; i < line.length; i++) {
+    numbersAndNames.push(`${i+1}. ${line[i]}`);
   }
+  return `The line is currently: ${numbersAndNames.join(", ")}`;
+}
+
+function nowServing(line){
+  if(!line.length) {
+    return "There is nobody waiting to be served!";
+  }
+  return `Currently serving ${line.shift()}.`;
+}
+
+function takeANumber(line, name) {
+  line.push(name);
+  
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
