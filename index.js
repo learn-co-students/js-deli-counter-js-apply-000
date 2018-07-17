4a5c7271253a4eab0f013d4d689c2cc00292290b
Siddeshwar Gauncar <sddshwr@gmail.com>
2018-07-17 21:33:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var takeANumber = function (katzDeli, newCustomer) {
+  if (katzDeli.length > 0);
+  katzDeli.push(newCustomer);
+  return "Welcome, " + newCustomer + ". You are number " + katzDeli.length + " in line."
+}
+
+function nowServing (katzDeliLine) {
+   if(katzDeliLine.length===0) {
+   return 'There is nobody waiting to be served!';
+}
+   var customer = katzDeliLine.shift();
+   return "Currently serving " + customer + ".";
+}
+
+var currentLine = function (katzDeliLine) {
+  if (katzDeliLine.length===0) {
+  return "The line is currently empty.";
+}
+   var customerAndNumber = [];
+   for (var i=0; i<katzDeliLine.length; i++) {
+   customerAndNumber.push(i+1 + ". " + katzDeliLine[i])
+}
+  return "The line is currently: " + customerAndNumber.join(", ");
+}
