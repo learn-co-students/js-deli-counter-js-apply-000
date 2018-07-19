f430d19684a12818763b148c9f0806c08d2a42f5
Tiffany Choong <t.choong@yahoo.com>
2018-07-19 15:15:20 +0000

    Done.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function currentLine(line){
   }
   var newLine = [];
   for (let i=0; i<line.length; i++){
-    newLine.push(` ${numbers[i]}. ${line[i]}`);
+    newLine.push(`${numbers[i]}. ${line[i]}`);
     
   }
     return `The line is currently: ${newLine}`;
@@ -35,17 +35,3 @@ function currentLine(line){
   }
 }
 
-
-
-
-
-  describe('currentLine(line)', () => {
-    it('returns "The line is currently empty." if no one is in line', () => {
-      expect(currentLine([])).toEqual("The line is currently empty.");
-    });
-
-    it('says who is in line when there are people waiting', () => {
-      expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
-    });
-  });
-})
