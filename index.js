22cbed8cbc15fbf2645a066f9f98b5851923c843
Tiffany Choong <t.choong@yahoo.com>
2018-07-19 15:14:02 +0000

    Done.

--- index.js
+++ index.js
@@ -16,3 +16,36 @@ function nowServing (Deli){
   }
 }
 
+function currentLine(line){
+  var numbers = [];
+  var n = 0;
+  if(line.length>0){
+  while (n<line.length){
+    n = n+1;
+    numbers.push(n);
+  }
+  var newLine = [];
+  for (let i=0; i<line.length; i++){
+    newLine.push(` ${numbers[i]}. ${line[i]}`);
+    
+  }
+    return `The line is currently: ${newLine}`;
+  } else {
+    return "The line is currently empty.";
+  }
+}
+
+
+
+
+
+  describe('currentLine(line)', () => {
+    it('returns "The line is currently empty." if no one is in line', () => {
+      expect(currentLine([])).toEqual("The line is currently empty.");
+    });
+
+    it('says who is in line when there are people waiting', () => {
+      expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
+    });
+  });
+})
