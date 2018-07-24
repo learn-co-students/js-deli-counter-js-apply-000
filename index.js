282708187b22f0c0e25e5a8c49da0ab62f488a2e
nouran.nadhem@gmail.com Nadhem Haider <nouran.nadhem@gmail.com>
2018-07-24 05:02:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-var takeANumber = function (katzDeli, newCustomer) {
+function takeANumber  (katzDeli, newCustomer) {
   if (katzDeli.length > 0);
   katzDeli.push(newCustomer);
   return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
   }
   
-  
-  function nowServing (katzDeliLine) { 
-    if(katzDeliLine.length===0) { 
+function nowServing (katzDeliLine) { 
+  if(katzDeliLine.length===0) { 
       return 'There is nobody waiting to be served!'; 
     }
   var customer = katzDeliLine.shift(); 
   return "Currently serving " + customer + "."; 
   }
   
-  
-  var currentLine = function (katzDeliLine) {
-    if (katzDeliLine.length===0) {
+function currentLine (katzDeliLine) {
+   if (katzDeliLine.length===0) {
       return "The line is currently empty."; 
       }
    var customerAndNumber = [];
