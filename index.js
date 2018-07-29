652b805059d568cac913fc79c5e79842fab0f4ea
lilianasrmartins@gmail.com <lilianasrmartins@gmail.com>
2018-07-29 23:34:39 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const LondonDeliLine = [];
 function takeANumber(LondonDeliLine,name) {
   LondonDeliLine.push(name)
   return `Welcome, ${name}. You are number ${LondonDeliLine.length} in line.`
-}
+};
 
 function nowServing(LondonDeliLine) {
   if (LondonDeliLine.length === 0) {
@@ -11,19 +11,20 @@ function nowServing(LondonDeliLine) {
   } else {
   return `Currently serving ${LondonDeliLine.shift()}.`
   }
-}
+};
 
 function currentLine(LondonDeliLine) {
   if (LondonDeliLine.length === 0) {
     return "The line is currently empty."
+  };
+
+  const currentQueue = []
+
+  for (let i = 0; i < LondonDeliLine.length; i++) {
+    currentQueue.push(`${i + 1}. ${LondonDeliLine[i]}`)
   }
-  
-  const currentQueue = [];
 
-  for (let queueIndex = 0; LondonDeliLine.length; queueIndex++) {
-    currentQueue.push(`${queueIndex +1} ${LondonDeliLine[queueIndex]}`)
-    }
+  return `The line is currently: ${currentQueue.join(', ')}`
+};
 
-    return `The line is currently ${currentQueue.join(',')}`
 
-}
