73eed1e1b79549deb7b8472a12848e9b52ca188e
Olabode <oaadimul@gmail.com>
2018-08-01 02:41:02 +0000

    Done.

--- index.js
+++ index.js
@@ -9,4 +9,16 @@ return "Welcome, " + name +"You are number "+ position+" in line."
 function nowServing(deliLine) {
   var name;
   
+  if(deliLine.length ==+ 0) {
+    return "There is nobody waiting to be served!"
+  }
+name = deliLine.shift()
+return "Currently serving "+ name + "."
 }
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  
+    }
