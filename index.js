6980f57dc57328477f2ce3caf72c86927b60daa7
Phat Doan <phatdoan@yahoo.com>
2018-08-01 23:51:09 +0000

    Done.

--- index.js
+++ index.js
@@ -26,28 +26,24 @@ function nowServing(line) {
 }
 
 function currentLine(line) {
-	var lineup = "";
 	var retString = "";
-
-    if (line.length === 0) {
-    	retString = "The line is currently empty.";
+  if (line.length === 0) {
+      retString = "The line is currently empty."
+  }
+  else {
+    var fullString = "";
+    for (var i=0; i<line.length; i++) {
+      var person = line[i];
+      var position = i+1 ;
+      var miniString = position + ". " + person;
+      if (position === line.length) {
+        fullString = fullString + miniString;
+        }
+        else {
+          fullString = fullString + miniString + ", ";
+        }
     }
-    else {
-    	var peopleInLine = "";
-    	// split the final string in half:
-    		// 1st half="The line is currently:"
-    		// 2nd half: "1. Ada, 2. Grace"
-		for (var i=0; i<line.length; i++) {
-		// loop through the line, stop when i is equal to the length of the line
-			var numberInLine = i + 1; // this is the "1." or the "2."
-			var person = "";
-			person = numberInLine + ". " + line[i]; // this looks like "1. Ada," or "2. Grace"
-			if (i+1 !== line.length) {
-				person = person + ", "; // only add the comma after if it isn't the last person
-			}
-			peopleInLine = peopleInLine + person;
-		}
-		retString = "The line is currently: " + peopleInLine; // build the 2nd half of the final string
-	}
-  return retString;
+    retString = "The line is currently: " + fullString
+  }
+  return retString
 }
