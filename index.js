ee8c93e7ff1c18d25254929b25d371147565e6b6
Olabode <oaadimul@gmail.com>
2018-08-01 02:28:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function takeANumber(katzDeliLine, name) {
+  var position = katzDeliLine.length+1
+  katzDeliLine.push(name)
+return "Welcome, " + name +"You are number "+ position+" in line."
+  
+}
+
+function nowServing(deliLine) {
+  var name;
+  
+}
