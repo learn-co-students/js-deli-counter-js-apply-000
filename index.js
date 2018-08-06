d367ee4dd4dc804c8b82a94bf092f38b86306437
Ben Storey <benstorey0@gmail.com>
2018-08-06 10:07:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(queue, name){
+  queue.push(name)
+  return "Welcome, " + name + ". You are number " + (queue.length) + " in line."
+}
+
+function nowServing(queue){
+  if (queue.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  return "Currently serving " + queue.shift() + "."
+}
+
+function currentLine(queue){
+  var startStr = "The line is currently"
+
+  if (queue.length == 0){
+    return startStr + " empty."
+  }
+
+  startStr = startStr + ": "
+  for (var i=0; i<queue.length;i++){
+    startStr = startStr + (i+1) + ". " + queue[i] +", "
+  }
+  return startStr.slice(0,startStr.length-2) //removes last comma, <adding new if
+}
