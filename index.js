119e9ac9fa53d749d617f561b18620d10ec0d54e
neaty.soopaul@gmail.com <neaty.soopaul@gmail.com>
2018-08-08 08:25:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeliLine = [];
+
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(`${name}`);
+  return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
+}
+
+function nowServing (katzDeliLine) {
+  let i = 0;
+  while (i < katzDeliLine.length) {
+    i++;
+  }
+  
+  if (katzDeliLine.length === 0) {
+return 'There is nobody waiting to be served!';
+  }
+ else
+ return 'Currently serving ' + katzDeliLine.shift() + '.';
+}
+
+function currentLine (katzDeliLine) {
+  let line = 'The line is currently: ';
+ let x = [];
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    x.push(`${i+1}. ${katzDeliLine[i]}`);
+  }
+  
+if (katzDeliLine.length === 0) {
+  return 'The line is currently empty.';
+} else
+return line + x.join(", ");
+}
