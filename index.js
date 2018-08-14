90216f4af84310c94a8e3ab2ee996671816ff276
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-14 14:02:39 +0000

    Done.

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var katzDeli = [];
 
 
 // function takeANumber accepts two parameters katzDeli and customerName, it checks using if statement if the katzDeli array is empty (katzDeli.length > 0). If the array is not empty it pushes the argument for parameter customerName to the end of the katzDeli array and returns the following string `Welcome ${customerName}. You are number ${katzDeli.length} in line.`
-function takeANumber(katzDeli, customerName) {
-  if (katzDeli.length > 0);
-  katzDeli.push(customerName);
-  return (`Welcome, ${customerName}. You are number ${katzDeli.length} in line.`)
+function takeANumber(line, customerName) {
+  if (line.length > 0);
+  line.push(customerName);
+  return (`Welcome, ${customerName}. You are number ${line.length} in line.`)
 };
 
 //function nowServing accepts one parameter katzDeli. The function body creates a counter variable i and assigns it value 0. A while loop runs as long as i < katzDeli.length (we are using while loop because we might not know how long the array is and want to it run as long as the array is not empty). Each iteration of the loop increments the counter variable by 1. The loop body also uses an if statement to check if the katzDeli is empty. If yes it returns a string "There is nobody waiting to be served!". Else it returns the `Currently serving ${katzDeli.shift()}.` The first element of katzDeli array is removed from the array.
