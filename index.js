08344208125b829c3605fa2975c1fca09d21bbe5
sydneygrant <sgrant@nyu.edu>
2018-08-15 15:59:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeliLine = [];
+var count = 0;
+
+function takeANumber(currentLine, newPerson){
+
+  return "Welcome, " + newPerson + ". You are number " + (katzDeliLine.length + 1) + " in line.";
+}
