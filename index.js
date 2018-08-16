0d5ce1363e7ed0a7dae83c28104c4c6b6d7f9bb3
sydneygrant <sgrant@nyu.edu>
2018-08-16 03:16:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 var katzDeliLine = [];
 
-function takeANumber(currentLine, name){
-  var placeInLine = currentLine.length + 1;
-  var str = "Welcome, " + name + ". You are number " + placeInLine + " in line.";
-  currentLine.push(name);
-  return str;
+function takeANumber(line, name){
+  var spot = line.length + 1;
+  line.push(name);
+  return `Welcome, ${name}. You are number ${spot} in line.`;
+
 }
+
 
 // 2. Build a function `nowServing`. This function should accept the current line of people (`katzDeliLine`)
 // and return the first person in line and then remove that individual from the line. If there is nobody in
 // line, it should return "There is nobody waiting to be served!"
 
-function nowServing(currentLine){
-  if (currentLine.length == 0){
+function nowServing(line){
+  if (line.length == 0){
     return "There is nobody waiting to be served!";
   } else {
-    var current = currentLine[0];
-    currentLine.splice(0, 1);
-    return "Currently serving " + current + ".";
-  }
+    var str = `Currently serving ${line[0]}.`;
+    line.splice(0, 1);
+  } return str;
 }
+
 
 // 3. Build a function `currentLine` that accepts the current line of people and returns the current line as
 // a string; for example, if 'katzDeliLine' is currently `["Ada", "Grace"]`, `currentLine(katzDeliLine)`
@@ -27,20 +28,16 @@ function nowServing(currentLine){
 // or parameter name in your function though, it's just an example of a variable that might be passed to it.
 // If there is nobody in line, it should return `"The line is currently empty."`
 
-function currentLine(currentLine){
-  if (currentLine.length == 0){
+function currentLine(line){
+  if (line.length == 0){
     return "The line is currently empty.";
-  }
-  else {
+  } else {
     var str = "The line is currently: ";
-    for (var i = 0; i < currentLine.length; i++){
-      str += (i+1) + ". " + currentLine[i];
-      if (currentLine[i+1] != null){
+    for (var i = 0; i < line.length; i++){
+      str += `${i+1}. ${line[i]}`;
+      if (i != line.length -1){
         str += ", ";
       }
     }
-    return str;
-  }
-
-
+  } return str;
 }
