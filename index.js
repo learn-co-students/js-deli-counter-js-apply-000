d6831a4d9e5315dcb8789261014678663d88a35e
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-21 21:34:00 +0000

    Done.

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ function nowServing(katzDeliLine) {
     return "Currently serving " + katzDeliLine.shift() + "."; }
   }
   
-  function currentLine (katzDeliLine) {
-    if (katzDeliLine.length === 0) {
-      return "The line is currently empty." }
-      else {
-        return "The line is currently: 1. " + katzDeliLine [0] + ", 2. " + katzDeliLine [1] + ", 3. " + katzDeliLine [2];
-      }
-  }
- 
+function currentLine (katzDeliLine) {
+  var customerLine = [];
+  if (katzDeliLine.length > 0) {
+    for (var i=0; i<katzDeliLine.length; i++) {
+      customerLine.push(" " + (i + 1) + ". " + katzDeliLine[i]);
+    }
+  return ("The line is currently:" + customerLine.slice(0, customerLine.length));
+} else {
+  return "The line is currently empty.";
+}
+}
