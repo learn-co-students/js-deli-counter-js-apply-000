6e030c2604876aafdea731a4fa5fd193eb0a69d4
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-08-30 03:30:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var greeting = null
+var lineLength = null
+var lineStatement = ''
+var thisPatron = null
+var i = null
+
+function takeANumber(currentDeliLine, newCustomer){
+  lineLength = currentDeliLine.length + 1
+  greeting = 'Welcome, ' + newCustomer + '. You are number ' + lineLength + ' in line.'
+  currentDeliLine.push(newCustomer)
+  return greeting
+}
+
+function nowServing (currentDeliLine) {
+    if (currentDeliLine.length === 0) {
+      return "There is nobody waiting to be served!"
+  } else {
+    thisPatron = currentDeliLine[0]
+    currentDeliLine.shift()
+    return "Currently serving " + thisPatron + "."
+  }
+}
+
+function currentLine (currentDeliLine) {
+  if (currentDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    i = 0
+    while (i < currentDeliLine.length){
+      lineStatement +=  i + 1 + ". " + currentDeliLine[i] + ", "
+      i++
+    }
+    return "The line is currently: " + lineStatement.slice(0,-2)
+  }
+}
