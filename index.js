4dc53d405a1410873581254bcf2d1f9d7627b2e1
Arti M <artismarti@gmail.com>
2018-09-03 23:12:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-function takeANumber(queue, name) {
-  return `Welcome, ${name}. You are ${queue + 1} in line."
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var prefix = `The line is currently: `
+  var suffix = [];
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for(var i = 0; i < katzDeliLine.length; i++) {
+      suffix[i] = `${i+1}. ${katzDeliLine[i]}`
+    }
+  return prefix + suffix.join(", ")
+  }
 }
