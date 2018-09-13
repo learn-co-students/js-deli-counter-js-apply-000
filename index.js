d4d8046e0258cb85f8b616af55bc34ef53caa48b
Ben Haugen <ben1232jazz@hotmail.com>
2018-09-13 19:41:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeli = [];
+const takeANumber = (katzDeli, name) => { 
+    katzDeli.push(`${name}`);
+
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+const nowServing = (katzDeli) => {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+    else {
+    return (`Currently serving ${katzDeli.shift()}.`)
+    }
+}
+
+var line = [];
+const currentLine = (katzDeli) => {
+  for (let i=0; i < katzDeli.length; i++) {
+    line.push(` ` + [i+1] + `. ` + katzDeli[i])
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    return (`The line is currently:` + line)
+  }
+}
