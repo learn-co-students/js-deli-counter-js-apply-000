603c705e3d657cb77b2851c40f60eec642a1d498
Manny Shapir <mshapir95@gmail.com>
2018-09-17 17:32:21 +0000

    Done.

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ function currentLine(line){
   if(line.length === 0){
     return "The line is currently empty." 
   } else{
-    var stringLine = "The line is currently"
+    var stringLine = "The line is currently: "
     for(var i=0; i<line.length; i++){
-      stringLine = stringLine + i + "." + line[i] + ","
-    }
+      stringLine = stringLine + (i+1) + ". " + line[i] + ", "
+    } 
+    return stringLine
   }
   
 }
