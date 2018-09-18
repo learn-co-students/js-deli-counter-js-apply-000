ce783d0b25ee14f49b53b48b4eeb6d9b5fe4be1d
Ray Raven <rayzafir@aol.com>
2018-09-18 20:43:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 function takeANumber(line,name) {
-  line.push(name)
-  var length= line.length 
- return (`Welcome, ${name}. You are number ${length} in line.`)
+  line.push(name);
+  var length= line.length; 
+ return (`Welcome, ${name}. You are number ${length} in line.`);
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length>0){
   var line= katzDeliLine.shift();
-    return `Currently serving ${line}.`
+    return `Currently serving ${line}.`;
   }
   else{
-   return "There is nobody waiting to be served!"
+   return "There is nobody waiting to be served!";
   }
 }
 function currentLine (line) {
  if (line.length>0) {
-  for (i=0, n=1; line.length>0; i++, n++) {
-  return `The line is currently:${n}. ${line[i]},`
-   }
-  }else{
-     return "The line is currently empty."
-  } 
+    var statement="The line is currently:";
+    for (var i=0; i<line.length ; i++) {
+  statement += ` ${i+1}. ${line[i]}`;
+  if (i<line.length -1) {
+    statement += ",";
+  }
+    }
+   return statement;
+ }else{
+     return "The line is currently empty.";
+ }
 }
