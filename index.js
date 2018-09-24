8bde60fae60ba434dd701db43ac0c07ee203092c
John Lew <johnlew@me.com>
2018-09-24 00:25:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+}  
+
+function nowServing(katzDeliLine) {
+if (katzDeliLine.length === 0) {
+  return "There is nobody waiting to be served!";
+} 
+else {
+  katzDeliLine.slice();
+  return "Currently serving " + katzDeliLine.shift() + ".";
+}
+}
+
+function currentLine(katzDeliLine) {
+  var i = 0;
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  }
+  var queue = [];
+  for(i=0; i<katzDeliLine.length; i++) {
+    queue.push(i+1 + ". "+ katzDeliLine[i]);
+  }
+  return "The line is currently: " + queue.join(', ');
+}
