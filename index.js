447c14513fb754b61e57d8760edc5c8052b6722f
John Lew <johnlew@me.com>
2018-09-25 15:26:10 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
-  var i = 0;
+  var i;
   var line = [];
   for(i=0; i<katzDeliLine.length; i++) {
     line.push(i+1 + ". "+ katzDeliLine[i]);
