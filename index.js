35a47cfbf1e0189b11352bd86056c9ffd2caada5
John Lew <johnlew@me.com>
2018-09-25 14:37:46 +0000

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in lin
 }  
 
 function nowServing(katzDeliLine) {
-if (katzDeliLine.length === 0) {
-  return "There is nobody waiting to be served!";
+if (katzDeliLine.length > 0) {
+  return "Currently serving " + katzDeliLine.shift() + ".";
 } 
 else {
-  return "Currently serving " + katzDeliLine.shift() + ".";
+  return "There is nobody waiting to be served!";
 }
 }
 
