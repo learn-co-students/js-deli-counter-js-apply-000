afc0480dc150d5f2885e9b3c3bec14adf46c8e91
danhart1 <danhart249@live.com>
2018-10-01 16:25:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 //Question 1
 
-var katzDeliLine = [];
 
 function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name);
   return 'Welcome, '+ name +'.' + ' You are number '+ katzDeliLine.length + ' in line.';
 }
+
+
 //Question 2
 function nowServing(katzDeliLine){
-  if(katzDeliLine.length===0){
+  if(katzDeliLine.length<1){
     return 'There is nobody waiting to be served!';
   }
   else{
@@ -16,18 +17,53 @@ function nowServing(katzDeliLine){
     }
 }
 
+/*
+//Question 3 
+//(3 VARIATIONS)
 
-//Question 3
+//Variation1.(9 lines of code) 
 
 function currentLine(katzDeliLine){
-   var queue=[];
-  for(let i=0; i<katzDeliLine.length; i+=1){
-    queue.push(' '+[i+1]+'. '+katzDeliLine[i]);
+  var curLine=[];
+  for(let i=0;i<katzDeliLine.length;i+=1){
+    curLine.push(' '+(i+1)+'. '+ katzDeliLine[i]);
   }
   if(katzDeliLine.length<1){
-    return "The line is currently empty.";
+    return'The line is currently empty.';
   }else
-  return ('The line is currently:'+ [queue]);
+  return 'The line is currently:'+ curLine;
+}
+*/
+
+//Variation 2.(9 lines of code)
+//Same as variation1 but used .join method to list array
+//Slightly more complicated than variation1 but the best in my opinion.
+
+function currentLine(katzDeliLine){
+  var curLine=[];
+  for(var i=0;i<katzDeliLine.length;i+=1){
+    curLine.push(i+1+'. '+katzDeliLine[i]);
+  }
+  if(katzDeliLine.length<1){
+    return 'The line is currently empty.';
+  }else
+  return 'The line is currently: '+curLine.join(', ');
 }
 
+/*
+//Variation3.(11 lines of code)
+//Used a while loop instead of a for loop with .join method.
 
+function currentLine(katzDeliLine){
+  var curLine=[];
+  var counter=0;
+  while(counter<katzDeliLine.length){
+    curLine.push(counter+1+'. '+katzDeliLine[counter]);
+    counter+=1;
+  }
+  if(katzDeliLine.length<1){
+    return 'The line is currently empty.';
+  }else
+  return 'The line is currently: '+curLine.join(', ');
+}
+*/
