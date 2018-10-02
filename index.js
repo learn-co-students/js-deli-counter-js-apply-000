1ed5e3a47a0168f317e8b25beab64c9ad9d77199
Arpan Kaul <kaul.arpan@gmail.com>
2018-10-02 20:10:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 var katzDeliLine = [];
 
-function currentLine(x) {
-  var line = [];
-  if (x.length === 0) {
-    return "The line is currently empty."
-  } else {
-    for(var i = 0; i < x.length; i++) {
-      line += (i + 1) + ". " + x[i] + ", "
-    }
-    line = line.slice(0, line.length-2)
-    return "The line is currently: " + line
-  }
-}
-
-
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
     return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
@@ -29,3 +15,15 @@ function nowServing(katzDeliLine) {
   }
 }
 
+function currentLine(x) {
+  var line = [];
+  if (x.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for (var i = 0; i < x.length; i++) {
+      line += (i + 1) + ". " + x[i] + ", "
+    }
+    line = line.slice(0, line.length-2)
+    return "The line is currently: " + line
+  }
+}
