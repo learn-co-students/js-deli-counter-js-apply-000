2a77c38bf74df5dbd1f92724219a245ceec0eb86
Jason Basuil <basuilj@gmail.com>
2018-10-08 19:50:20 +0000

    Done.

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ function takeANumber (katzDeliLine, newPerson){
 //this function takes the current line of people, returns the first person in line then removes them
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) {
-    return (`Currently serving ${katzDeliLine.shift()}.`)
+    return (`Currently serving ${katzDeliLine.shift()}.`);
   }
   else {
-    return ('There is nobody waiting to be served!')
+    return ('There is nobody waiting to be served!');
   }
 }
 
-//this function accepts the current line of people and returns the current line as a string 
+//this function accepts the current line of people and returns the current line as a string (Make this a for loop!!)
+
 function currentLine(katzDeliLine){
-  if (katzDeliLine.length > 0){
-    return (`The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`); }
-  else { 
-    return("The line is currently empty.");
+  if (katzDeliLine.length === 0){
+      return("The line is currently empty.");
   }
+  var localCurrentLine = katzDeliLine;
+  for (var i = 1; i < katzDeliLine.length; i++){
+      localCurrentLine.push(`${i}. `+`${localCurrentLine[i-1]}`);
+  }
+  return (`The line is currently: ${localCurrentLine}`);
 }
