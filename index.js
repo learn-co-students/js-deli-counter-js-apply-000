8a9397c03deb4042ac325b552d35e50fc43b7c73
Albina Zakirova <zak1804@gmail.com>
2018-10-27 16:26:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+//1. add newCustomer
+
+function takeANumber(katzDeli, newCustomer )
+{
+  katzDeli.push(newCustomer)
+  return("Welcome,"+ newCustomer + ". You are number " + katzDeli.length + " in line.")
+}
+
+//2.
+function nowServing(katzDeliLine)
+{
+  if (katzDeliLine.length === 0)
+  {
+    return ('There is nobody waiting to be served!')
+  }
+  else 
+  {
+    var Customer = katzDeliLine.shift() // remove first customer from array
+    return ('Currently serving' + Customer +'.')
+  }
+}
