1c09e341f004b204daee53a457d2c64c5679cd0e
Clara Munro <clarajmunro@gmail.com>
2018-11-14 20:53:01 +0000

    Done.

--- index.js
+++ index.js
@@ -4,39 +4,52 @@ function takeANumber(katzDeliLine, name){
 katzDeliLine.push(name)  //will push the name to the end of the array
 //return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`  //The .length method returns the length of the array on which the method is operated.
+//<<<<<<< HEAD
+//=======
+
+
+//>>>>>>> 073afe217fb0fdbe041c6a6048551d1d949b7cc7
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+  if (katzDeliLine.length === 0) {  // (!katzDeliLine.length) ? can we go over the ! Not logical operator?
     return "There is nobody waiting to be served!"
     }
+//<<<<<<< HEAD
     else {
-      /*
-      var shiftLine = katzDeliLine.shift(0);   //The .shift method in Javascript removes the first item of an array & then RETURNS that item
-      return "Currently serving " + shiftLine + ".";
-      */
-     return "Currently serving " + katzDeliLine.shift(0) + ".";
-    /*
-    else {
-    var name = katzDeliLine[0];  // who is at index 0?
-    katzDeliLine.splice(0,1)  // removes the fist person in the line at index 0
-    // return "Currently serving " + name + ".";
-    return `Currently serving ${name}.`;
-    */
+      return `Currently serving ${katzDeliLine.shift()}.`;    // shift method removes the first element from the array & returns that removed element- mutates array length
+    //  return "Currently serving " + katzDeliLine.shift() + ".";
+    }
   }
-}
+
+//    Other Option 1...
+//    var shiftLine = katzDeliLine.shift();   //The .shift method in Javascript removes the first item of an array & then RETURNS that item
+//    return "Currently serving " + shiftLine + ".";
+
+// =======
+// >>>>>>> 073afe217fb0fdbe041c6a6048551d1d949b7cc7
+
+//    Other Option 2...
+//    var name = katzDeliLine[0];     // who is at index 0?
+//    katzDeliLine.splice(0,1)       // removes the fist person in the line at index 0
+//    return "Currently serving " + name + ".";
+//    return `Currently serving ${name}.`;  } }
 
 function currentLine(katzDeliLine) {
   var line = []
-  if (katzDeliLine.length === 0) {
+  if (katzDeliLine.length === 0) {  // (!katzDeliLine.length) ? can we go over the ! Not logical operator?
     return "The line is currently empty."
     }
-  else {
-         for (var i=0; i<katzDeliLine.length; i++) {
-         line += (i+1) + ". " +katzDeliLine[i] + ", "
-         }
-         line = line.slice(0, line.length-2);
-    // return "The line is currently: " + currentLine
-    return `The line is currently: ${line}`;
-  }
-}
+    for (var i=0; i<katzDeliLine.length; i++) {  // iteration using a for loop for new array
+        line.push(i+1 + ". " + katzDeliLine[i]);
+    }
+ return "The line is currently: " + line.join(`, `);
+ }
+  // else {
+        // for (var i=0; i<katzDeliLine.length; i++) {
+        // line += (i+1) + ". " +katzDeliLine[i] + ", ";
+        // }
+        // line = line.slice(0, line.length-2);  // removes the last 2 characters the , and space from the end of the aray
+
+    // return "The line is currently: " + line
+    // return `The line is currently: ${line}`; }
