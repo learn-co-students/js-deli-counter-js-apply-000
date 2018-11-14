17edf5c24c30af0fb940aaf5cc2e3af689afed74
Clara Munro <clarajmunro@gmail.com>
2018-11-14 05:29:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-var katzDeliLine = [];
+
 
 function takeANumber(katzDeliLine, name){
-katzDeliLine.push(name)
+katzDeliLine.push(name)  //will push the name to the end of the array
 //return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
-return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`  //The .length method returns the length of the array on which the method is operated.
 }
 
-function nowServing(a) {
-  if (a.length === 0) {
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
     }
-  else {
-    var name = a[0];  // who is at index 0?
-    a.splice(0,1)  // removes the fist person in the line at index 0
-    // return "Currently/Now serving " + name + ".";
+    else {
+      /*
+      var shiftLine = katzDeliLine.shift(0);   //The .shift method in Javascript removes the first item of an array & then RETURNS that item
+      return "Currently serving " + shiftLine + ".";
+      */
+     return "Currently serving " + katzDeliLine.shift(0) + ".";
+    /*
+    else {
+    var name = katzDeliLine[0];  // who is at index 0?
+    katzDeliLine.splice(0,1)  // removes the fist person in the line at index 0
+    // return "Currently serving " + name + ".";
     return `Currently serving ${name}.`;
-    }
+    */
+  }
 }
 
-function currentLine(b) {
+function currentLine(katzDeliLine) {
   var line = []
-  if (b.length === 0) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty."
     }
   else {
-         for(var i=0; i<b.length; i++) {
-         line += (i+1) + ". " +b[i] + ", "
+         for (var i=0; i<katzDeliLine.length; i++) {
+         line += (i+1) + ". " +katzDeliLine[i] + ", "
          }
-         line = line.slice(0, line.length-2)
+         line = line.slice(0, line.length-2);
     // return "The line is currently: " + currentLine
     return `The line is currently: ${line}`;
   }
