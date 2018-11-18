ca2771051b7ba9bcb76ff73f8ed5a1122d4c9638
Joe Mullen <mullenjoe13@gmail.com>
2018-11-18 23:06:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeli = [];
+
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
+  }
+  var linesNamesNumbers = [];
+  
+  for(var i=0; i<line.length; i++) {
+    linesNamesNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + linesNamesNumbers)
+  return "The line is currently: " + linesNamesNumbers.join(', ');
+}
+
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    //console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift() + ".";
+  }
+}
+
+function takeANumber(line, name){
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
