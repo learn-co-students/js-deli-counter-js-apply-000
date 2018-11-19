00c8a7b9d58d768d4687fe9045f559107a12018d
Diane Korongy <dkorongy@gmail.com>
2018-11-19 18:05:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-
-//push method
 var katzDeliLine = [];
+
+// 1. Take a number
+
 function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  // add +1 to end of Array
-  //return string
-  var position = katzDeliLine.indexOf(name)+1;
-  return 'Welcome, ${name}. You are number ${position} in line.'
+ if (katzDeliLine.length > 0); 
+ // so array position doesn't start at zero
+   katzDeliLine.push(name);
+
+ // other way +1
+  
+ return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
-//shift method
+
+// 2. Now serving 
+
+function nowServing (katzDeliLine) { 
+   if(katzDeliLine.length === 0) {  
+   return 'There is nobody waiting to be served!'; 
+}
+// shift method removes first person in line and returns
+   var customer = katzDeliLine.shift(0); 
+   return `Currently serving ${customer}.`; 
+}
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length){
-    return 'Currently serving' + katzDeliLine.shift() + ".";
-  }
-  else return 'There is nobody waiting to be served!'
+  if(katzDeliLine.length){
+    return 'Currently serving ' + katzDeliLine.shift() + ".";
+  } else return `There is nobody waiting to be served!`
 }
 
-function currentLine(katzDeliLine){
-  
-}
-else return "The line is currently empty."
-}
+// 3. Current line
+
+var currentLine = function (katzDeliLine) {
+if (katzDeliLine.length===0) {
+   return "The line is currently empty.";
+ }
+    var customerAndNumber = [];
+    for (var i=0; i<katzDeliLine.length; i++) {
+    customerAndNumber.push(i + 1 + ". " + katzDeliLine[i])
+ }
+   return "The line is currently: " + customerAndNumber.join(", ");
+ }
