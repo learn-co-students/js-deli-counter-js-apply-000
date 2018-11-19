2909dbb819715c95b17de46dc7ea215f5967f2df
Berkay Akcakil <burkeakcakil@gmail.com>
2018-11-19 22:39:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+//Return name and line number//
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+  katzDeliLine.push(name); //to tell people their line number//
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
- function nowServing(katzDeliLine){
-  if(katzDeliLine.length > 0) {
-      return(`Currently serving ${katzDeliLine.shift()}.`);
-  } else {
+//Return "serving'line number'". If no one is in the line, return that//
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+  return (`Currently serving ${katzDeliLine.shift()}.`)
+  }
+  else {
     return "There is nobody waiting to be served!"
   }
 }
- function currentLine(line) {
-  if(line.length > 0) {
-    var lineArray = [];
-    for(var i = 0; i < line.length; i++){
-      lineArray.push(` ${i + 1}. ${line[i]}`)
+//Return the line w/ people waiting <ol>. If empty; return "empty"//
+function currentLine(line) {
+  if (line.length > 0) {
+    var array = []
+    for (let i = 0; i < line.length; i++) {
+      array.push(` ${i + 1}. ${line[i]}`)
     }
-    return "The line is currently:" + lineArray
-  } else {
+    return "The line is currently:" + array
+  } else
     return "The line is currently empty."
-  }
 }
