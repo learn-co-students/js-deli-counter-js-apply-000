486a0d3d9199138431a29a5373cd3a2888beab62
George Jeffery <georgejeffery@gmail.com>
2018-11-20 12:39:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function takeANumber(katzDeli, newPerson){
+  var katzDeliLine = katzDeli
+  var linelength = katzDeliLine.length;
+  var position = linelength + 1  
+  var message;
+  katzDeliLine[linelength] = newPerson;
+  return "Welcome, " + newPerson + ". You are number " + position + " in line.";  
+ 
+}
+
+function nowServing(katzDeli){
+  if (katzDeli.length ===0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    var current = katzDeli.shift();
+    return "Currently serving " + current + "."
+    
+  }
+}
+
+function currentLine(katzDeli){
+  if (katzDeli.length ===0){
+    return "The line is currently empty."
+  }
+  else{
+   
+    var end = katzDeli.length -1
+    var index = 0
+    var position = 1 
+    var output = "The line is currently: " 
+    while (index < katzDeli.length){
+    if (index < end){
+      output +=  position + ". " + katzDeli[index] + ", " 
+    }
+    else if (index == end){
+      output += position + ". " + katzDeli[index]  
+    }
+    index++
+    position++
+  }
+  return output
+}
+}
