8a5a1a6dad9de2c858dce10338f1d158eb3d4a89
Tali Scheer <tali.scheer@gmail.com>
2018-11-20 02:37:57 +0000

    Done.

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ function nowServing(line) {
  if (line.length === 0) {
     return `There is nobody waiting to be served!`;
 } else {
-   var str = line[0]; //assigned variable for first element of array so that we can call this element in our solution before we remove it from our array
-   line.shift(0); //remove first element of array
+   var str = line[0]; 
+   line.shift(0); 
    return `Currently serving ${str}.`;
 }
 }
 
 
 function currentLine(line) {
-var newLine = []; 
-for (var i = 0; i < line.length; i++) {
-      newLine.push(` ${i + 1}. ${line[i]}`); //add new customers and their number in line to a new array so that we can later call this array as part of our solution.
-}
 if (line.length === 0) {
       return `The line is currently empty.`;
 } else {
+  var newLine = []; 
+for (var i = 0; i < line.length; i++) {
+      newLine.push(` ${i + 1}. ${line[i]}`);
+}
   return (`The line is currently:` + newLine);
 }
 }
