fb44c218b7258b035adab1025e1d4f883049634e
Clara Munro <clarajmunro@gmail.com>
2018-11-20 17:24:23 +0000

    Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`  //The
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {  // (!katzDeliLine.length) ? can we go over the ! Not logical operator?
+  if (katzDeliLine.length === 0) {  // (!katzDeliLine.length) ? can we go over the ! Not logical operator?   == vs. ===?
     return "There is nobody waiting to be served!"
     }
 //<<<<<<< HEAD
@@ -22,29 +22,30 @@ function nowServing(katzDeliLine) {
     }
   }
 
-//    Other Option 1...
-//    var shiftLine = katzDeliLine.shift();   //The .shift method in Javascript removes the first item of an array & then RETURNS that item
+//    ...Other Option 1...
+//    var shiftLine = katzDeliLine.shift();   //  The .shift method in Javascript removes the first item of an array & then RETURNS that item
 //    return "Currently serving " + shiftLine + ".";
 
 // =======
 // >>>>>>> 073afe217fb0fdbe041c6a6048551d1d949b7cc7
 
-//    Other Option 2...
-//    var name = katzDeliLine[0];     // who is at index 0?
-//    katzDeliLine.splice(0,1)       // removes the fist person in the line at index 0
+//    ...Other Option 2...
+//    var name = katzDeliLine[0];     //  Who is at index 0?
+//    katzDeliLine.splice(0,1)       //  Removes the fist person in the line at index 0
 //    return "Currently serving " + name + ".";
 //    return `Currently serving ${name}.`;  } }
 
 function currentLine(katzDeliLine) {
   var line = []
-  if (katzDeliLine.length === 0) {  // (!katzDeliLine.length) ? can we go over the ! Not logical operator?
+  if (katzDeliLine.length === 0) {  // (!katzDeliLine.length) ? can we go over the ! Not logical operator?  == vs. ===?
     return "The line is currently empty."
     }
-    for (var i=0; i<katzDeliLine.length; i++) {  // iteration using a for loop for new array
+    for (var i=0; i<katzDeliLine.length; i++) {  // iterations, using a for loop for new array
         line.push(i+1 + ". " + katzDeliLine[i]);
     }
- return "The line is currently: " + line.join(`, `);
+ return "The line is currently: " + line.join(', ');
  }
+ // ...Other Option 1...
   // else {
         // for (var i=0; i<katzDeliLine.length; i++) {
         // line += (i+1) + ". " +katzDeliLine[i] + ", ";
@@ -53,3 +54,11 @@ function currentLine(katzDeliLine) {
 
     // return "The line is currently: " + line
     // return `The line is currently: ${line}`; }
+
+ //    ...Other Option 2...
+ //    var myString = `The line is currently: 1. ${katzDeliLine[0]}`
+ //    for(let i = 1; i<katzDeliLine.length;i++){
+ //    myString += `, ${i+1}. ${katzDeliLine[i]}`
+ //    }
+ //    return myString
+ //    }
