9b078c2a99a085555b0c3f05914b042d7b4b5a7f
georgejeffery <georgejeffery@gmail.com>
2018-11-21 15:26:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function takeANumber(katzDeli, newPerson){
-  var position = katzDeli.length + 1  
+  var position = katzDeli.length + 1  //have to do this way because index starts at 0
   katzDeli[katzDeli.length] = newPerson;
   return "Welcome, " + newPerson + ". You are number " + position + " in line.";  
- 
+ //Putting the +1 to length calculation in string doesn't work.... does it need to be escaped some other way?
 }
 
 function nowServing(katzDeli){
