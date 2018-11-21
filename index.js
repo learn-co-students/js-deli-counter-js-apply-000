b90b6de4ba94cd3c6c39ee28ff44ef744f986fe2
andy reyes <11710reyes@gmail.com>
2018-11-21 14:44:07 +0000

    Done.

--- index.js
+++ index.js
@@ -11,15 +11,28 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(nextPerson) {
-  var i = 0;
-  while (i < nextPerson.length) {
-    nextPerson.push(`${i+1} ${nextPerson}, `)
-    i++;
-  }
-  if (nextPerson.length === 0) {
+// function currentLine(nextPerson) {
+//   var i = 0;
+//   while (i < nextPerson.length) {
+//     nextPerson.push(`${i+1} ${nextPerson}, `)
+//     i++;
+//   }
+//   if (nextPerson.length === 0) {
+//     return 'The line is currently empty.'
+//   } else {
+//     return `The line is currently: ${nextPerson}`
+//     }
+// }
+
+function currentLine(line) {
+  
+  if (line.length === 0) {
     return 'The line is currently empty.'
   } else {
-    return `The line is currently: ${nextPerson}`
+    var newPeople = [];
+  for (var i = 0; i < line.length; i++) {
+    newPeople.push(`${i+1}. ${line[i]}`)
+  }
+    return `The line is currently: ${newPeople.join(', ')}`
     }
 }
