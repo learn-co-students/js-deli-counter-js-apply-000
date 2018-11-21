d53bd024cb9e1eb54f0a2909f4af31268f24a2f1
andy reyes <11710reyes@gmail.com>
2018-11-21 00:31:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(katzDeliLine, name) {
+katzDeliLine.push(name);
+return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(nextPerson) {
+  var i = 0;
+  while (i < nextPerson.length) {
+    nextPerson.push(`${i+1} ${nextPerson}, `)
+    i++;
+  }
+  if (nextPerson.length === 0) {
+    return 'The line is currently empty.'
+  } else {
+    return `The line is currently: ${nextPerson}`
+    }
+}
