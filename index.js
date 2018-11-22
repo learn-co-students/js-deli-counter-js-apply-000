f4522894463fc5abd07d4fb491bd8fba0652eb52
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2018-11-22 22:39:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+function takeANumber(line, newPerson){
+line.push(newPerson);
+  return 
+  (welcome,
+  $`{newPerson}. You are number ${line.length} in line.;`)
+}
+
+function nowServing(){
+const nowServing = (x)
+=> { if (x.length === 0) 
+  return "There is nobody waiting to be served!"; } 
+  return" Currently serving ${x.shift().;"
+
+
+
+//function nowServing(line){
+//  firstPerson.pop(line);
+//  return $`{line}. serving ${firstPersont}`; 
+  
+}
+
