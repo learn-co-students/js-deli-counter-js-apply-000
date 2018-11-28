23e3409d935114a4712e56e364dfb7c7fb6d96d3
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2018-11-28 00:55:16 +0000

    Done.

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ line.push(newPerson);
   return 
   (welcome,
   $`{newPerson}. You are number ${line.length} in line.;`)
-}
+} 
 
 function nowServing(){
-const nowServing = (x)
-=> { if (x.length === 0) 
-  return "There is nobody waiting to be served!"; } 
-  return" Currently serving ${x.shift().;"
+const nowServing = (x);
+ { if (x.length === 0) 
+  return "There is nobody waiting to be served!"; 
+  return "Currently serving ${x.shift().;"}
+  
+
+}
+    
+function currentLine(line){
+  if (!line.length) 
+    return "The line is currently empty."
 
 
 
-//function nowServing(line){
-//  firstPerson.pop(line);
-//  return $`{line}. serving ${firstPersont}`; 
+const x = []
+     for (let i = 0, l = line.length; i < l; i++) { 
+      x.push(`${i + 1}. ${line[i]}`)
   
 }
-
+ 
+ return `The line is currently: ${x.join(', ')}`
+ };
