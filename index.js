80a96e532f99c997d23ce1910755a3ea48cf015b
mcarneal09 <mcarneal09@yahoo.com>
2018-12-01 05:13:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+ function takeANumber(katzDeliLine,name){
+            katzDeliLine.push(name);
+                      return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+      }
+   
+   
+  
+  
+  function nowServing(katzDeliLine){
+          if (katzDeliLine.length===0){
+                  return "There is nobody waiting to be served!"
+          } else
+                  return `Currently serving ${katzDeliLine.splice(0,1).toString()}.`
+  }
+  
+  
+  function currentLine(katzDeliLine){
+          var line=[]
+          if (katzDeliLine.length===0){
+                  return "The line is currently empty."
+          } else
+                  for (var i = 0; i<katzDeliLine.length;i++){
+                          line.push(" "+[i+1]+". "+katzDeliLine[i])
+                  }
+          return `The line is currently:${line}`
+  }
