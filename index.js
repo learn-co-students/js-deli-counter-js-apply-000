b7d903ec59961eede4ae43c027c3f99dc563d6a6
Joe Mullen <mullenjoe13@gmail.com>
2018-12-04 03:52:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-var katzDeli = [];
+var line = [];
+var linesNamesNumbers = [];
 
-function currentLine(line){
-  if(!line.length) {
-    return "The line is currently empty.";
-  }
-  var linesNamesNumbers = [];
-  
-  for(var i=0; i<line.length; i++) {
-    linesNamesNumbers.push(i+1 + ". "+ line[i]);
-  }
-  console.log("The line is currently: " + linesNamesNumbers)
-  return "The line is currently: " + linesNamesNumbers.join(', ');
+function takeANumber(line, name){
+  line.push(name);
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
 
 function nowServing(line) {
   if(!line.length) {
-    console.log("There is nobody waiting to be served!")
     return "There is nobody waiting to be served!"
   } else {
     return "Currently serving " + line.shift() + ".";
   }
 }
 
-function takeANumber(line, name){
-  line.push(name);
-  
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-  
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
+  }
+  for(var i=0; i<line.length; i++) {
+    linesNamesNumbers.push(i+1 + ". "+ line[i]);
+  }
+  return "The line is currently: " + linesNamesNumbers.join(', ');
 }
+
+
+
