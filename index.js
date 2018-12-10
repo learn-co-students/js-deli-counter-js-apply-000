590f75196ae5fea2f20efed625b075beca993779
Nazrul Babu <n12babu@siena.edu>
2018-12-10 17:57:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const takeANumber = (line , name)=>{
-line.push(name)
-return `Welcome, ${name}. You are number ${line.length} in line.`
-};
+const takeANumber = (line, name)=>{
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 
+}
 
-const nowServing = (line) =>{
+const nowServing = (line)=>{
   if (line.length===0)
-  return "There is nobody waiting to be served!"
+    return `There is nobody waiting to be served!`
   else
-  return `Currently serving ${line.shift()}.`
+    return `Currently serving ${line.shift()}.`
 
-};
-
-const currentLine = (line) =>{
-if (line.length ===0){
-  return `The line is currently empty.`;
 }
 
-let position =[];
-for (let i=0; i<line.length; i++){
-  position.push(i+1 + ". "+line[i])
+
+const currentLine = (line)=>{
+  if (line.length===0){
+    return `The line is currently empty.`;
+  }
+let position = [];
+for (let i= 0; i<line.length; i++){
+  position.push (i+1+'. '+line[i])
 }
-return "The line is currently: " + position.join(', ');
+return `The line is currently: ${position.join(', ')}`
 
 };
