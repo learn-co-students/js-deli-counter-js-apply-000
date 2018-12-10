bf337ad6669e88f9561085d89a964fc4ca5a956f
Robert Han <robmj.han@gmail.com>
2018-12-10 03:18:43 +0000

    Done.

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ var line = []
 function currentLine (katzDeliLine) {
   let i = 0;
   while (i < katzDeliLine.length) {
-    line.push(` `+[i+1]+`.` +'katzDeliLine[i]' )
-    i++;
-      }
-    if (katzDeliLine === 0) {
+    line.push(` `+[i+1]+`. ` + katzDeliLine[i])
+    i++;}
+    
+    if (katzDeliLine.length === 0) {
       return ("The line is currently empty.")
     }
     else {
-      return ("The line is currently:" + line)
+      return ("The line is currently:" + line);
     }
 }
