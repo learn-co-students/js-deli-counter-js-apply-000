a1931cc4958076dc93913309fe67aeefa7182e4a
Adrienne Miller <millera.92@gmail.com>
2018-12-16 21:43:10 +0000

    Done.

--- index.js
+++ index.js
@@ -16,16 +16,15 @@ return ("There is nobody waiting to be served!");
 var line = [];
 
 function currentLine(katzDeliLine) {
- 
- let i = 0;
-  while (i < katzDeliLine.length) {
-    line.push(` `+[i+1]+`. `  + katzDeliLine[i])
-    i++;
+  
+for (let i=0; i < katzDeliLine.length; i++){
+  line.push(" " + [i+1] + ". " + katzDeliLine[i])
+  
+} if (katzDeliLine.length === 0) {
+    return ("The line is currently empty.");
+  } else {
+  return("The line is currently:" + line);
   }
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
 }
 
 
