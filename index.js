705cdf8b7d7d5a74c3a10a3ab74f6f7e71acae28
Adrienne Miller <millera.92@gmail.com>
2018-12-16 21:29:40 +0000

    Done.

--- index.js
+++ index.js
@@ -13,19 +13,20 @@ return ("There is nobody waiting to be served!");
 
 }
 
+var line = [];
+
 function currentLine(katzDeliLine) {
  
+ let i = 0;
+  while (i < katzDeliLine.length) {
+    line.push(` `+[i+1]+`. `  + katzDeliLine[i])
+    i++;
+  }
   if (katzDeliLine.length === 0) {
-    return ("The line is currently empty.");
-  } else {
-    for(var i=0; i<katzDeliLine.length; i++) {
-      katzDeliLine.push([i]);
-    }
-    return ("The line is currently: " + katzDeliLine + ".");
-    }
-  
-} 
-  
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
 
 
 
