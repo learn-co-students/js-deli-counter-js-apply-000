6b2e0fff978b62ddcf6a7597d302ad870cee8b5d
Jaimie Nguyen <jaimietn@gmail.com>
2018-12-17 00:55:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-function takeANumber(line, name){
-  line.push(name);
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
 function nowServing(line) {
-  let i = 0;
-  while (i < line.length) {
-    i++;
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
   }
-  if (line.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-  else
-  return (`Currently serving ${line.shift()}.`);
+  return `Currently serving ${line.shift()}.`
 }
 
-function currentLine(line){
-  if(!line.length) {
-    return "The line is currently empty.";
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
   }
-  var lineNamesandNumbers = [];
-  
-  for(var i=0; i<line.length; i++) {
-    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+
+  const numbersNames = []
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersNames.push(`${i + 1}. ${line[i]}`)
   }
-  console.log("The line is currently: " + lineNamesandNumbers)
-  return "The line is currently: " + lineNamesandNumbers.join(', ');
+  return `The line is currently: ${numbersNames.join(', ')}`
 }
