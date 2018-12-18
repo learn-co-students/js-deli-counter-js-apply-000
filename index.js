a3c43ac9c11ffe65bf997a4f4fe10415ec3676d5
Sunny Chang Mei <sunnympegasus@gmail.com>
2018-12-18 18:12:12 +0000

    Done.

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ function takeANumber(line, name) {
 
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
-
-  /** If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.**/
 // takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
 // takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
 // takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
+  /** If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.**/
 
 function nowServing(line) {
+  var remove = line.shift();
   if (line.length === 0) {
     return 'There is nobody waiting to be served!';
   } else {
-    return `Currently serving ${line.shift()}.`;
+    return `Currently serving ${remove}.`;
   }
 }
 // nowServing(katzDeliLine); // "Currently serving Ada."
@@ -31,10 +31,12 @@ function currentLine(line) {
 
   var lineOfNameAndNum = [];
   for (var i = 0; i < line.length; i++) {
-    let current = line[i];
-    lineOfNameAndNum.push(i+1 + '. ' + current);
+    var current = line[i];
+    var count = i + 1;
+    lineOfNameAndNum.push(count + '. ' + current);
   }
 
   return `The line is currently: ${lineOfNameAndNum.join(', ')}`;
 }
 // currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+//If there is nobody in line, it should return "The line is currently empty."
