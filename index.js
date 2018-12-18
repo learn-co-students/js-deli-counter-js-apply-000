b5a71760bdda9f70d599266b4816f56aea53bbd8
Sunny Chang Mei <sunnympegasus@gmail.com>
2018-12-18 14:45:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
 function currentLine(line){
   if(!line.length) {
     return "The line is currently empty.";
   }
   var lineNamesandNumbers = [];
-  
+
   for(var i=0; i < line.length; i++) {
     lineNamesandNumbers.push(i+1 + ". "+ line[i]);
   }
-  
+
   return "The line is currently: " + lineNamesandNumbers.join(', ');
 }
 
 function nowServing(line) {
   if(!line.length) {
-   
+
     return "There is nobody waiting to be served!"
   } else {
-    
+
     return "Currently serving " + line.shift() + ".";
   }
 }
 
 function takeANumber(line, name){
   line.push(name);
-  
+
   console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-  
+
   return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
