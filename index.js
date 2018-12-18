ff7facde9c3e8eed00f187f0644650bba18ef5e1
scruffy-text-8914 <dorota.sawicki@gmail.com>
2018-12-18 17:55:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+var katzDeli = [];
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty.";
+  }
+  var lineNamesandNumbers = [];
+  
+  for(var i = 0; i < line.length; i++) {
+    lineNamesandNumbers.push((i + 1) + ". " + line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
+}
+
+  
+ function nowServing(line) {
+  if (!line.length) {
+    console.log("There is nobody waiting to be served!");
+    return ("There is nobody waiting to be served!");
+  } else {
+    return ("Currently serving " + line.shift() + ".");
+  }
+}
+
+
+
+function takeANumber (line, name) {
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
+
+
