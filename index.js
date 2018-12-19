cd07997f3b01ffa39170f98587d41e4dbbe60eed
Reid Boudreau <rtboudreau@gmail.com>
2018-12-19 19:40:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeliLine = [];
+
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.'
+  } else {
+    let queue = [];
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      queue.push( `{i + 1}. ${queue[1]}` );
+    }
+    return `The line is currently: ${queue}`
+  }
+  
+}
