2c5a89a9eb5b86ef9c259929acec35b64bc49e7d
Moses Olawoyin <m.bastford@gmail.com>
2018-12-31 19:29:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function takeANumber(currentLine, newPerson){
+  currentLine.push(newPerson)
+  
+  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  return nowServing
+  
+  if(nowServing > 0){
+  katzDeliLine.pop()
+  }
+  if( nowServing = 0){
+    return ("There is nobody waiting to be served")
+  }
+}
+
+
