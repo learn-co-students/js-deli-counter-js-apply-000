b414ab0cc5e910efb81e723f330d59e456b03a13
Moses Olawoyin <m.bastford@gmail.com>
2018-12-31 22:42:47 +0000

    Done.

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ function takeANumber(currentLine, newPerson){
 }
 
 function nowServing(katzDeliLine){
- return
   
-  if(nowServing > 0){
-  katzDeliLine.pop()
+  if(!katzDeliLine.length){
+    return 'There is nobody waiting to be served!'
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
-  if( nowServing = 0){
-    return ("There is nobody waiting to be served!")
-  }
+  //if( nowServing = 0){
+    //return ("There is nobody waiting to be served!")
+  //}
 }
 
+function currentLine(line){
+  //console.log(line)
+  if(line.length === 0){
+    return "The line is currently empty."
+  }
+  
+  const numbersAndNames = []
+  
+  for (let i = 0; line.length > i ; i++ ) {
 
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+  return `The line is currently: ${numbersAndNames.join(", ")}`
+}
