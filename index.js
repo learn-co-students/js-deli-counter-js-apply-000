2c933c5e093531d24ec47e05e7bc3d5cb65478da
Laura Royle <lauraamie.royle@gmail.com>
2019-01-16 13:17:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+}
+
+function nowServing (katzDeli) {
+  if (katzDeli.length > 0) {
+    var nowServing = katzDeli[0];
+    katzDeli.shift();
+      return `Currently serving ${nowServing}.`;
+        } else {
+            return `There is nobody waiting to be served!`;
+              }
+}
+
+var array = [];
+function currentLine(line) {
+  if (line.length == 0) {
+    return `The line is currently empty.`;
+  } else {
+    for (var i = 0; i < line.length; i++) {
+      array.push(` ${i+1}. ${line[i]}`);
+    }
+    return `The line is currently:` + array;
+  }
+}
