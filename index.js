0d2ad2252decc9e31c761d9bb7d861979b414842
Gioacchino Di Nardo <gio.amms@gmail.com>
2019-01-21 09:27:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function takeANumber(line, name){
-  line.push(name);
+  line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
-function nowServing(katzDeliLine){
-  if (!katzDeliLine.length) {
-    return "There is nobody waiting to be served!";
-  }else if (katzDeliLine.length > 0) {
-    return `Currently serving ${katzDeliLine.shift()}.`
+function nowServing(line){
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }else {
+    return `Currently serving ${line.shift()}.`
   }
 }
 
 function currentLine(line){
-  var nexxt = [];
-  if (line.length === 0) {
+  let newArr = [];
+  if (!line.length) {
     return "The line is currently empty."
-  } else {
+  }else {
     for (var i = 0; i < line.length; i++) {
-      nexxt.push(nexxt.length + 1 + ". " + line[i])
+      newArr.push(newArr.length + 1 + ". " + line[i])
     }
   }
-  return "The line is currently: " + nexxt.join(', ');
+  return `The line is currently: ${newArr.join(', ')}`
 }
