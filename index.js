fcc32db628a4c45e68a44de7d197e7cca78a83ec
Karyna Dominguez <karinaminty@gmail.com>
2019-01-21 00:04:44 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.);
