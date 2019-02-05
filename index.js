cf8f66feef660c8ddbd24b617078f925290bd22b
Stefan Hench <stefan.hench@gmail.com>
2019-02-05 16:20:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber (currentLine, name) {
+  currentLine.push(name)
+  return `Welcome, ${name}. You are number ${currentLine.length} in line.`
+}
+
+function nowServing (line) {
+  if (line.length>0) {
+    return (`Currently serving ${line.shift()}.`)
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
+}
+
+function currentLine (line) {
+  if (line.length > 0) {
+    var announcement = 'The line is currently:'
+    for (var i = 0; i<line.length; i++) {
+      if (i !== 0) {
+        announcement = announcement + ','
+      }
+      announcement = announcement + ` ${i+1}. ${line[i]}`
+    }
+    return announcement
+  } else {
+    return 'The line is currently empty.'
+  }
+}
+
