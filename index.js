cb5aba1be9a7e5bd6c725051830e33ab57489081
Carlos Barrera <barrera9696@gmail.com>
2019-02-19 19:43:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 function takeANumber(line, name) {
  line.push(name);
 return (`Welcome, ${name}. You are number ${line.length} in line.`);
@@ -15,14 +12,21 @@ return (`Currently serving ${katzDeli.shift()}.`);
 }
 }
 function currentLine(line) {
-var i = []; 
+var lineAndNumbers = [];
 if (line.length === 0) {
   return 'The line is currently empty.';
+} 
+for (var i=0; i<line.length; i++) {
+  lineAndNumbers.push(`${i+1}. ${line[i]}`);
+} 
+return `The line is currently: ${lineAndNumbers.join(', ')}`;
 }
-for (i=0; i<line.length; i++) {
-console.log('The line is currently' + line);
-}
-}
+
+
+
+///("The line is currently: 1. Bill, 2. Jane, 3. Ann");
+/// ['Bill', 'Jane', 'Ann']
+
    
 
 
