c7482a63382e82f9a23f5150f06e7bb44e3dfbec
Rachel Reinauer <rereinauer@gmail.com>
2019-02-20 01:38:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-function takeANumber(line, name) {
-  line.push(name);
-  return `Welcome, ${name}. You are number ${line.length} in line.`;
+function takeANumber(current, name) {
+  current.push(name);
+  return `Welcome, ${name}. You are number ${current.length} in line.`
 }
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!";
-  } else { 
-    var next = katzDeliLine[0];
-    katzDeliLine.shift();
-    return `Currently serving ${next}.`;
+    return 'There is nobody waiting to be served!'
+  } else {
+    var next = katzDeliLine.shift();
+    return `Currently serving ${next}.`
   }
 }
-function currentLine(line) {
-  if(line.length === 0) {
-    return "The line is currently empty.";
-  } else { var newLine = [];
-    for(let i = 0; i < line.length; i++) {
-      newLine.push(` ${i+1}. ${line[i]}`);
-  }
-    return `The line is currently:${newLine}`;
-  }
+function currentLine(current) {
+  if(current.length === 0) {
+    return 'The line is currently empty.'
+  } else {
+    var string = 'The line is currently: '
+    for(let i = 0; i < current.length; i++) {
+      if(i === current.length - 1) {
+        string += `${i+1}. ${current[i]}`
+      } else {
+      string += `${i+1}. ${current[i]}, `
+      }
+    }
+  } return string;
 }
