bb2b1662d5f1e8f96a302bcd28df546ed229f3f5
Saumya Bajracharya <ssbajracharya@hotmail.com>
2019-02-22 20:57:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(line, name){
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function currentLine(line){
+  if(line.length === 0){
+    return "The line is currently empty."
+  }
+
+  const numAndNames = []
+  for( let i = 0, l = line.length; i < l; i++){
+    numAndNames.push(`${i+1}. ${line[i]}`)
+  }
+  return `The line is currently: ${numAndNames.join(', ')}`
+
+};
