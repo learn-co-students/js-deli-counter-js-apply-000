689b20c74b6d4f123bbaa8375eebb8b4425de174
Matthew Bartl <mattbartl@gmail.com>
2019-02-26 03:35:25 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(line)
 {
   if (!line.length)
   {
-    return "There is nobody waiting to be served!";
+    return `There is nobody waiting to be served!`;
   }
   return `Currently serving ${line.shift()}.`;
 }
@@ -17,12 +17,12 @@ function currentLine(line)
 {
   if (!line.length) 
   {
-    return "The line is currently empty.";
+    return `The line is currently empty.`;
   }
   var lineNumbers = [];
   for (var i = 0; i < line.length; i++) 
   {
     lineNumbers.push(` ${i+1}. ${line[i]}`);
   }
-  return "The line is currently:" + lineNumbers;
+  return `The line is currently:${lineNumbers}`;
 }
