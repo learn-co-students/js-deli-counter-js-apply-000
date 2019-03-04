2d41f44eab226228a474e1b71ca4f67a1f916b42
Stuart Dow <stuart.dow@blackrock.com>
2019-03-04 17:31:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function takeANumber(DeliLine, CustName) {
-  Line.push(CustName);
-  console.console.log("Welcome, " CustName +". You Are Number " +DeliLine.length + " in line.");
-  return "Welcome, " CustName +". You Are Number " +DeliLine.length + " in line."
+function takeANumber(Line, CustName) {
+  Line.push(CustName); // push() method adds new items to the end of an array, and returns the new length
+  //console.log("Welcome, " + CustName + ". You are number " +Line.length + " in line.");
+  return "Welcome, " + CustName +". You are number " +Line.length + " in line.";
+}
+
+function nowServing (Line){
+if (Line.length === 0) {
+  return "There is nobody waiting to be served!";
+}
+else {
+  return "Currently serving " + Line.shift() +".";  //.shift removes the first element from an array and returns that removed element
+}
+}
+
+function currentLine (Line){
+  if (Line.length === 0) { //tripe "=" sign compares both value and type, whereas "==" compares only value (either or both operands are converted to a common type)
+    return "The line is currently empty.";
+  }
+  else {
+    var WhoIsInLine = [];
+    for (var i = 0; i<Line.length; i++) {
+      WhoIsInLine.push(i + 1 +". " + Line[i]); //push() method adds new items to the end of an array, and returns the new length
+    }
+  return "The line is currently: " + WhoIsInLine.join(', ');
+  }
 }
