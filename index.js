c6275fe391bd99e4aba7ff0610115745edde6ccd
Stuart Dow <stuart.dow@blackrock.com>
2019-03-04 16:08:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+function takeANumber(DeliLine, CustName) {
+  Line.push(CustName);
+  console.console.log("Welcome, " CustName +". You Are Number " +DeliLine.length + " in line.");
+  return "Welcome, " CustName +". You Are Number " +DeliLine.length + " in line."
+}
