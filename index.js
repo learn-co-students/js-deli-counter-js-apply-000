8555782dc636ea27989e966222b4cdad04950e77
Blake A Long <blake.a.long@gmail.com>
2019-03-06 18:45:37 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   var line = [];
   for (var i = 0; i < katzDeliLine.length; i++) {
-    line.push(` `+[i+1]+`. `  + katzDeliLine[i]);
+    line.push(` ${i+1}. ${katzDeliLine[i]}`);
   }
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
