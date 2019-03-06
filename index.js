e9d2e82d43f9c4fdf5fe2e683fd5b1d7b9567529
algebraic-propagation-5468 <luishong50@gmail.com>
2019-03-06 05:32:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+ //how to access index and person?
+ 
+ var people = ["Ada",  // only registering the first one 
+              "Grace", 
+              "Kent"
+ ]
+// var katzDeliLine =["1","2","3"
+//     ];
+function takeANumber(katzDeliLine, people){
+  return 'Welcome, '+ people +'. You are number '+ katzDeliLine + ' in line.'
+
+}
+
+function nowServing(katzDeliLine){ //needs conditional statement?
+  
+}
+
+function currentLine(){
+  
+}
