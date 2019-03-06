21b0f1d3bcb98a222a8b6fc6e22b5b6281ff7e18
Blake A Long <blake.a.long@gmail.com>
2019-03-06 18:50:07 +0000

    Done.

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var line = [];
+  var cLine = [];
   for (var i = 0; i < katzDeliLine.length; i++) {
-    line.push(` ${i+1}. ${katzDeliLine[i]}`);
+  cLine.push(` ${i+1}. ${katzDeliLine[i]}`);
   }
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else { 
-    return `The line is currently:` + line;
+    return `The line is currently:` + cLine;
   }
 }
