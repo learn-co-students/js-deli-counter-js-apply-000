f96cd12e9179c0198d625c18879b19bce65a4d80
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-08 18:02:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,77 @@
-var katzDeliLine = [];
+//takeANumber function:
 
 function takeANumber(katzDeli, otherDeli) {
-
   katzDeli.push(otherDeli);
-  return `Welcome, `+ otherDeli + `. You are number ` + katzDeli.length + ` in line.`;
+  return `Welcome, ${otherDeli}. You are number ${katzDeli.length} in line.`;
+}
+
+
+//nowServing function:
+
+function nowServing(deliLine) {
+  if (deliLine.length >= 1) {
+    let serving = deliLine[0];
+    deliLine.shift();
+    return 'Currently serving ' + serving + '.';
+  }
+  do {
+    return 'There is nobody waiting to be served!';
+  } while (!deliLine.length);
+}
+
+
+//currentLine function:
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty.";
+  }
+  var inLine = [];
+  for (let i = 0; i < line.length; i++) {
+    inLine.push(` ${i+1}. ${line[i]}`);
+  }
+  var result = "The line is currently:" + inLine;
+  return result.toString();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* WORK FROM PREVIOUS ATTEMPT & NOTES FROM FIRST TECHNICAL INTERVIEW ARE COMMENTED OUT HERE:
+
+
+
+
+
+const katzDeliLine = [];
+let newNumber = 0;
+
+// function takeANumber(katzDeli, otherDeli) {
+
+//   katzDeli.push(otherDeli);
+//   return `Welcome, `+ otherDeli + `. You are number ` + katzDe.length + ` in line.`;
+
+// }
+// i++
+// i = i + 1
+
+function takeANumber() {
+  
+  newNumber = newNumber + 1;
+  
+  katzDeliLine.push(newNumber);
+  
+  return `Welcome. You are number ` + newNumber;
 
 }
 
@@ -28,7 +96,20 @@ function currentLine(line) {
     }
 
     if (line.length !== 0) {
+    const displayCustomers = [];
+    for (let i = 0; i < line.length; i++) {
+
+    `${i + 1}. ${line[i]}`
+    }
     return "The line is currently: 1. " + line[0] + ", 2. " + line[1] + ", 3. " + line[2];
     }
     
 }
+
+//How do you use displayCustomers on line 52 to create everything from (1. " + line[0] + ", 2. " + line[1] + ", 3. " + line[2];)
+
+//figure out what to do on line 52. do something else to displaycustomers to get it to print in this way
+
+
+
+*/
