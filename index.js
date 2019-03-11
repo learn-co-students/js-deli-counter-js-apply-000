3913d24ec042d89b2965dcad6b32458c2b50a0e7
Danny Tseng <dtfocus1025@gmail.com>
2019-03-11 06:39:01 +0000

    Done.

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    var first = line[0];
-    line.shift();
-    return "Currently serving " + first + ".";
+    return "Currently serving " + line.shift() + ".";
   }
 }
 
