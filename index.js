821396c780d0d1c96db047e1f8021f1f1eaae310
Zayd Zori <zaydzori@gmail.com>
2019-03-14 05:13:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  
+  katzDeliLine.push(name);
+  
+  return ("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.");
+}
+
+
+function nowServing(katzDeliLine) {
+  
+  if (katzDeliLine.length > 0) {
+    
+    return ("Currently serving " + katzDeliLine.shift() + ".")
+  }else {
+    
+    return ("There is nobody waiting to be served!")
+  }
+  
+}
+
+function currentLine(katzDeliLine) {
+  
+  var line = []
+  
+  for (var i = 0; i < katzDeliLine.length; i++) {
+
+      line.push(" " + [i + 1] + ". " + katzDeliLine[i])
+  }
+  
+  if (line.length === 0) {
+      return ("The line is currently empty.")
+  }
+  
+  
+  return ("The line is currently:" + line)
+}
