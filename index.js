5471ecd1996e66eb2be10fec5f1533abbe46962b
Brian Velez <bvez94@gmail.com>
2019-03-17 14:39:02 -0700

    Reformatted code and added comments

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 function takeANumber(line, customerName) {
     // provide actual position in line & not index
     var position = line.length + 1;
-    // add customer names to line 
+    // add customers to the line
     line.push(customerName);
     return "Welcome, " + customerName + "." + " You are number " + position
   + " in line.";
   }
-  
-  function nowServing(line) {
-    if (line.length > 0) {
-      var announcement = "Currently serving " + line[0] + ".";
-      line.shift();
-      return announcement;
-    }
-    else {
-      return "There is nobody waiting to be served!";
-    }
+
+function nowServing(line) {
+  // if there are people in the line
+  if (line.length > 0) {
+    // serve the current person in the line
+    var announcement = "Currently serving " + line[0] + ".";
+    // remove the current person from the line
+    line.shift();
+    // return the person who was just served from the line
+    return announcement;
   }
-  
-  function currentLine(line) {
-    if (line.length === 0) {
-      return "The line is currently empty.";
-    }
-    else {
-      var announcement = "The line is currently: ";
-      for (var x = 0; x < line.length; x++) {
-        announcement += x+1 + "." + " " + line[x];
-        if ((x + 1) < line.length) {
-          announcement += ", ";
-        }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty.";
+  }
+  else {
+    var announcement = "The line is currently: ";
+    for (var x = 0; x < line.length; x++) {
+      announcement += x+1 + "." + " " + line[x];
+      // if there are multiple people in line, add commas 
+      if ((x + 1) < line.length) {
+        announcement += ", ";
       }
-      return announcement;
     }
+    return announcement;
   }
+}
