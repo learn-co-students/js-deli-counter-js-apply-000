cdd439a89b357fe2f179a6505051c5a6477b1ca1
Micu Dulnoan <micu.dulnoan@blackrock.com>
2019-03-18 14:19:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,52 +1,40 @@
+
 function takeANumber(queue,name){
-  var x = queue.length+1;
-  var text = "Welcome, " + name +". You are number " + x +" in line.";
-  queue.push(name)
-  console.log("where is ADA",text);
+  var i = queue.length + 1;
+  var text = "Welcome, "+ name + "." +" You are number "+ i + " in line.";
+  queue.push(name);
+  console.log("First test",text);
   return text;
 }
 
 function nowServing(katzDeliLine){
-  var i = 0;
-  var x = katzDeliLine[i];
+  var i =0;
   var text;
-  
-  if (x === undefined){
-    text = "There is nobody waiting to be served!" 
-  } else {
-    text = "Currently serving " + x + "."
+  var x = katzDeliLine[i];
+  if (x === undefined){text = "There is nobody waiting to be served!";
   }
-  
-
+  else {text = "Currently serving " + katzDeliLine[i] + ".";
+  }
   katzDeliLine.shift(1);
-  console.log("TEST",text)
+  console.log("Second Test Results =",text);
   return text;
 }
-
 
 function currentLine(katzDeliLine){
- 
- if (katzDeliLine.length === 0){
-   return "The line is currently empty." 
- }
- 
-  var x = katzDeliLine[i];
-
-  for (var i=0; i<katzDeliLine.length; i++){
-  var q = i+1;
-  var name
-  if (q === 1){
-    name = "1. " + katzDeliLine[i]
-  } else {
-  name = " " + q + ". " + katzDeliLine[i];
+  console.log(katzDeliLine);
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty.";
   }
-  katzDeliLine[i] = name;
+  
+  for (var i=0; i < katzDeliLine.length; i++){
+    var textname;
+    var q = i+1;
+    if(q === 1){textname = "1. " + katzDeliLine[i];}
+    else {textname = " " + q + ". " + katzDeliLine[i];}
+    console.log("Array and String",katzDeliLine[i],textname);
+  katzDeliLine[i] = textname; //we are replacing our looped string into the Array
 }
-
-var text = "The line is currently: " + katzDeliLine
-  
-  console.log("TEST2",text)
-  return text;
-  
+var finaltext = "The line is currently: " + katzDeliLine; //this add each loop into a string
+console.log("Final Result =",finaltext);
+return finaltext;
 }
-  
