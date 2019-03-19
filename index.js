055b7c80105c196f373257513bffaaf8641a22b7
Micu Dulnoan <micu.dulnoan@blackrock.com>
2019-03-19 20:32:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
-
-function takeANumber(queue,name){
-  var i = queue.length + 1;
-  var text = "Welcome, "+ name + "." +" You are number "+ i + " in line.";
-  queue.push(name);
-  console.log("First test",text);
-  return text;
+function takeANumber(line,name){
+  var i = line.length + 1;
+  var greeting = "Welcome, " + name +". You are number " + i +" in line.";
+  line.push(name);
+  return greeting;
 }
 
 function nowServing(katzDeliLine){
-  var i =0;
-  var text;
+  var i= 0;
   var x = katzDeliLine[i];
-  if (x === undefined){text = "There is nobody waiting to be served!";
-  }
-  else {text = "Currently serving " + katzDeliLine[i] + ".";
-  }
-  katzDeliLine.shift(1);
-  console.log("Second Test Results =",text);
-  return text;
+  if(x === undefined){
+    return "There is nobody waiting to be served!"}
+    else {
+    var nowserving = katzDeliLine[i];
+    katzDeliLine.shift(1); 
+    return "Currently serving " +nowserving+".";
+    }
+
 }
 
 function currentLine(katzDeliLine){
-  console.log(katzDeliLine);
   if(katzDeliLine.length === 0){
+    console.log("this should be an empty array",katzDeliLine)
     return "The line is currently empty.";
+  } 
+ 
+  for(var i = 0; i< katzDeliLine.length; i++){
+    var string;
+    var q = i + 1;
+    string = " "+q +". " + katzDeliLine[i];
+  katzDeliLine[i] = string;
   }
   
-      for (var i=0; i < katzDeliLine.length; i++){
-        var textname;
-        var q = i+1; //because the first person is at position 1 not 0 in the array
-                    if(q === 1){textname = "1. " + katzDeliLine[i];}
-                    else {textname = " " + q + ". " + katzDeliLine[i];}
-                    console.log("Array and String",katzDeliLine[i],textname);
-              katzDeliLine[i] = textname;
-          }
-var finaltext = "The line is currently: " + katzDeliLine;
-
-console.log("Final Result =",finaltext);
-
-return finaltext;
+  var finaltextwitharray = "The line is currently:" + katzDeliLine;
+  console.log("finaltextwitharray",finaltextwitharray)
+  return finaltextwitharray;
+  
+    
 }
+  
