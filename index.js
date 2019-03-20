8e14ff3d88c8b38df162d06f8ed4b5d642718f06
Jake L <jakelovitz@gmail.com>
2019-03-20 02:15:52 +0000

    Done.

--- index.js
+++ index.js
@@ -11,16 +11,17 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
-  var first = katzDeliLine.shift()
+  var first = katzDeliLine.shift();
   return `Currently serving ${first}.`;
 }
 
 function currentLine(katzDeliLine){
-  var currentKatzLine = [];
   
   if (katzDeliLine.length === 0) {
     return `The line is currently empty.`;
   }
+  
+  var currentKatzLine = [];
   
   for(let i = 0; i < katzDeliLine.length; i++){
     currentKatzLine.push((i + 1) + ". " + katzDeliLine[i]);
