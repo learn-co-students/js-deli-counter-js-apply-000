cfbfe86b22802283c9f5ea881c1cfec0d4411792
Bianca Galvan <bianca_galvan@me.com>
2019-03-29 17:21:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-//Takes 2 parameters and adds values/indexes to the end of an array argument. It returns a string.
+// accepts 2 parameters, adds values to array and returns values in a string
 function takeANumber(line,newPerson){
   line.push(newPerson);
-  return(`Welcome, ${newPerson}. You are number ${line.length} in line.`);
+  return ('Welcome, ' + newPerson + '. You are number '+ line.length +' in line.');
 }
- //receives array parameter, returns a string with beginning array index and shifts/removes it until none is left
+
+//accept a parameter and uses conditional statement to remove person from line
+//returns string that tells customer if they are next
 function nowServing(line){
-
-    if (line.length === 0) {
-      return "There is nobody waiting to be served!";
-    }else
-      return (`Currently serving ${line.shift()}.`);
+  if (line.length===0){
+    return ("There is nobody waiting to be served!");
+  }else{
+    return ("Currently serving "+ line.shift() + ".")
+  }
 }
-
- //add line/array contents into a new array with string
- function currentLine(line){
-   var newLine=[]
-   let i=0;
-   while(i<line.length){
-    newLine.push(' '+(i+1) + '. ' + line[i])
+//accepts a parameter, adds the content to a new array and returns the content of that array in a string 
+//it will also evaluate whether the array is empty in order to return the correct string 
+//it tells you your place in line
+function currentLine(line){
+  var newLine=[];
+  let i=0
+  while(i<line.length){
+    newLine.push(" " + (i+1)+". "+ line[i])
     i++
-   }
-   if(line.length===0){
-     return ('The line is currently empty.')
-   }else{
-     return ('The line is currently:'+ newLine)
-   }
- }
+  }
+  if(line.length===0){
+    return("The line is currently empty.")
+  }else{
+    return("The line is currently:" + newLine)
+  }
+}
