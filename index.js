0929ec70781ce0a882099b8a3cc5400cc2ed5005
Steven Chu <work@stevenchustudio.com>
2019-03-29 19:37:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function takeANumber(katzDeliLine,name) {
   katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    return "Currently serving " + katzDeliLine.shift() + "."
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
   else {
     return "There is nobody waiting to be served!"
@@ -16,11 +16,11 @@ function currentLine(katzDeliLine) {
 
   var output = []
   for(var i = 0;i<katzDeliLine.length;i++) {
-      output.push(" " + (i+1) + ". " + katzDeliLine[i])
+      output.push(` ${(i+1)}. ${katzDeliLine[i]}`)
   }
 
   if (katzDeliLine.length > 0) {
-    return "The line is currently:" + output
+    return `The line is currently:${output}`
   }
   else {
     return "The line is currently empty."
