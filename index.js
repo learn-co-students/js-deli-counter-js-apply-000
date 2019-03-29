1bb24adbf093220765e0aa331d282558dd28e1d4
Neal JIn Oh <no4259@gmail.com>
2019-03-29 22:31:55 +0000

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,4 @@ function nowServing(katzDeliLine) {
   while (i < katzDeliLine.length) {
     i++;
   }
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  }
-  else
-    return (`The line is currently:` + line)
 }
