00d5f9ba48ec7319093844b66333939a7e3c1ee0
Neal JIn Oh <no4259@gmail.com>
2019-03-29 21:57:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,13 @@
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number 1 in line.`)
-}
 
 function takeANumber(otherDeli, name) {
   otherDeli.push(`${name}`)
-  return(`Welcome, ${name}. You are number 4 in line.`)
+  otherDeli.length
+  return(`Welcome, ${name}. You are number ${otherDeli.length} in line.`)
 }
 
-
-describe('deli', () => {
-  describe('takeANumber', () => {
-    var katzDeli;
-    var otherDeli;
-
-    beforeEach(() => {
-      katzDeli = [];
-      otherDeli = ["Steven", "Blake", "Avi"];
-    });
-
-    it('adds a person to the line', () => {
-      expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, Ada. You are number 1 in line.");
-      expect(katzDeli).toEqual(['Ada']);
-    });
-
-    it('appends the person the end of the line if there are already people on it', () => {
-      expect(takeANumber(otherDeli, 'Grace')).toEqual("Welcome, Grace. You are number 4 in line.");
-      expect(otherDeli).toEqual(["Steven", "Blake", "Avi", "Grace"]);
-    });
-
-    it("properly handles multiple people being added", () => {
-      takeANumber(katzDeli, 'Ada');
-      takeANumber(katzDeli, 'Grace');
-      takeANumber(katzDeli, 'Kent');
-
-      expect(katzDeli).toEqual(["Ada", "Grace", "Kent"]);
-    });
-  });
-});
+function nowServing(otherDeli) {
+  let i = 0;
+  while (i < otherDeli.length) {
+    return
+  }
+}
