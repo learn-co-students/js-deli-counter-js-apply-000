8ad8b4f9efd4176dab01de2c5ba895a90cae783d
Toni Lamont <tlam212@gmail.com>
2019-03-30 21:31:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
-  
-} katzDeliLine ('Ada', 'Grace', 'Kent');
-function nowServing(katzDeli) {
-  if (katzDeli.length === 0){
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+} 
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0){
     return 'There is nobody waiting to be served!'
   }else{ 
-    return 'Currently serving ' + katzDeli.shift() + '.';
+    return 'Currently serving ' + katzDeliLine.shift() + '.';
 }
 } 
 
-function currentLine(line) {
+function currentLine(katzDeliLine) {
   let lineNumber = [];
-  if(line.length === 0) {
+  if(katzDeliLine.length === 0) {
     return 'The line is currently empty.'; 
-  } for (let i = 0; i < line.length; i++) {
-    lineNumber.push( ' ' + (i+1) + '. '  + line[i]);
+  } for (let i = 0; i < katzDeliLine.length; i++) {
+    lineNumber.push( ' ' + (i+1) + '. '  + katzDeliLine[i]);
   } return 'The line is currently:' + lineNumber;
 }
