1cc6024b012b35539b3e05ca8fcae2d5838d8793
Neal JIn Oh <no4259@gmail.com>
2019-03-30 00:12:06 +0000

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ function takeANumber(otherDeli, name) {
   return(`Welcome, ${name}. You are number ${otherDeli.length} in line.`)
 }
 
+
+
+
+
 function nowServing(katzDeliLine) {
-  let i = 0;
-  while (i < katzDeliLine.length) {
-    i++;
+  {
+    return 'There is nobody waiting to be served!'
   }
+  return `The line is ${condition.shift()}`
 }
+
+
+
+
+
+function currentLine(line){
+  {
+  return "The line is currently empty."
+}
+
+var newName = []
+
+for (let i = 0, l = line.length; i < l; i++) {
+  newName.push(`${i + 1}. ${line[i]}`)
+}
+}
+
+
+
