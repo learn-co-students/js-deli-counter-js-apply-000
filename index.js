8b861b4110013f148d04760a6e8f65f1f9b92afd
Joe Miller <joemiller065@gmail.com>
2019-04-01 16:29:15 +0000

    Done.

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
+  var lineFormating = [];
   if(katzDeliLine.length === 0) {
     return "The line is currently empty.";
-  }
-  else {
-    return "The line is currently: " + "1. " + katzDeliLine[0] + ", " + "2. " + katzDeliLine[1] + ", " + "3. " + katzDeliLine[2];
+  } else {
+    for(let i = 0; i<katzDeliLine.length; i++) {
+      lineFormating += ( i + 1) + ". " + katzDeliLine[i] + ", ";
+    }
+    lineFormating = lineFormating.slice(0, lineFormating.length-2);
+    return "The line is currently: " + lineFormating;
   }
 }
 
