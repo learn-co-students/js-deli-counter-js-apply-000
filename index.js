17b619f08a8df2636555a6327dfe2c4a73dfe818
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-03 00:43:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`; 
+}
+
+function nowServing(katzDeliLine){
+  if(!katzDeliLine.length){
+    return "There is nobody waiting to be served!";
+  }
+  return `Currently serving ${katzDeliLine.shift()}.`;
+}
+
+function currentLine(katzDeliLine){
+  if(!katzDeliLine.length){
+    return 'The line is currently empty.'
+  }
+  var numName = []
+  for(let i=0; i < katzDeliLine.length; i++){
+    numName.push(`${i+1}. ${katzDeliLine[i]}`)
+  }
+  return `The line is currently: ${numName.join(', ')}`
+}
