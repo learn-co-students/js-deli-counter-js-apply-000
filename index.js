b1117f1cf49e53358a90eff6938db10748810897
Christopher Yuan <chrisyuan7@gmail.com>
2019-04-04 03:55:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number "+katzDeliLine.length + " in line."
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   } else {
-    var annouce = "Currently serving " + katzDeliLine[0] + "."
-    katzDeliLine.shift()
-    return annouce
+    var annouce = `Currently serving ${katzDeliLine[0]}.`;
+    katzDeliLine.shift();
+    return annouce;
   }
 }
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   } else {
-    var lineOrder = "The line is currently:"
+    var lineOrder = "The line is currently:";
     for (let i = 0; i < katzDeliLine.length; i++) {
-      lineOrder += " " + (i + 1) + ". " + katzDeliLine[i] +","
+      lineOrder += ` ${(i + 1)}. ${katzDeliLine[i]},`;
   }
-  return lineOrder.slice(0, -1)
+  return lineOrder.slice(0, -1);
   }
 }
