173a418b3896c1664c0b7dc47a423bef87ce4a3e
Belinda Lei <belinda.lei95@gmail.com>
2019-04-06 20:48:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
-var jetties = [];
-
 function takeANumber(jettiesLine, name){
   jettiesLine.push(name);
-  var string = `Welcome, ${name}. You are number ${jettiesLine.length} in line.`
+  var string = `Welcome, ${name}. You are number ${jettiesLine.length} in line.`;
   return string;
 }
 
 function nowServing(jettiesLine){
   if(jettiesLine.length === 0){
-    return 'There is nobody waiting to be served!'
+    return 'There is nobody waiting to be served!';
   }
   else{
   var firstCustomer = jettiesLine.splice(0,1);
-  var currentServe = `Currently serving ${firstCustomer}.`
+  var currentServe = `Currently serving ${firstCustomer}.`;
   }
   return currentServe; 
 }
 
 function currentLine(jettiesLine){
   if(jettiesLine.length === 0){
-    return 'The line is currently empty.'
+    return 'The line is currently empty.';
   }
   else{
     let i=0;
     var positionAndName = []; //empty array to push string into
     while(i<jettiesLine.length){
       positionAndName.push(` ${i+1}. ${jettiesLine[i]}`);
-      i++
+      i++;
     }
   }
   return `The line is currently:${positionAndName}`; 
 }
-
-function totalInLine(jettiesLine){
-  const add = (a,b) => {
-    a + b
-  }
-  const sum = jettiesLine.reduce(add);
-  return sum; 
-}
-
-totalInLine()
