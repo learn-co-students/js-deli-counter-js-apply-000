2d7292df500a7ac7bc9736e0696db3b9d2ded93b
moniet <moniet.sawhney@gmail.com>
2019-04-11 10:41:18 +0100

    removed comments, made more readable

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 katzDeli = [];
 
 function takeANumber(line, name) {
-/*
-  push the name into the line
-  get the index of their name and tell them the location in the line
-*/
-line.push(name);
-let position = line.indexOf(name) + 1;
-return `Welcome, ${name}. You are number ${position} in line.`;
-
+  line.push(name);
+  var position = line.indexOf(name) + 1;
+  return `Welcome, ${name}. You are number ${position} in line.`;
 }
 
 function nowServing(line) {
   if (line.length == 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return line.shift();
+      var toServe = line.shift();
+      return `Currently serving ${toServe}.`;
   }
 }
 
 function currentLine(line) {
-  if (line.length == 0) {
-    return "The line is currently empty.";
-  } else {
-    for (let i=0; i < line.length; i++) {
-      let position = i + 1;
-      let name = line[i]
-      return `the line is currently: ${position}. ${line[i]}`;
+  var newLine = [];
+  var counter;
+  var len = line.length;
+  if (len > 0) {
+    for (var i=0; i < len; i++) {
+      counter = i + 1;
+      newLine[i] = `${counter}. ${line[i]}`;
     }
+    return `The line is currently: ${newLine.toString().split(',').join(', ')}`;
+  } else {
+      return "The line is currently empty.";
   }
 }
+
+var theLine = ["Bill", "Jane", "Ann"];
+currentLine(theLine);
