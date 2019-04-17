459efa18172d26de42632079d85e140e8ede7ab3
Xinran Che <gracexinran@gmail.com>
2019-04-17 14:46:59 +0000

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeliLine){
     if(katzDeliLine.length){
         return `Currently serving ${katzDeliLine.shift()}.`;
     }else{
-        return `There is nobody waiting to be served.`
+        return `There is nobody waiting to be served!`
     }
 }
 
