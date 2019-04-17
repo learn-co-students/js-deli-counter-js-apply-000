56a272f43e181f7c0155baf69b30ba7816c48ef6
Xinran Che <gracexinran@gmail.com>
2019-04-17 14:46:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(katzDeliLine, name){
+	katzDeliLine.push(name);
+    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine){
+    if(katzDeliLine.length){
+        return `Currently serving ${katzDeliLine.shift()}.`;
+    }else{
+        return `There is nobody waiting to be served.`
+    }
+}
+
+
+function currentLine(katzDeliLine){
+	var line = [];
+    if(katzDeliLine.length){
+        for(var i=0; i<katzDeliLine.length; i++){
+            line.push(`${i+1}. ${katzDeliLine[i]}`)
+        }
+		return `The line is currently: ${line.join(', ')}`
+    } else{
+        return `The line is currently empty.`
+    }
+}
