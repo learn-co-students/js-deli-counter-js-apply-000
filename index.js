32f91a229b4d6f02fe7cbc802553c6e2e716c809
moniet <moniet.sawhney@gmail.com>
2019-04-17 09:42:03 +0100

    Made DRY Changes and Made more Readable

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function nowServing(line) {
   if (line.length == 0) {
     return "There is nobody waiting to be served!";
   } else {
-      var toServe = line.shift();
-      return `Currently serving ${toServe}.`;
+    var toServe = line.shift();
+    return `Currently serving ${toServe}.`;
   }
 }
 
@@ -19,13 +19,17 @@ function currentLine(line) {
   var newLine = [];
   var counter;
   var len = line.length;
+
   if (len > 0) {
+
     for (var i=0; i < len; i++) {
       counter = i + 1;
       newLine[i] = `${counter}. ${line[i]}`;
     }
+
     return `The line is currently: ${newLine.join(', ')}`;
   } else {
+
       return "The line is currently empty.";
   }
 }
