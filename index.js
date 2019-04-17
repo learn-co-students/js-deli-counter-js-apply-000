5f196fcafaf1e04d8ef6c23dd093faf915686f5d
moniet <moniet.sawhney@gmail.com>
2019-04-17 19:13:40 +0100

    " "

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(line, name) {
 
 function nowServing(line) {
   if (line.length == 0) {
-    return "There is nobody waiting to be served!";
+    return 'There is nobody waiting to be served!';
   } else {
     var toServe = line.shift();
     return `Currently serving ${toServe}.`;
@@ -17,19 +17,15 @@ function nowServing(line) {
 
 function currentLine(line) {
   var newLine = [];
-  var counter;
   var len = line.length;
 
   if (len > 0) {
-
-    for (var i=0; i < len; i++) {
-      counter = i + 1;
-      newLine[i] = `${counter}. ${line[i]}`;
+    for (var i = 0; i < len; i++) {
+      var positions = i + 1;
+      newLine[i] = `${positions}. ${line[i]}`;
     }
-
     return `The line is currently: ${newLine.join(', ')}`;
   } else {
-
-      return "The line is currently empty.";
+    return 'The line is currently empty.';
   }
 }
