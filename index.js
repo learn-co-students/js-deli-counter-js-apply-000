2c69f44081ced8f846680ffb9ce1d5318dbb401a
Asyet Argynbai <aset2012@gmail.com>
2019-04-24 20:04:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+var katzDeliLine = [];
+function takeANumber(number, name){
+number.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${number.length} in line.`);
+}
+
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
+
+var line = [];
+function currentLine(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
