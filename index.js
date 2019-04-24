dd72fd987d54e938fe20169147bd6a90700602b4
Danielle Martinoli <daniechris5@gmail.com>
2019-04-24 22:52:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var katzDeliLine = []
-function takeANumber(katzDeliLine) {
+function takeANumber(name, number) {
   return "Welcome, ${name} + You are number ${number} in line."
 }
 
-function currentLine(katzDeliLine) {
+function currentLine(name, number) {
   if (katzDeliLine > num) {
     return "Now serving, ${name}"
   }  else
       return "There is nobody waiting to be served!"
 }
 
-function nowServing(katzDeliLine) {
+function nowServing(name, number) {
   if (katzDeliLine > num) {
     return `The line is currently: ${number}, ${name}`
 }    else
