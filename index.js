77f07713327a239d49ef0f71ed5f8a723f04d5fc
Danielle Martinoli <daniechris5@gmail.com>
2019-04-24 22:59:43 +0000

    Done.

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ function takeANumber(name, number) {
 
 function currentLine(name, number) {
   if (number > 0) {
-    return "Now serving, ${name}"
+    return `The line is currently: ${number}, ${name}`
   }  else
-      return "There is nobody waiting to be served!"
+      return "The line is currently empty."
+
 }
 
 function nowServing(name, number) {
   if (number > 0) {
-    return `The line is currently: ${number}, ${name}`
-}    else
-      return "The line is currently empty."
+    return "Now serving, ${name}"
+  }  else
+      return "There is nobody waiting to be served!"
 }
