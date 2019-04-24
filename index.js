92e61dd6c6b00dab9aa923105cc02c19eb88469a
Danielle Martinoli <daniechris5@gmail.com>
2019-04-24 22:28:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var katzDeliLine = []
+function takeANumber(katzDeliLine) {
+  return "Welcome, ${name} + You are number ${number} in line."
+}
+
+function currentLine(katzDeliLine) {
+  if (someone in line) {
+    return "Now serving, ${name}"
+  }  else noone in line
+      return "There is nobody waiting to be served!"
+}
+
+function nowServing(katzDeliLine) {
+  if (people waiting) {
+  return `The line is currently: ${number}, ${name}`
+} else (no one waiting)
+  return "The line is currently empty."
+}
