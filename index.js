943465bbf3946f0e24a2046839e316d4c88fbdef
Danielle Martinoli <daniechris5@gmail.com>
2019-04-25 23:15:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+var name = []
+var number = []
+
+
 function takeANumber(array, name, number) {
-  var name = []
-  var number = []
-  for (let i= 0; i > 0; i++)
-  return "Welcome, ${name} + You are number ${number} in line."
+  return (`Welcome, ${name} + You are number ${number} in line.`)
 }
 
 function currentLine(array, name, number) {
   if (number > 0) {
-    return `The line is currently: ${number}, ${name}`
+    return (`The line is currently: ${number}, ${name}`)
   }  else
       return "The line is currently empty."
 
@@ -15,7 +16,7 @@ function currentLine(array, name, number) {
 
 function nowServing(array, name, number) {
   if (number > 0) {
-    return "Now serving, ${name}"
+    return (`Now serving, ${name}`)
   }  else
       return "There is nobody waiting to be served!"
 }
