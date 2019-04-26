5ec6306c46354e462d8fb4b37f7b6b0a63936f8b
Danielle Martinoli <daniechris5@gmail.com>
2019-04-26 01:35:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function takeANumber(katzDeliLine, name) {
-    array.push(`Welcome, ${name} + You are number ${katzDeliLine.length} in line.`)
-}
+var katzDeliLine = [];
 
+function takeANumber(katzDeliLine, name) {
+    katzDeliLine.push(name)
+    return 'Welcome,' + name + 'You are number' + katzDeliLine.length + 'in line.'
+}
 
 function currentLine(n) {
   var peopleInLine = []
