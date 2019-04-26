5be4d9486402edf2386c0b15bf7c8018a93af2b3
Danielle Martinoli <daniechris5@gmail.com>
2019-04-26 01:58:02 +0000

    Done.

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
     katzDeliLine.push(name)
-    return 'Welcome,' + name + 'You are number' + katzDeliLine.length + 'in line.'
+    return 'Welcome,' + name + ' You are number' + katzDeliLine.length + ' in line.'
 }
 
-function currentLine(n) {
+function currentLine(peopleInLine) {
   var peopleInLine = []
   if (n.length === 0) {
       return "The line is currently empty."
   } else {
     for(var i= 0; i < n.length; i++) {
-    peopleInLine += (i + 1) + '.' + n[i] + ','
+     peopleInLine += (i + 1) + '. ' + n[i] + ', '
   }
     peopleInLine = peopleInLine.slice(0, peopleInLine.length-2)
-    return "The line is currently:" + peopleInLine
+    return "The line is currently: " + peopleInLine
   }
 }
 
 function nowServing(n) {
   if (n.length === 0) {
-    return "There is nobody waiting to be served!"
+     return "There is nobody waiting to be served!"
   }  else {
      var name = n[0];
      n.splice(0, 1);
-     return 'Currently serving,' + name + '.';
+     return 'Currently serving ' + name + '.';
   }
 }
