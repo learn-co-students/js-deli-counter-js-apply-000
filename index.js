bcd4fc526ea7fbbf87755578e8ed534421a24fa3
Dom Gallo <dgallo519@gmail.com>
2019-04-29 19:52:17 -0400

    Index Update
    Added comments and reviewed code in preparation of technical interview.

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
+/*
+// Store is open!
+katzDeliLine = [];
+
+// dewclaw enters
+console.log(takeANumber(katzDeliLine, 'dewclaw'));
+
+//now serving button is pressed twice. shift() will remove dewclaw from the line and announce his name
+console.log(nowServing(katzDeliLine));
+
+// There should be nobody on line at this point.
+console.log(nowServing(katzDeliLine));
+
+// Add more people to the line.
+console.log(takeANumber(katzDeliLine, 'clawdew'));
+console.log(takeANumber(katzDeliLine, 'personA'));
+console.log(takeANumber(katzDeliLine, 'aPerson'));
+
+// someone checks to see who is on and how many people are, on line.
+console.log(currentLine(katzDeliLine));
+*/
+
+// Pass in dummy variables, first is array representing deli line and next is name of customer.
 function takeANumber(katzDeliLine, newCustomer){
+  // Since the line has to persist through several other functions. I choose to use a mutable array method
   katzDeliLine.push(newCustomer);
   return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine){
+  // Check for empty line.
   if(katzDeliLine.length === 0 ){
     return "There is nobody waiting to be served!"
   } else {
+    // if the line isnt empty, then we remove the next customer from line and serve him/her
     return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
@@ -16,10 +42,15 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
     return `The line is currently empty.`
   }
-  // Create intermediate array
+  /*
+  Create intermediate array
+  since this function isn't intended to actually change or alter the
+  deli line a temporary array is used to perform operations.
+  */
   var currentlyWaiting = [];
 
   // Loop through and fill intermediate array with index numbers and names
+  // There is probably a better way to do this.
   katzDeliLine.forEach(function(item,index,array){
     currentlyWaiting.push(`${index + 1}. ${item}`)
   })
