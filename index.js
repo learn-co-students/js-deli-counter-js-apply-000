6822a7a8df084ad4c24414ef9d55ef2ab5956c83
Kyle Conlon <kyleconlon13@gmail.com>
2019-05-20 19:54:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var katzDeliLine = [];
+
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  }
+}
+
+function currentLine (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    var line = [];
+    
+    for (var i=0; i<katzDeliLine.length; i++) {
+      line.push(i+1 + ". " + katzDeliLine[i]);
+    }
+    return "The line is currently: " + line.join(", ");
+  }
+}
