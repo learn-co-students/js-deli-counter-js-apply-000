e9d2ce9ea91cd1369304159d14fbffbb34b60488
Abideen Kareem <abideenkareem17@gmail.com>
2019-05-27 20:56:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-function takeANumber(line, name){
+function takeANumber(line,name){
   line.push(name)
   return "Welcome, " + name + ". You are number " + (line.length) + " in line."
 }
 
 function nowServing(katzDeliLine){
-  if(katzDeliLine.length === 0) return "There is nobody waiting to be served!"
+  if (katzDeliLine.length === 0) {
+  var serving = "There is nobody waiting to be served!"
+  }
+  else {
   var person = katzDeliLine[0]
   katzDeliLine.splice(0, 1)
-  return "Currently serving " + person + "."
+  serving = "Currently serving " + person + "."
+  }
+  return serving 
 }
 function currentLine(line){
-  if(line.length === 0) return "The line is currently empty."
-var result = "The line is currently: "
+  if (line.length === 0) {
+  var inline = "The line is currently empty."
+  }
+  else {
+inline = "The line is currently: "
 for(var i = 0;i< line.length; i++){
-  result += (i+1) + ". " + line[i]
-  if(i < line.length - 1) result += ", "
+  inline += (i+1) + ". " + line[i]
+  if(i < line.length - 1) inline += ", "
 }
-
-  return result
+}
+  return inline
 }
