ab0f9109dcc24c88dd8bcfef2e26ba71616df893
amusing-history-4188 <mackenzieerinpech@gmail.com>
2019-06-05 14:13:24 +0000

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+  return "Welcome, " + name + ". You are number " + 
+  katzDeliLine.length + " in line."
 }
 
 function currentLine(NewLine) {
     var line = []
+    
     if (NewLine.length === 0) {
       return "The line is currently empty."
     } 
@@ -17,12 +19,12 @@ function currentLine(NewLine) {
       line = line.slice(0, line.length-2)
       return "The line is currently: " + line
     }
-    //
 }
 function nowServing(x) {
   if (x.length === 0) {
     return "There is nobody waiting to be served!"
-  } else {
+  } 
+  else {
     var name = x[0];
     x.splice(0, 1);
     return "Currently serving " + name + ".";
