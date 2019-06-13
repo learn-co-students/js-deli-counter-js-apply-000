cea5c70c5d1ad0a5788c748d8a6ffbd1ee8c4993
mitchell williams <mscottwilliams1994@gmail.com>
2019-06-13 03:56:39 +0000

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var line = [];
 
 function takeANumber(line, name){
   line.push(name);
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
+   console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  return ("Welcome, " + name + ". You are number " + line.length + " in line.")
 }
 function nowServing(line) {
   if(!line.length) {
-    console.log("There is nobody waiting to be served!")
+     console.log("There is nobody waiting to be served!")
     return "There is nobody waiting to be served!"
   } else {
     return "Currently serving " + line.shift()+".";
