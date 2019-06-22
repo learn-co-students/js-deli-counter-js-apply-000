870ac2febcfaf53818718c0caa56970db852e1d4
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-06-22 23:20:45 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeliLine = [];
 function takeANumber(line, name){  
   line = katzDeliLine.length + 1;
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number `+ line + `in line.`;
+  return `Welcome, ${name}. You are number `+ line + ` in line.`;
 }
 
 function nowServing(){
