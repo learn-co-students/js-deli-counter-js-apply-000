1f61139e7ef4c15e2d49dd2d9eda1f47749ff727
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-06-22 23:57:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 var katzDeliLine = [];
 
 function takeANumber(line, name){  
-  line = katzDeliLine.length + 1;
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number `+ line + ` in line.`;
+  let length = line.length + 1;
+  line.push(name);
+ return `Welcome, ${name}. You are number `+ length + ` in line.`;
 }
 
-function nowServing(){
+function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
-  } else katzDeliLine.shift();
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
   
 }
-
+/*
 function currentLine(){
   var numberline = [];
     if (katzDeliLine.length === 0){
     return "The line is currently empty.";
   } else {return katzDeliLine;}
   
-}
+}*/
