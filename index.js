7d103e8ae79ec8e50ec51e05b4afaf95aa250eea
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-06-25 01:39:07 +0000

    Done.

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ function nowServing(katzDeliLine){
   }
   
 }
-/*
-function currentLine(){
-  var numberline = [];
-    if (katzDeliLine.length === 0){
-    return "The line is currently empty.";
-  } else {return katzDeliLine;}
+
+function currentLine(line){
   
-}*/
+    if (line.length === 0){
+    return "The line is currently empty.";
+  } else { 
+    var numberAndName = [];
+  for (var i = 0; i < line.length; i++) { 
+  numberAndName.push(` ${i+1}. ${line[i]}`)}
+  return `The line is currently:${numberAndName}`}
+  }
+                            
+
