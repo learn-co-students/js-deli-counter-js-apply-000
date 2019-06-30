db81dc425ccec3e06f2df025ca118458fa2d595d
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-06-30 20:30:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function takeANumber(katzDeliLine, personName)
+{
+  katzDeliLine.push(personName);
+  return "Welcome, " + personName + ". You are number " + katzDeliLine.length + " in line.";
+}
+
+function nowServing(katzDeliLine)
+{
+  if(katzDeliLine.length === 0)
+    return "There is nobody waiting to be served!";
+  else
+  {
+    var firstPerson = katzDeliLine.shift();
+    return `Currently serving ${firstPerson}.`;
+  }
+}
+
+function currentLine(katzDeliLine)
+{
+  if(katzDeliLine.length === 0)
+  {
+    return "The line is currently empty."
+  }
+  else
+  {
+    var str = "";
+    katzDeliLine.forEach(function(value, index)
+    {
+      str += `${index+1}. ${value}`;
+      if(index !== katzDeliLine.length-1)
+      str += ", ";
+    });
+    return "The line is currently: " + str;
+  }
+}
