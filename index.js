687eff2f209153482105d9b1b0ff1205b960daba
Rachel Fuerst <rach.fuerst@gmail.com>
2019-07-07 22:43:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+const katzDeli = []; //Empty string where we insert the names of customers in line
+var name; //Variable where we insert parameters
+function takeANumber(katzDeli, name){
+  katzDeli.push(name); // Add the value of the "name" parameter into the empty string
+  var numberInLine = katzDeli.length; //Count the number of customer names in the array, which equals the number of customers in line
+  var message = `Welcome, ${name}. You are number ${numberInLine} in line.`;
+  return message
+}
+
+function nowServing (katzDeli) {
+  if (katzDeli.length === 0) { //if katzDeli is empty, return the following message
+    var message = "There is nobody waiting to be served!"
+    return message
+  }
+  else { //Otherwise, check the first person in line, remove that person from the line and return the message below
+    var message = `Currently serving ${katzDeli[0]}.`
+    katzDeli.shift(); // Removes the zeroth index item from the deli line array
+    return message
+  }
+
+  // return katzDeli[0]
+  // // if (numberInLine > 0) {
+  // if (katzDeli && katzDeli.length){
+  //     katzDeli.shift();
+  // }
+  // else {
+  //   return "There is nobody waiting to be served!"
+  // }
+  }
+
+const line = []; //Array to store customer names
+function currentLine (line) {
+  if (line.length === 0) { //if there are no entries in the line array, return this message
+    var message = "The line is currently empty."
+    return message
+  }
+  else { //otherwise, return a message with the list of names from the line array
+    var message = `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`
+    return message
+  }
+}
