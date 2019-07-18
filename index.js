e0c772018afe5cae44ae89d37a25fea9e3fefa98
Yula Ko <larkspury.k@gmail.com>
2019-07-18 05:19:11 +0000

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var katzDeli = [];
 
 function takeANumber(currentLine, newCustomer) {
     
-    if (currentLine.length > 0);
+    // if (currentLine.length > 0);
       currentLine.push(newCustomer);
       return 'Welcome, ' + newCustomer + '. You are number ' + currentLine.length + ' in line.'
   }
@@ -19,7 +19,8 @@ function nowServing (currentLine) {
 
 
 
-var currentLine = function (currentLine) {
+// var currentLine = function (currentLine) {
+function currentLine(currentLine){
   if (currentLine.length === 0) {
   return "The line is currently empty.";
 }
