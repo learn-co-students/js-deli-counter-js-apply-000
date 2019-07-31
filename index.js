d34095636a39d4957934a55086f67a1d88f4ebf2
Urfan Guliyev <urfan196@yahoo.com>
2019-07-31 04:04:20 +0000

    Done.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function takeANumber (katzDeliLine,name) {
 }
 
 
-
-function nowServing (katzDeliLine) {
-   if(katzDeliLine.length===0) {
-   return 'There is nobody waiting to be served!';
-}
-   var customer = katzDeliLine.shift();
-   return "Currently serving " + customer + ".";
+function nowServing(katzDeliLine) {
+  var removeFirst = katzDeliLine.shift();
+  if (katzDeliLine.length > 0) {
+    return 'Currently serving ' + removeFirst + '.';
+  } else {
+    return 'There is nobody waiting to be served!';
+  }
 }
 
 
