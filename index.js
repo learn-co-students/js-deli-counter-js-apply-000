2b93b202215fc6f54e88613878ad7c0161a02436
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-07 00:07:50 +0000

    Done.

--- index.js
+++ index.js
@@ -8,15 +8,33 @@ function takeANumber(katzDeliLine, name) {
   return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
 }
 
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) 
+  { return 'The line is currently empty'
+  } else{ for (var i = 0; i < katzDeliLine.length; i++)
+   {line += (i + 1) + '.' + line
+  }
+    line = line.slice(0, line.length-2)
+    return 'The line is currently:' + line
+  }
+}
+
 function nowServing(katzDeliLine) 
-{
-if (katzDeliLine.lenght == 0) 
-   {
-  return 'There is no one waiting to be served!'
+{ if (katzDeliLine.length === 0) 
+   { return 'There is nobody waiting to be served!'
    }
- 
- else 
-    {  
-      return 'Currently serving' + name + '.' 
-    }
+  else 
+  var name = katzDeliLine[0];
+  katzDeliLine.splice(0,1);
+ {  return 'Currently serving ' + name + '.' 
+  }
+}
+
+
+
+// how to iterate through an array 
+
+function currentLine(katzDeliLine) {
+  
+  
 }
