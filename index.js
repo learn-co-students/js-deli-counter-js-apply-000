4677e77b5222972cc6ddd9763538a0dd40d5da14
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-07 00:27:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
 
 var katzDeliLine = [];
 
-var katzdDeli = ['Ada', 'Grace', 'Kent']
+var katzdDeli = ['Ada', 'Grace', 'Kent'];
 
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
+  katzDeliLine.push(name);
   return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
 }
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) 
-  { return 'The line is currently empty'
+  { return 'The line is currently empty.';
   } else{ for (var i = 0; i < katzDeliLine.length; i++)
-   {line += (i + 1) + '.' + line
+   {katzDeliLine += (i + 1) + '.' + katzDeliLine[i] + ',';
   }
-    line = line.slice(0, line.length-2)
-    return 'The line is currently:' + line
-  }
+    katzDeliLine = katzDeliLine.slice(0, line.length-2);
+    return 'The line is currently: ' +  katzDeliLine;
+  } 
 }
 
 function nowServing(katzDeliLine) 
 { if (katzDeliLine.length === 0) 
-   { return 'There is nobody waiting to be served!'
+   { return 'There is nobody waiting to be served!';
    }
   else 
   var name = katzDeliLine[0];
   katzDeliLine.splice(0,1);
- {  return 'Currently serving ' + name + '.' 
+ {  return 'Currently serving ' + name + '.'; 
   }
 }
 
 
 
-// how to iterate through an array 
 
-function currentLine(katzDeliLine) {
-  
-  
-}
