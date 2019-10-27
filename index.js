8a0452b40b1ac1c78be9dd1403781ed6ab2516ea
Sam Hoidal <sjhoidal@gmail.com>
2019-10-27 17:09:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var takeANumber = function(katzDeliLine, newCustomer) {
+  if (katzDeliLine.length > 0);
+  katzDeliLine.push(newCustomer);
+  return "Welcome, " + newCustomer + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length===0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+  var nextCustomer = katzDeliLine.shift();
+  return "Currently serving " + nextCustomer + ".";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length===0) {
+    return "The line is currently empty."
+  }
+  else {
+  var lineOrder = [];
+  for(let i=0; i<katzDeliLine.length; i++) {
+    lineOrder.push((i + 1) + ". " + katzDeliLine[i])
+    }
+  }
+  return "The line is currently: " + lineOrder.join(', ');
+}
+  
