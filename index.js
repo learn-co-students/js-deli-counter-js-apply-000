6371f9c54b959f453948aff758c9060528e2e6ce
Michael De La Rosa <mdelarosa86@gmail.com>
2019-12-16 07:13:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
 function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
   }
 
   return `Currently serving ${line.shift()}.`
-}
-
-function takeANumber(line, name) {
-  line.push(name)
-
-  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
 function currentLine (line) {
