d3f962217b4aae5f6fc54f69816dffb4d6a36405
Michael De La Rosa <mdelarosa86@gmail.com>
2019-12-16 07:00:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+}
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function currentLine (line) {
+   if (line.length === 0) {
+    return "The line is currently empty."
+   }
+   
+   const numberName = []
+   
+   for (let i = 0; i < line.length; i++) {
+     numberName.push(`${i+1}. ${line[i]}`)
+   }
+   
+   return `The line is currently: ${numberName.join(', ')}`
+   
+}
