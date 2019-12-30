b045e58e1cd90b7d10b28c71bff6ef1f0156d1ad
Placido Wang <placidow@gmail.com>
2019-12-30 06:49:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,99 @@
+// function takeANumber(line, name) {
+//   line.push(name);
+//   return "Welcome, " + name + ". You are number " + line.length + " in line.";
+// }
+
+// function nowServing(line) {
+//   if (Array.isArray(line) && line.length === 0) {
+//     return "There is nobody waiting to be served!";
+//   }
+  
+//   let nextPerson = line.splice(0, 1);
+  
+//   return "Currently serving " + nextPerson + ".";
+// }
+
+// function currentLine(line) {
+//   if (Array.isArray(line) && line.length === 0) {
+//     return `The line is currently empty.`;
+//   }
+  
+//   let lineWithPositions = [];
+  
+//   line.forEach((name, i) => {
+//     const position = i + 1;
+//     lineWithPositions.push(`${position}. ${name}`);
+//   });
+ 
+//   return `The line is currently: ${lineWithPositions.join(", ")}`;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function takeANumber(line, name) {
   line.push(name);
-  return "Welcome, " + name + ". You are number " + line.length + " in line.";
+  let position = line.length;
+  
+  return `Welcome, ${name}. You are number ${position} in line.`;
 }
 
 function nowServing(line) {
-  if (Array.isArray(line) && line.length === 0) {
-    return "There is nobody waiting to be served!";
+  if (line.length === 0) {
+    return `There is nobody waiting to be served!`;
   }
   
-  let nextPerson = line.splice(0, 1);
+  let currentPerson = line.splice(0, 1);
   
-  return "Currently serving " + nextPerson + ".";
+  return `Currently serving ${currentPerson}.`;  
 }
 
 function currentLine(line) {
-  if (Array.isArray(line) && line.length === 0) {
+  if (line.length === 0) {
     return `The line is currently empty.`;
   }
   
-  let lineWithPositions = [];
+  let currentLineWithPositions = [];
   
   line.forEach((name, i) => {
     const position = i + 1;
-    lineWithPositions.push(`${position}. ${name}`);
+    currentLineWithPositions.push(`${position}. ${name}`);
   });
- 
-  return `The line is currently: ${lineWithPositions.join(", ")}`;
+  
+  return `The line is currently: ${currentLineWithPositions.join(", ")}`;
 }
