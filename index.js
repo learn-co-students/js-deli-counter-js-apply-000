8581040fa1fe7ccba3f5cd61d1ebb8fcf20e943f
Samuel Lesser <ineedmoreplates@gmail.com>
2020-01-06 22:37:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+const person = [];
+var i = [];
+
+
+function takeANumber(katzDeliLine,person) {
+    katzDeliLine.push(person);
+    if (i >= 0) { [i]+1;
+    for (i = 0; i < katzDeliLine.length; i++) {
+    return String("Welcome, "+person+". You are number "+[katzDeliLine.length]+" in line.");}
+  }
+}
+
+
+function nowServing(katzDeliLine) {
+        
+        if ([i] < katzDeliLine.length) {
+        for (i= 0; i < katzDeliLine.length; i++) { 
+        var ticket = 'Currently serving '+katzDeliLine[i]+'.';
+        katzDeliLine.shift(0);
+        return ticket;}
+          
+                }   else if ([i] < 0); { return "There is nobody waiting to be served!" }
+          }
+          
+          
+function currentLine(line) {
+     if ([i] < line.length) {
+      var list = [" "+[i+1]+'. '+line[i]," "+[i+2]+'. '+line[i+1]," "+[i+3]+'. '+line[i+2]];
+       for (i=0; i < list.length; i++)
+          return String('The line is currently:'+list);
+
+        } else if ([i] < 0); { 
+
+        return "The line is currently empty.";}
+        }
