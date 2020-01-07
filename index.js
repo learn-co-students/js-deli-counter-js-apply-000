fab6ae231c0bf4052fa8be60d4c3bf14364e690a
calm-fork-7535 <komald.4@gmail.com>
2020-01-07 00:36:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var katzDeli = [];
+function takeANumber (currentLine, name){
+  var placeInLine = currentLine.length +1;
+  var outputString ="Welcome, " + name. "You are number " + placeInLine + " in line.";
+  currentLine.push(name);
+  return outputString;
+}
+
+function nowServing(currentLine) {
+  var outputString;
+  if (currentLine.length === 0) {
+    outputString = "Theere is nobody waiting to be served!"
+  }
+  else {
+    outputString = "Currently serving " + currentLine[0] + ".";
+    currentLine.shift();
+  }
+  return outputString;
+}
+
+  function currentLine (line){
+    var outputString;
+    if (line.length === 0) {
+      outputString ="The line is currently:"
+      for(var i = 0; i < line.length; i++){
+        if(i===0){
+          outputString = outputString + (i+1) + “. ” + line[i];
+          }
+          else{
+          outputString = outputString + “, ” + (i+1) + “. ” + line[i];
+          }
+          }
+          }
+          return outputString;
+          }
