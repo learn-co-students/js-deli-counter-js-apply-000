be94f2abdec0d2ee32f7e5adecac4634efaa38b2
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-01-30 01:10:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 var katzDeliLine = [];
 
-function takeANumber(currLine, newName) {
-    currLine.push(newName);
-    // returns welcome message including new person's position in line:
-    console.log(`Welcome, ${newName}. You are number ${currLine.length} in line.` )
-    return `Welcome, ${newName}. You are number ${currLine.length} in line.` 
-    // "Welcome, Ada. You are number 1 in line."
-        // #1 in this case is 0 index
+// takeANumber accepts 1) current line, 2) new person's name.
+// --> returns welcome message to new person including their place in line.
+// ex: "Welcome, Ada. You are number 1 in line."
+function takeANumber (line, name) {
+	line.push(name);
+	return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-
-
-function nowServing(currLine) {
-
-    if (currLine.length < 1) {
+// nowSeving accepts 1) current line.
+// --> returns first person in line, then removes them from the line.
+function nowServing (line) {
+  if (line.length < 1) {
         return "There is nobody waiting to be served!";
     }
-    let current = 'Currently serving ' + currLine[0] + '.';
-    currLine.shift();
-    return current;
+  
+	let currentPerson = line[0];
+	line.shift();
+	return `Currently serving ${currentPerson}.`;
 }
 
 
+// currentLine accepts 1) current line
+// --> returns the current line as a string formatted as:
+// "The line is currently: 1. Ada, 2. Grace"
+// **IF** the line is empty, "The line is currently empty"
 
+function currentLine (line) {
 
-function currentLine(currLine) {
-    let line = 'The line is currently: ';
-
-    if (currLine.length < 1) {
-        return "The line is currently empty."
+	if (line.length < 1) {
+        return "The line is currently empty.";
     }
 
-    for (var i = 0; i < currLine.length; i++) {
-        line += `${i + 1}. ${currLine[i]}`;
-        if (i !== currLine.length - 1) {
-            line += ', ';
-        }
-    }
-    console.log(line);
-    return line;
+	let numberedNames = [];
+
+	for (let i = 0; i < line.length; i++) {
+		let name = line[i];
+		numberedNames.push(`${i + 1}. ${name}`);
+	}
+	let nameList = numberedNames.join(', ');
+	return `The line is currently: ${nameList}`;
 }
