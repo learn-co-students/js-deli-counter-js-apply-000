b2d6307c1351076880dbcc5921bb7d7f4343e547
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-19 22:13:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeANumber(current, name){
+  if (current.length === 0){
+    current.push(name);
+    return "Welcome, " + name + ". You are number " + (current.length) +" in line.";
+  }else if (current.length > 0){
+    current.push(name);
+     return "Welcome, " + name + ". You are number " + (current.length) +" in line.";
+  }
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  }else{
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  }
+}
+
+function currentLine(katzDeliLine){
+   let newA = [];
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty.";
+  }else{
+    for(let i = 0; i < katzDeliLine.length; i++){
+      newA.push(" "+(i + 1)+ ". " + katzDeliLine[i]);
+    }
+    return "The line is currently:" + newA;
+  }
+}
