a8340dd28876ded1a371b120f7cd77aae47a2d63
Adrian Head <soahead@gmail.com>
2020-02-20 21:40:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+function takeANumber(line,name){
+  var message = "Welcome," + name[i] + " . You are number " + line[i] + " in line.";
+  }
+  line.push(message)
+  name.push(name)
+return takeANumber(line,name)
