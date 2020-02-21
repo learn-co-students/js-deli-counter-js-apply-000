a8d1316a7af6926157e425c5ceea296a0dd78203
Adrian Head <soahead@gmail.com>
2020-02-21 01:16:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-function takeANumber(line,name){
-  var message = "Welcome," + name[i] + " . You are number " + line[i] + " in line.";
+function takeANumber(katzDeli, person){
+  katzDeli.push(person)
+  
+  return "Welcome, " + person + ". You are number " + katzDeli.length + " in line.";
+  
+}
+
+function nowServing(katzDeli){
+  if (katzDeli.length === 0){  
+    return "There is nobody waiting to be served!";
   }
-  line.push(message)
-  name.push(name)
-return takeANumber(line,name)
+  else {
+   var message = "Currently serving " + katzDeli[0] + ".";
+   katzDeli.shift()
+   return message
+  }
+  
+}
+function currentLine(katzDeli){
+  if (katzDeli.length === 0){
+    return "The line is currently empty."
+  }
+  else {
+    var str = "The line is currently: "
+   for (var i = 0; i < katzDeli.length; i++){
+   if (i < katzDeli.length - 1){
+        str += `${i+1}. ${katzDeli[i]}, `  ;
+   }
+   else {
+        str += `${i+1}. ${katzDeli[i]}`  ;
+   }
+  }
+  return str
+  }
+}
