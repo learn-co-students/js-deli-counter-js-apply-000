dfc9e78ae8d502d9ae136d4c2e82203e67a68990
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 22:14:39 +0000

    Done.

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0){
     return "The line is currently empty.";
   }else{
-    for(let i = 0; i < katzDeliLine.length; i++){
-      newA.push(" "+(i + 1)+ ". " + katzDeliLine[i]);
+    for( let i = 0; i < katzDeliLine.length; i++){
+      newA.push( " " + (i + 1) + ". " + katzDeliLine[i] );
     }
     return "The line is currently:" + newA;
   }
