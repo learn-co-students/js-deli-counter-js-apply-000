4386832ce042f563b0f6d595fa36cf58573f40fc
Midori Yang <midoki.yang@gmail.com>
2020-02-27 16:29:06 +0000

    Done.

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function nowServing(line) {
 function currentLine(line) {
   if (line.length === 0) return "The line is currently empty."
   else {
-    var msg = "The line is currently:";
-    for (var i=0; i<line.length; i++) {
+    let msg = "The line is currently:";
+    for (let i=0; i<line.length; i++) {
       msg += ` ${i+1}. ${line[i]},`
     }
     msg = msg.slice(0, -1);
