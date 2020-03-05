2890ba13d0bfc67fc81b17fa5ff1045ab75e72a8
Francisco Andaur <francisco.andaur@icloud.com>
2020-03-05 15:45:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-function takeANumber(line,person) {
-  line.push(person);
-  return `Welcome, ${person}. You are number ${line.length} in line.`;
+function takeANumber(lineArray, nameString){
+    lineArray.push(nameString);
+    for (let i = lineArray.length + 1; i > 0; i++) {
+      return `Welcome, ${nameString}. You are number ${i - 1} in line.`;
+    }
+
+
 }
 
-function nowServing(katzDeliLine, person) {
+function nowServing(line){
+    if (line.length === 0) {
+      return 'There is nobody waiting to be served!'
+    } else {
+      return `Currently serving ${line.shift()}.`
+    }
+}
 
-  if (katzDeliLine.length === 0){
-    return 'There is nobody waiting to be served!';
+function currentLine(line) {
+  if (line.length === 0) {
+      return `The line is currently empty.`;
   } else {
-    return `now serving ${katzDeliLine[i]}.`;
+      const currentLine = []
+
+      for(let i = 0; i < line.length; i++) {
+        currentLine.push(` ${i + 1}. ${line[i]}`)
+      }
+
+  return `The line is currently:${currentLine}`
   }
 }
