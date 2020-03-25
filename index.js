eaea23b98500173640f4bb32585eba14127f79af
ultimate-collaborator-4688 <aalbrecht33733@gmail.com>
2020-03-25 19:17:07 +0000

    Done.

--- index.js
+++ index.js
@@ -24,31 +24,3 @@ function currentLine(arr) {
 
   return `The line is currently: ` + orderedList.slice(0, -2);
 }
-
-
-
-
-// otherDeli = ["Steven", "Blake", "Avi"];
-//
-// function takeANumber(deliLine, name) {
-//   console.log(deliLine)
-//
-//   var nextPersonInLine;
-//
-//   nextPersonInLine = deliLine.pop()
-//
-//   console.log(nextPersonInLine)
-//
-//   console.log(deliLine)
-//
-//   // console.log(deliLine[deliLine.length-1])
-//
-//   // console.log("Welcome, " + deliLine[deliLine.length-1] + ". You are number 4 in line.")
-// }
-//
-// takeANumber(otherDeli, 'Grace')
-
-
-// Array. push()
-// Array.pop()
-// Array.shift()
