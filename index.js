cf48eced9365e6411dbff7a00d9ec258a8dd3fbe
christy l tropila <ctropilalakebeverage@gmail.com>
2020-03-29 00:58:13 +0000

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,35 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  return "Welcome " + name ". You are number " + katzDeliLine.length + " in line."
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
+// adds a person to the end of the line
+
+function nowServing(katzDeliLine){
+
+  let i = 0;
+  while(i<=katzDeliLine.length){
+    i++;
+  }
+  if(katzDeliLine.length===0){
+    return "There is nobody waiting to be served!";
+  }else{
+    return "Currently serving " + katzDeliLine.shift() + "." ;
+  }
+
+}
+
+var line = [];
+function currentLine(katzDeliLine){
+let i =1;
+while(i<katzDeliLine.length){
+  line.push(i + "." + katzDeliLine[i] + ".");
+  i++;
+}
+
+if(katzDeliLine.length===0){
+  return "The line is currently empty.";
+}else{
+  return "The line is currently " + line;
+}
+  }
