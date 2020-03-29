c435226b087f43ec98821a3ec08a35d63808823b
christy l tropila <ctropilalakebeverage@gmail.com>
2020-03-29 00:18:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return "Welcome " + name ". You are number " + katzDeliLine.length + " in line."
+}
