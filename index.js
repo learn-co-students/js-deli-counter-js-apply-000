0564ba58327ae7b3111ae011f91c4e6b83f62a7b
Rocio C <rociocordero@gmail.com>
2020-03-29 22:52:35 +0000

    Done.

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     var output = "";
     for(var i = 0; i < katzDeliLine.length; i++) {
-        output += (i + 1) + ". " + katzDeliLine[i] + ", ";
+       output += `${(i + 1)}. ${katzDeliLine[i]}, `;
       }
-    output = output.slice(0, -2);
+        output = output.slice(0, -2);
     return `The line is currently: ${output}`;
   } else {
     return "The line is currently empty.";
