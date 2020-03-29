b7b12423aee35c063dc6e1d88ba0456393cb7f0a
christy l tropila <ctropilalakebeverage@gmail.com>
2020-03-29 01:19:54 +0000

    Done.

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ function nowServing(katzDeliLine){
 
 var line = [];
 function currentLine(katzDeliLine){
-let i =1;
+let i =0;
 while(i<katzDeliLine.length){
-  line.push(i + "." + katzDeliLine[i] + ".");
+  line.push(` `+[i+1]+`. `  + katzDeliLine[i]);
   i++;
 }
 
 if(katzDeliLine.length===0){
   return "The line is currently empty.";
 }else{
-  return "The line is currently " + line;
+  return "The line is currently:"  + line;
 }
   }
