4c9d89179712b7b86f367d90a1283e09d129a481
christy l tropila <ctropilalakebeverage@gmail.com>
2020-03-30 13:24:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
 var katzDeliLine = [];
+//this var is declared globally and can be used in any function
+
+
+/*this function is called with 2 parameters . ex. takeANumber(katzDeliLine, "Christy") . katzDeliLine=the current line of people,
+"christy"= the new name to be added to the array */
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."  //concatenation
 }
-// adds a person to the end of the line
+// the push function adds a person to the END of the array
+
+
+
+
+
+
 
 function nowServing(katzDeliLine){
-
-  let i = 0;
+/*  var i = 0;
   while(i<=katzDeliLine.length){
     i++;
-  }
+  }*/
+
+  //another way to write this would be using a for loop
+  for(let i = 0; i<=katzDeliLine.length; i++){
+
   if(katzDeliLine.length===0){
     return "There is nobody waiting to be served!";
   }else{
     return "Currently serving " + katzDeliLine.shift() + "." ;
   }
-
+//the shift function removes elements at the beginning of an array
+}
 }
 
+
+
+
+
+
 var line = [];
+//global variable in case we need to use this variable in outside functions in the future
 function currentLine(katzDeliLine){
-/*let i =0;
+
+/*var i =0;
 while(i<katzDeliLine.length){
   line.push(` `+[i+1]+`. `  + katzDeliLine[i]);
   i++;
 } */
-//
-for(let i=0; i<katzDeliLine.length; i++){
-  line.push(` `+[i+1]+`. `  + katzDeliLine[i]);
+
+//refactoring; another way to write this is using a for loop
+for(let i=0; i<katzDeliLine.length; i++){  //this loop will go through all the people in the katzDeliLine and add them to a new array "line" and tell us their place in line with a number
+  line.push(` `+[i+1]+`. `  + katzDeliLine[i]); //[i+1] adds one each time through the loop so that each person in line is assigned a new number
 }
 
+//this condition checks to see if there is anybody in the katzDeliLine, if so, they will be returned as a string that was created pushing each customer to the new array "line"
 if(katzDeliLine.length===0){
   return "The line is currently empty.";
 }else{
