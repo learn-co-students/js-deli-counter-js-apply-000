5c17f0ad5d734679aea9cad692514caa152975ee
Lidia Y De La Cruz <lidcruz@hotmail.com>
2020-04-08 02:00:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeliLine = [];
+
+ function takeANumber (katzDeliLine, name){ 
+ katzDeliLine.push(name)
+let welcomeMessage = 'Welcome,' + " " + name  + ". You are number " + katzDeliLine.length + " in line." 
+ return welcomeMessage
+}
+
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }else{
+    return "Currently serving " + katzDeliLine.shift()+ '.'
+  }
+  
+}
+function currentLine(katzDeliLine){
+  let currentLineMessage = []
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  } else{
+    for(let i = 0; i< katzDeliLine.length; i++){
+      currentLineMessage.push((i + 1 + ".") +" "+ katzDeliLine[i])
+    } return `The line is currently: ${currentLineMessage.join(", ")} `;
+
+    
+  }
+}
