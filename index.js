b0e76de2809d73e68df9df0fd203c60ccef9f271
Adrian Apodaca <aapodaca@gmail.com>
2020-04-16 20:21:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 
 function takeANumber(line, person) {
   line.push(person)
-  console.log("Welcome, ${person}. You are number ${line.length} in line.")
+  return(`Welcome, ${person}. You are number ${line.length} in line.`)
 
 }
 
-function nowServing() {
+function currentLine(line){
 
+  if (line.length === 0) {
+  return "The line is currently empty."
+  }
+
+  if (line.length > 0) {
+    var current = "The line is currently:"
+    for (let num = 0; num < line.length; num +=1) {
+      current += ` ${num + 1}. ${line[num]}${num + 1 < line.length ? ',': ''}`
+    }
+  return current
+  }
+
+}
+
+
+// function nowServing(line) {
+// const deliLine = line
+//   if (line.length === 0) {
+//     return "There is nobody waiting to be served!"
+//   }
+//
+//   if (line.length > 0) {
+//     deliLine.shift
+//     return `Currently serving ${line[0]}.`
+//   }
+
+// }
+function nowServing(line){
+    if (line.length === 0) {
+      return 'There is nobody waiting to be served!'
+    }
+    else {
+      return `Currently serving ${line.shift()}.`
+    }
 }
