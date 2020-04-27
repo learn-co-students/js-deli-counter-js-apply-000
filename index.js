7491545a575552cabae12f5227c5808f9d48124d
stupendous-keyboard-4608 <nicholas.mcdonnell@gmail.com>
2020-04-27 02:55:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 function takeANumber(line, name){
   line.push(name)
-  var position = line.length
+  const position = line.length
   return "Welcome, " + name + ". You are number " + position + " in line." 
 }
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length < 1){
-    return "There is nobody waiting to be served"
+    return "There is nobody waiting to be served!"
   } else {
-    var first = katzDeliLine[i];
-    katzDeliLine.splice(0,1);
-    return first
+    const first = katzDeliLine.shift();
+    return "Currently serving " + first + "."
+  }
+}
+
+function currentLine(line){
+  if (line.length < 1){
+    return "The line is currently empty."
+  } else {
+    const positions = []
+      for (let i = 0; i < line.length; i++){
+        const place = i + 1;
+        const person = place.toString() + ". " + line[i]
+        positions.push(person)
+    } 
+    return "The line is currently: " + positions.join(", ");
   }
 }
