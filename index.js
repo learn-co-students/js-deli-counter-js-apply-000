b9431ec5ce796bc85ea28a463bd6782415e26026
Michael Bade <mbade314@gmail.com>
2020-04-29 19:12:04 +0000

    Done.

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-function nowServing(num) {
-  if (num.length === 0) {
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    var name = num[0];
-    num.splice(0, 1);
+    var name = katzDeliLine[0];
+    katzDeliLine.splice(0, 1);
     return "Currently serving " + name + ".";
   }
 }
 
-function currentLine(movingLine) {
+function currentLine(katzDeliLine) {
   var line = [];
-  if (movingLine.length === 0) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    for (var i = 0; i < movingLine.length; i++) {
-      line += (i + 1) + ". " + movingLine[i] + ", "
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      line += (i + 1) + ". " + katzDeliLine[i] + ", "
     }
     line = line.slice(0, line.length - 2)
     return "The line is currently: " + line
