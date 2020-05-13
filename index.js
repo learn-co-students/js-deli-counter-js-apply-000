d48fb60de31abec8aa96f18383f67f0061887b0e
danny <ohdangitsdaniel@gmail.com>
2020-05-13 18:31:24 +0000

    Done.

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function currentLine(katzDeliLine){
     if(katzDeliLine.length < 1){
       return "The line is currently empty.";
     }
-    let temp = katzDeliLine.reduce(function(a,b,i){
+    const temp = katzDeliLine.reduce(function(a,b,i){
       return a.concat(i+1+ ". " + b);
-    },[]);
-    return "The line is currently: " + temp.join(', ');
+    },[]);    //[ '1. Danny', '2. Jon', '3. Coco' ]
+    return "The line is currently: " + temp.join(', '); //The line is currently: 1. Danny, 2. Jon, 3. Coco
 }
