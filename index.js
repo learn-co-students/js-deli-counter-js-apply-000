6b1507c65474d280deeaa3d1bc690dc8a6a0dc58
tlrollins9 <tlrollins9@gmail.com>
2020-06-30 10:56:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift() + "." }
+    else {
+      return "There is nobody waiting to be served!"
+    }
+  }
+  function currentLine(katzDeliLine) {
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      katzDeliLine.push(i + 1) + "." + katzDeliLine[i] + "," 
+    }
+      if (katzDeliLine.length === 0) {
+        return "The line is currently empty."
+      }
+    }
+  
