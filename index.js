431759d06b9cf9e771b84745706c8a60b4599f17
clarkj99 <clark@stontech.net>
2020-07-15 10:16:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,96 @@
-function takeANumber(line, name) {
-  line.push(name);
-  return "Welcome, " + name + ". You are number " + line.length + " in line.";
+const takeANumber = (line, person) => {
+  line.push(person)
+  return (`Welcome, ${person}. You are number ${line.length} in line.`)
 }
 
-function nowServing (line) {
-  if (line.length >= 1 ) {
-    return "Currently serving " + line.shift() + ".";
+
+const nowServing = (line) => {
+  if (line.length > 0)
+    return `Currently serving ${line.shift()}.`
+  else
+    return "There is nobody waiting to be served!"
+}
+
+const currentLine = (line) => {
+  if (line.length === 0) {
+    return ("The line is currently empty.")
   } else {
-    return "There is nobody waiting to be served!";
-  }
-}
-
-function currentLine (line) {
-  if (line.length >= 1 ) {
-    var statement = "The line is currently: ";
-    for (var i = 0; i < line.length; i++) {
-      var place = i+1; 
-      var separator = (place == line.length) ? "" : ", "; // no comma for last place
-      statement += place.toString() + ". " + line[i] + separator;
+    let names = ""
+    for (let [name, i] of line) {
+      console.log(name)
+      debugger
+      names = ` ${i}. ${name}`
     }
-    return statement;
-  } else {
-    return "The line is currently empty.";
+    return `The line is currently: ${names}`
   }
 }
+
+// currentLine([""])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function takeANumber(line, name) {
+//   line.push(name);
+//   return "Welcome, " + name + ". You are number " + line.length + " in line.";
+// }
+
+// function nowServing (line) {
+//   if (line.length >= 1 ) {
+//     return "Currently serving " + line.shift() + ".";
+//   } else {
+//     return "There is nobody waiting to be served!";
+//   }
+// }
+
+// function currentLine (line) {
+//   if (line.length >= 1 ) {
+//     var statement = "The line is currently: ";
+//     for (var i = 0; i < line.length; i++) {
+//       var place = i+1; 
+//       var separator = (place == line.length) ? "" : ", "; // no comma for last place
+//       statement += place.toString() + ". " + line[i] + separator;
+//     }
+//     return statement;
+//   } else {
+//     return "The line is currently empty.";
+//   }
+// }
