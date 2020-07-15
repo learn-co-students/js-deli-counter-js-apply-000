2f69d973ab97664c4ebd4825ba6f85cce0b215df
clarkj99 <clark@stontech.net>
2020-07-15 13:17:08 -0400

    Done.

--- index.js
+++ index.js
@@ -15,82 +15,10 @@ const currentLine = (line) => {
   if (line.length === 0) {
     return ("The line is currently empty.")
   } else {
-    let names = ""
-    for (let [name, i] of line) {
-      console.log(name)
-      debugger
-      names = ` ${i}. ${name}`
+    let namesArr = []
+    for (let i = 0; i < line.length; i++) {
+      namesArr.push(`${i + 1}. ${line[i]}`)
     }
-    return `The line is currently: ${names}`
+    return (`The line is currently: ${namesArr.join(", ")}`)
   }
 }
-
-// currentLine([""])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function takeANumber(line, name) {
-//   line.push(name);
-//   return "Welcome, " + name + ". You are number " + line.length + " in line.";
-// }
-
-// function nowServing (line) {
-//   if (line.length >= 1 ) {
-//     return "Currently serving " + line.shift() + ".";
-//   } else {
-//     return "There is nobody waiting to be served!";
-//   }
-// }
-
-// function currentLine (line) {
-//   if (line.length >= 1 ) {
-//     var statement = "The line is currently: ";
-//     for (var i = 0; i < line.length; i++) {
-//       var place = i+1; 
-//       var separator = (place == line.length) ? "" : ", "; // no comma for last place
-//       statement += place.toString() + ". " + line[i] + separator;
-//     }
-//     return statement;
-//   } else {
-//     return "The line is currently empty.";
-//   }
-// }
