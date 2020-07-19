aeab829e5e0df478fa9f636242aaa961b0b006f7
ZHENGJIANLIU <782059891@qq.com>
2020-07-19 20:31:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name) // push or add name to the katzDeliLine [] in order start from 0
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 0){
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }else{
+    return 'There is nobody waiting to be served!'
+  }
+}
+
+function currentLine(katzDeliLine){
+  var output = []
+  if (katzDeliLine.length > 0){
+    for (var i = 0; i < katzDeliLine.length; i++){
+      output += ` ${i+1}. ${katzDeliLine[i]},` 
+    }
+    return 'The line is currently:' + output.slice(0,output.length-1)
+  }else{
+    return 'The line is currently empty.'
+  }
+}
