447a8982aef516a74a281ae982a368ac49efc7c8
zhengjianliu <49765007+zhengjianliu@users.noreply.github.com>
2020-07-19 16:31:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-function takeANumber(position,name){
-  position.push(name);
-  return 'Welcome, ' + name +'. You are number ' + position.length + ' in line.'
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name) // push or add name to the katzDeliLine [] in order start from 0
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(position){
-  if (position.length > 0){
-    return 'Currently serving ' + position.shift() + '.'
-  } else{
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 0){
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }else{
     return 'There is nobody waiting to be served!'
   }
 }
 
-function currentLine(position) {
-    var output = [];
-    if (position.length > 0) {
-      for(var i = 0; i < position.length; i++) {
-        output += (i + 1) + ". " + position[i] + ", ";
-      }
-      output = output.slice(0, output.length - 2);
-      return "The line is currently: " + output;
-    } else {
-        return "The line is currently empty.";
+function currentLine(katzDeliLine){
+  var output = []
+  if (katzDeliLine.length > 0){
+    for (var i = 0; i < katzDeliLine.length; i++){
+      output += ` ${i+1}. ${katzDeliLine[i]},` 
     }
+    return 'The line is currently:' + output.slice(0,output.length-1)
+  }else{
+    return 'The line is currently empty.'
+  }
 }
