18195a5b035dce63e6d80f8ece5bcc9b64ccf6da
Allyson Camitta <allyson.camitta@gmail.com>
2020-09-02 20:30:06 +0000

    Done.

--- index.js
+++ index.js
@@ -8,28 +8,23 @@ function takeANumber(currentLine, newName) {
 
 //return first person in line as currently serving
 function nowServing(currentLine) {
-  if (currentLine.length != 0) {
-    
-    return `Currently serving ${currentLine.splice(0, 1)}.`;
+  if (currentLine.length !== 0) {
+    return `Currently serving ${currentLine.shift()}.`;
   }
   else {
-    
     return 'There is nobody waiting to be served!';
   }
 }
 
 //convert current line to string
 function currentLine(currentLine) {
-  let inLine = [];
   if (currentLine.length === 0) {
-    
     return 'The line is currently empty.';
   }
-  else {
-    for (let i = 0; i < currentLine.length; i++) {
-      inLine.push(`${i + 1}. ${currentLine[i]}`);
-    }
-  }
   
+  let inLine = [];
+  for (let i = 0; i < currentLine.length; i++) {
+    inLine.push(`${i + 1}. ${currentLine[i]}`);
+  }
   return `The line is currently: ${inLine.join(", ")}`;
 }
