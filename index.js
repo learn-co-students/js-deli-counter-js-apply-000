661fda08fd437d433e8a1609c2208c9282a07f8c
Joshua Frank <eurosewusa@gmail.com>
2020-10-06 14:03:34 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ function takeANumber( line, customer) {
   line.push( customer );
   return `Welcome, ${customer}. You are number ${line.length} in line.`
 }
+
+function nowServing ( line ) {
+  if ( line.length > 0 ) {
+    var serving = line.shift();
+    return `Currently serving ${serving}.`
+  }
+  return `There is nobody waiting to be served!`
+}
+
+function currentLine( line ) {
+  var result = "The line is currently";
+  if ( line.length === 0 ) return result+" empty.";
+  result += ": ";
+  for ( var i = 0; i < line.length - 1; i++ ) { 
+    result += `${ i + 1 }. ${ line[ i ] }, `;
+  }
+  result += `${ i + 1 }. ${ line[ line.length - 1 ] }`;
+  return result;
+}
