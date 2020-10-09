8d48120ef5133dd56313cc9c6d6804148c361188
Jordan Carroll <jordancarroll1994@gmail.com>
2020-10-09 03:14:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, person) {
+  katzDeliLine.push(person);
+  return `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing (katzDeliLine) {
+  let i = 0
+  while (i < katzDeliLine.length) {
+    i++
+  }
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeliLine.shift()}.`);
+}
+
+function currentLine (katzDeliLine) {
+  var line = []
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    line.push(` `+[i+1]+`. ` + katzDeliLine[i]);
+ }
+ if (katzDeliLine.length === 0) {
+   return "The line is currently empty.";
+ } else {
+ return(`The line is currently:` + line);
+ }
